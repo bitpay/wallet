9db1740b1f4518af63643137bc4873703968fc1b
Mario Colque <colquemario@gmail.com>
2014-04-14 16:48:12 -0300

    Added support for list and create address in a opened wallet

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -10,8 +10,15 @@ angular.module('copay.home').controller('HomeController',
       $location.path('signin');
     }
 
-    $scope.addrs = [
-    { addrStr: 'n3zUqNR7Bbbc4zJhPVj1vG2Lx66K3Xhzvb'},
-    { addrStr: 'my9wnLwwUrwpNfEgSrWY62ymEGf1edKf4J'}
-    ];
+    $scope.addrs = $rootScope.publicKeyRing.getAddresses();
+    $scope.selectedAddr = $scope.addrs[0];
+
+    $scope.newAddr = function() {
+      var a = $rootScope.publicKeyRing.generateAddress();
+      $scope.addrs.push({ addrStr: a.toString('hex') });
+    };
+
+    $scope.selectAddr = function(addr) {
+      $scope.selectedAddr = addr;
+    };
   });
