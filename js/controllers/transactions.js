5bf4bca0d3c0ca63a83a5a723c57af59e77cd7fc
Mario Colque <colquemario@gmail.com>
2014-04-14 17:58:12 -0300

    added list of pending transactions

--- js/controllers/transactions.js
+++ js/controllers/transactions.js
@@ -10,6 +10,9 @@ angular.module('copay.transactions').controller('TransactionsController',
       $location.path('signin');
     }
 
+    console.log('########################');
+    console.log($rootScope);
+
     $scope.txsinput = [
     {
       fromAddr: "n3zUqNR7Bbbc4zJhPVj1vG2Lx66K3Xhzvb",
@@ -23,16 +26,18 @@ angular.module('copay.transactions').controller('TransactionsController',
     }
     ];
 
-    $scope.txsoutput = [
-    {
-      fromAddr: "n3zUqNR7Bbbc4zJhPVj1vG2Lx66K3Xhzvb",
-      toAddr: "msvv2mDfE298s7boXwALq4Dqv77K3TWRZ1",
-      amount: 23.9982
-    },
-    {
-      fromAddr: "my9wnLwwUrwpNfEgSrWY62ymEGf1edKf4J",
-      toAddr: "monCusNiDuptf68rtr58hEjKpJt6cW6zwS",
-      amount: 2.22
-    }
-    ];
+    $scope.txsoutput = $rootScope.txProposals.txps;
+
+    // $scope.txsoutput = [
+    // {
+    //   fromAddr: "n3zUqNR7Bbbc4zJhPVj1vG2Lx66K3Xhzvb",
+    //   toAddr: "msvv2mDfE298s7boXwALq4Dqv77K3TWRZ1",
+    //   amount: 23.9982
+    // },
+    // {
+    //   fromAddr: "my9wnLwwUrwpNfEgSrWY62ymEGf1edKf4J",
+    //   toAddr: "monCusNiDuptf68rtr58hEjKpJt6cW6zwS",
+    //   amount: 2.22
+    // }
+    // ];
   });
