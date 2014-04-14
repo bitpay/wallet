00d4a4b9791708dad0bc58c0785542844eb35ad9
Manuel Araoz <manuelaraoz@gmail.com>
2014-04-14 18:30:08 -0300

    adding wallet factory

--- js/services/storage.js
+++ js/services/storage.js
@@ -3,69 +3,31 @@
 angular.module('copay.storage')
   .factory('Storage', function($rootScope) {
 
-
-    var _key = function(walletId, key) {
-      return walletId + '::' + key;
-    };
-
-    var _pushKey = function(walletId, key) {
-       var keys = localStorage.getItem(walletId);
-       localStorage.setItem(walletId, (keys?keys+',':'') +key);
-    };
-
-    return {        
-      getGlobal: function( key) {
-       return  JSON.parse(localStorage.getItem(key));
+    return {
+      getGlobal: function(key) {
+        return JSON.parse(localStorage.getItem(key));
       },
-      setGlobal: function( key, data) {
-       localStorage.setItem(key, JSON.stringify(data));
+      setGlobal: function(key, data) {
+        localStorage.setItem(key, JSON.stringify(data));
       },
       get: function(walletId, key) {
         if (!walletId) return;
-        return  JSON.parse(localStorage.getItem(_key(walletId,key)));
+        return JSON.parse(localStorage.getItem(_key(walletId, key)));
       },
       set: function(walletId, key, data) {
         if (!walletId) return;
-        var k = _key(walletId,key);
+        var k = _key(walletId, key);
         localStorage.setItem(k, JSON.stringify(data));
         _pushKey(walletId, k);
       },
       remove: function(walletId, key) {
-        localStorage.removeItem(_key(walletId,key));
+        localStorage.removeItem(_key(walletId, key));
       },
-      clearAll: function(walletId){
+      clearAll: function(walletId) {
         var keys = localStorage.getItem(walletId);
-        keys.split(',').forEach(function(k){
+        keys.split(',').forEach(function(k) {
           localStorage.removeItem(key);
         });
-      },
-      addWalletId: function(walletId) {
-        var ids = localStorage.getItem('walletIds');
-        if (ids) {
-          var list = ids.split(',');
-          var l = list.length;
-          for(var i=0; i<l; i++) 
-            if (walletId === list[i]) 
-              return;
-        }
-        localStorage.setItem('walletIds', (ids?ids+',':'')  + walletId);
-      },
-      delWalletId: function(walletId) {
-        var ids = localStorage.getItem('walletIds');
-        if (ids) {
-          var is = ids.split(','); 
-          var newIds = [];
-          is.forEach(function(i) {
-            if (i != walletId) {
-              newIds.push(i);
-            }
-          });
-          localStorage.setItem('walletIds', newIds.join(',') );
-        }
-      },
-      getWalletIds: function() {
-        var ids = localStorage.getItem('walletIds');
-        return ids ? ids.split(',') : [];
       },
     };
   });
--- test/test.wallet.js
+++ test/test.wallet.js
@@ -1,25 +1,47 @@
 'use strict';
 
-var chai           = chai || require('chai');
-var should         = chai.should();
-var bitcore        = bitcore || require('bitcore');
-var copay          = copay || require('../copay');
-var Wallet         = copay.Wallet || require('./js/models/core/Wallet');
+var chai = chai || require('chai');
+var should = chai.should();
+var copay = copay || require('../copay');
+var Wallet = require('soop').load('../js/models/core/Wallet', {
+  Storage: require('./FakeStorage'),
+  Network: copay.WebRTC,
+  Blockchain: copay.Insight
+});
 
+console.log(Wallet);
 
-var config = {
-  wallet: {
-    requiredCopayers: 3,
-    totalCopayers: 5,
-  }
-};
 
 describe('Wallet model', function() {
+  var config = {
+    wallet: {
+      requiredCopayers: 3,
+      totalCopayers: 5,
+    }
+  };
+  var opts = {};
 
-  it('should create an instance', function () {
+
+  it.skip('should create an instance', function () {
     var opts = {};
-    var w = new Wallet(config, opts);
+    var w = Wallet.create(config, opts);
     should.exist(w);
   });
+
+
+  describe('factory', function() {
+    it('should create the factory', function() {
+      should.exist(Wallet.factory);
+    });
+    it('should be able to create wallets', function() {
+      var w = Wallet.factory.create(config, opts);
+      should.exist(w);
+    });
+    it.skip('should be able to get wallets', function() {
+      var w = Wallet.factory.create(config, opts);
+      var v = Wallet.factory.get(config, w.id);
+      should.exist(w);
+    });
+  });
+
 });
-  
