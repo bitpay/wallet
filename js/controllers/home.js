aa3b7dabdaea36e88c1a222511e82ad98b2e7c12
Mario Colque <colquemario@gmail.com>
2014-04-14 09:46:43 -0300

    added support for generate new addresses from the home tab

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('copay.home').controller('HomeController',
-  function($scope, $rootScope, $location) {
+  function($scope, $rootScope) {
+
     $scope.title = 'Home';
 
     $scope.oneAtATime = true;
@@ -10,8 +11,25 @@ angular.module('copay.home').controller('HomeController',
       $location.path('signin');
     }
 
-    $scope.addrs = [
-    { addrStr: 'n3zUqNR7Bbbc4zJhPVj1vG2Lx66K3Xhzvb'},
-    { addrStr: 'my9wnLwwUrwpNfEgSrWY62ymEGf1edKf4J'}
-    ];
+//     $scope.addrs = [
+//       { addrStr: 'n3zUqNR7Bbbc4zJhPVj1vG2Lx66K3Xhzvb'},
+//       { addrStr: 'my9wnLwwUrwpNfEgSrWY62ymEGf1edKf4J'}
+//     ];
+
+    $scope.addrs = $rootScope.PublicKeyRing.getAddresses();
+    console.log('########################');
+    console.log($scope.addrs);
+
+    // by default select the first address
+    $scope.selectedQR = $scope.addrs[0];
+
+    $scope.changeQR = function(addr) {
+      $scope.selectedQR = addr;
+    };
+
+    $scope.newAddress = function() {
+      var a = $rootScope.PublicKeyRing.generateAddress();
+      console.log(a);
+      $scope.addrs.push({ addrStr: a });
+    };
   });
