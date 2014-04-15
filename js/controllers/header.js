d535522e9d895d1d7169a52f42d67930bc248a96
Bechi <bechilandia@gmail.com>
2014-04-15 12:33:00 -0300

    add icon navbar

--- js/controllers/header.js
+++ js/controllers/header.js
@@ -4,20 +4,29 @@ angular.module('copay.header').controller('HeaderController',
   function($scope, $rootScope, $location, Network) {
     $scope.menu = [{
       'title': 'Home',
+      'icon': 'fi-home',
       'link': '#/home'
     }, {
       'title': 'Copayers',
+      'icon': 'fi-torsos-all',
       'link': '#/peer'
     }, {
       'title': 'Transactions',
+      'icon': 'fi-loop',
       'link': '#/transactions'
     }, {
       'title': 'Send',
+      'icon': 'fi-arrow-right',
       'link': '#/send'
     }, {
       'title': 'Backup',
+      'icon': 'fi-archive',
       'link': '#/backup'
-    }];
+    }, {
+      'title': 'signout',
+      'icon': 'fi-power',
+      'link': '#/signout'
+    }]
 
     if (!$rootScope.peerId) {
       $location.path('signin');
