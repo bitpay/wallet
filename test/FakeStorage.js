3475c58e23d4bcaed0698cd259548e0ea3388812
Matias Alejo Garcia <ematiu@gmail.com>
2014-04-15 14:55:09 -0300

    fix Fakestorage

--- test/FakeStorage.js
+++ test/FakeStorage.js
@@ -3,12 +3,24 @@ var FakeStorage = function(){
   this.storage = {};
 }; 
 
-FakeStorage.prototype.set = function (id, payload) {
+FakeStorage.prototype.setGlobal = function (id, payload) {
   this.storage[id] = payload;
 };
 
-FakeStorage.prototype.get = function(id) {
+FakeStorage.prototype.getGlobal = function(id) {
   return this.storage[id];
+}
+
+FakeStorage.prototype.set = function (wid, id, payload) {
+  this.storage[wid + '-' + id] = payload;
+};
+
+FakeStorage.prototype.get = function(wid, id) {
+  return this.storage[wid + '-' +id];
+}
+
+FakeStorage.prototype.clear = function() {
+  delete this['storage'];
 }
 
 module.exports = require('soop')(FakeStorage);
