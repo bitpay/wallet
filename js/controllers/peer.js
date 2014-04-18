58a15502d1046b46cebfd5693295856d5d519c20
Mario Colque <colquemario@gmail.com>
2014-04-18 15:36:38 -0300

    clean up peerController

--- js/controllers/peer.js
+++ js/controllers/peer.js
@@ -2,11 +2,6 @@
 
 angular.module('copay.peer').controller('PeerController',
   function($scope, $rootScope, $location, $routeParams, Socket, controllerUtils) {
-    
-    $scope.init = function() {
-      //Network.connect($rootScope.masterId);
-    };
-
     controllerUtils.handleTransactionByAddress($scope);
   });
 
