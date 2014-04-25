bc77fabba91a71ed59d679fafb5f315c228972d7
Mario Colque <colquemario@gmail.com>
2014-04-25 17:34:24 -0300

    Added import angularjs controller

--- js/app.js
+++ js/app.js
@@ -15,7 +15,8 @@ var copayApp = window.copayApp = angular.module('copay',[
   'copay.controllerUtils',
   'copay.setup',
   'copay.directives',
-  'copay.video'
+  'copay.video',
+  'copay.import'
 ]);
 
 angular.module('copay.header', []);
@@ -30,4 +31,5 @@ angular.module('copay.setup', []);
 angular.module('copay.socket', []);
 angular.module('copay.directives', []);
 angular.module('copay.video', []);
+angular.module('copay.import', []);
 
---
+++ js/controllers/import.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('copay.import').controller('ImportController',
+  function($scope, $rootScope) {
+    $scope.title = 'Import a backup';
+  });
