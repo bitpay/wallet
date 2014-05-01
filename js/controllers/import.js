960b64affe60b1ccbdcc0b3568f6d63e8e6b95c8
Mario Colque <colquemario@gmail.com>
2014-05-01 19:08:35 -0300

    added import from text backup

--- js/controllers/import.js
+++ js/controllers/import.js
@@ -4,6 +4,13 @@ angular.module('copay.import').controller('ImportController',
   function($scope, $rootScope, walletFactory, controllerUtils, Passphrase) {
     $scope.title = 'Import a backup';
 
+
+    var _importBackup = function(encryptedObj) {
+      var passphrase = Passphrase.getBase64($scope.password);
+      $rootScope.wallet = walletFactory.fromEncryptedObj(encryptedObj, passphrase);
+      controllerUtils.startNetwork($rootScope.wallet);
+    };
+
     $scope.getFile = function() {
       var reader = new FileReader();
 
@@ -11,17 +18,18 @@ angular.module('copay.import').controller('ImportController',
       reader.onloadend = function(evt) {
         if (evt.target.readyState == FileReader.DONE) { // DONE == 2
           var encryptedObj = evt.target.result;
-          var passphrase = Passphrase.getBase64($scope.password);
-          $rootScope.wallet = walletFactory.fromEncryptedObj(encryptedObj, passphrase);
-          controllerUtils.startNetwork($rootScope.wallet);
+          _importBackup(encryptedObj);
         }
       };
+    };
 
-      $scope.import = function() {
-        if ($scope.password != '') {
+    $scope.import = function() {
+      if ($scope.password != '') {
+        if ($scope.backupText != '') {
+          _importBackup($scope.backupText);
+        } else {
           reader.readAsBinaryString($scope.file);
         }
-      };
-
+      }
     };
   });
