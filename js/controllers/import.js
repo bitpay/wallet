fc6a24c6b101dd101b84760fff892404d0bc00c5
Mario Colque <colquemario@gmail.com>
2014-05-01 19:39:01 -0300

    added import from backup text

--- js/controllers/import.js
+++ js/controllers/import.js
@@ -4,7 +4,7 @@ angular.module('copay.import').controller('ImportController',
   function($scope, $rootScope, walletFactory, controllerUtils, Passphrase) {
     $scope.title = 'Import a backup';
 
-
+    var reader = new FileReader();
     var _importBackup = function(encryptedObj) {
       var passphrase = Passphrase.getBase64($scope.password);
       $rootScope.wallet = walletFactory.fromEncryptedObj(encryptedObj, passphrase);
@@ -12,8 +12,6 @@ angular.module('copay.import').controller('ImportController',
     };
 
     $scope.getFile = function() {
-      var reader = new FileReader();
-
       // If we use onloadend, we need to check the readyState.
       reader.onloadend = function(evt) {
         if (evt.target.readyState == FileReader.DONE) { // DONE == 2
@@ -24,8 +22,8 @@ angular.module('copay.import').controller('ImportController',
     };
 
     $scope.import = function() {
-      if ($scope.password != '') {
-        if ($scope.backupText != '') {
+      if ($scope.password) {
+        if ($scope.backupText) {
           _importBackup($scope.backupText);
         } else {
           reader.readAsBinaryString($scope.file);
