225fce5ff8adacd3fa6d6a60d5a6548cd6c38edf
Manuel Araoz <manuelaraoz@gmail.com>
2014-05-05 16:28:56 -0300

    add lots of STUN/TURN servers

--- config.template.js
+++ config.template.js
@@ -24,9 +24,57 @@ var config = {
     },
     config: {
       'iceServers': [
-        // Pass in STUN and TURN server for maximum network compatibility
+        // Pass in STUN and TURN servers for maximum network compatibility
         {
+          url: 'stun:stun01.sipphone.com'
+        }, {
+          url: 'stun:stun.ekiga.net'
+        }, {
+          url: 'stun:stun.fwdnet.net'
+        }, {
+          url: 'stun:stun.ideasip.com'
+        }, {
+          url: 'stun:stun.iptel.org'
+        }, {
+          url: 'stun:stun.rixtelecom.se'
+        }, {
+          url: 'stun:stun.schlund.de'
+        }, {
           url: 'stun:stun.l.google.com:19302'
+        }, {
+          url: 'stun:stun1.l.google.com:19302'
+        }, {
+          url: 'stun:stun2.l.google.com:19302'
+        }, {
+          url: 'stun:stun3.l.google.com:19302'
+        }, {
+          url: 'stun:stun4.l.google.com:19302'
+        }, {
+          url: 'stun:stunserver.org'
+        }, {
+          url: 'stun:stun.softjoys.com'
+        }, {
+          url: 'stun:stun.voiparound.com'
+        }, {
+          url: 'stun:stun.voipbuster.com'
+        }, {
+          url: 'stun:stun.voipstunt.com'
+        }, {
+          url: 'stun:stun.voxgratia.org'
+        }, {
+          url: 'stun:stun.xten.com'
+        }, {
+          url: 'turn:numb.viagenie.ca',
+          credential: 'muazkh',
+          username: 'webrtc@live.com'
+        }, {
+          url: 'turn:192.158.29.39:3478?transport=udp',
+          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+          username: '28224511:1379330808'
+        }, {
+          url: 'turn:192.158.29.39:3478?transport=tcp',
+          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+          username: '28224511:1379330808'
         }, {
           url: 'turn:homeo@turn.bistri.com:80',
           credential: 'homeo'
