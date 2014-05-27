7788ea3fd45b0000060772b49b5c673b7a33774f
Manuel Araoz <manuelaraoz@gmail.com>
2014-05-27 17:05:43 -0300

    fix wallet creation problem

--- js/models/core/Wallet.js
+++ js/models/core/Wallet.js
@@ -77,11 +77,15 @@ console.log('[Wallet.js.58] connecting'); //TODO
 Wallet.prototype._handlePublicKeyRing = function(senderId, data, isInbound) {
   this.log('RECV PUBLICKEYRING:', data);
 
-  var recipients, pkr = this.publicKeyRing;
   var inPKR = copay.PublicKeyRing.fromObj(data.publicKeyRing);
+  var wasIncomplete = !this.publicKeyRing.isComplete();
+  var hasChanged = this.publicKeyRing.merge(inPKR, true);
 
-  var hasChanged = pkr.merge(inPKR, true);
   if (hasChanged) {
+    if (wasIncomplete) {
+      this.sendPublicKeyRing();
+      this.connectToAll();
+    }
     if (this.publicKeyRing.isComplete()) {
       this._lockIncomming();
     }
