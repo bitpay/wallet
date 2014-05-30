d831d166a4e32d16e614d9727ddc898c7c0fd670
Mario Colque <colquemario@gmail.com>
2014-05-30 11:36:14 -0300

    removes test folder

--- test/js/services/passphrase.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-angular.module('copay.passphrase').value('Passphrase', new copay.Passphrase(config.passphrase));
--- test/js/services/walletFactory.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-angular.module('copay.walletFactory').value('walletFactory', new copay.WalletFactory(config, copay.version));
-
