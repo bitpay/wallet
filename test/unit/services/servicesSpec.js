99a4ba027d44ea1b024684c080e0ba85f3f95546
Matias Alejo Garcia <ematiu@gmail.com>
2014-06-04 20:17:45 -0300

    add tests for Socket service

--- test/unit/services/servicesSpec.js
+++ test/unit/services/servicesSpec.js
@@ -9,6 +9,40 @@ describe("Unit: Testing Services", function() {
     expect(Socket).not.to.equal(null);
   }));
 
+
+  it('Socket should support #on', inject(function(Socket) {
+    expect(Socket.on).to.be.a('function');
+  }));
+
+
+  it('Socket should support #sysOn', inject(function(Socket) {
+    expect(Socket.sysOn).to.be.a('function');
+  }))
+
+
+  it('Socket should add handlers with #on', inject(function(Socket) {
+    Socket.on('a', function (){});
+    Socket.on('b', function (){});
+    Socket.sysOn('c', function (){});
+    var ret = Socket.getListeners();
+    expect(ret.a).to.be.equal(1);
+    expect(ret.b).to.be.equal(1);
+    expect(Object.keys(ret)).to.have.length(2);
+  }));
+
+  it('Socket should support #removeAllListeners', inject(function(Socket) {
+    Socket.on('a', function (){});
+    Socket.on('b', function (){});
+    Socket.sysOn('c', function (){});
+    var ret = Socket.getListeners();
+    expect(Object.keys(ret)).to.have.length(2);
+    Socket.removeAllListeners();
+    ret = Socket.getListeners();
+    expect(Object.keys(ret)).to.have.length(0);
+  }));
+
+
+
   it('should contain a walletFactory service', inject(function(walletFactory) {
     expect(walletFactory).not.to.equal(null);
   }));
