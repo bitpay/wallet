13a9eebd9d0113236a052f701a677731d5d456cd
Manuel Araoz <manuelaraoz@gmail.com>
2014-06-06 16:34:30 -0300

    add Passphrase test

---
+++ test/test.Passphrase.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var chai           = chai || require('chai');
+var should         = chai.should();
+var bitcore        = bitcore || require('bitcore');
+var buffertools    = bitcore.buffertools;
+var copay          = copay || require('../copay');
+var Passphrase     = copay.Passphrase;
+
+
+
+describe('Passphrase model', function() {
+
+  it('should create an instance', function () {
+    var p = new Passphrase();
+    should.exist(p);
+  });
+
+});
+
+
