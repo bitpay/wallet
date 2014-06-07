90d28febe20ba08f86df64dcb96443a127a781e3
Matias Alejo Garcia <ematiu@gmail.com>
2014-06-07 13:36:47 -0300

    update to peerjs 0.3.8. use bower dist

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ var defaultConfig = {
 
     // other PeerJS config
     maxPeers: 15,
-    debug: 1,
+    debug: 3,
 
     // Network encryption config
     sjclParams: {
@@ -44,55 +44,7 @@ var defaultConfig = {
         // Pass in STUN and TURN servers for maximum network compatibility
         {
           url: 'stun:stun01.sipphone.com'
-        }, {
-          url: 'stun:stun.ekiga.net'
-        }, {
-          url: 'stun:stun.fwdnet.net'
-        }, {
-          url: 'stun:stun.ideasip.com'
-        }, {
-          url: 'stun:stun.iptel.org'
-        }, {
-          url: 'stun:stun.rixtelecom.se'
-        }, {
-          url: 'stun:stun.schlund.de'
-        }, {
-          url: 'stun:stun.l.google.com:19302'
-        }, {
-          url: 'stun:stun1.l.google.com:19302'
-        }, {
-          url: 'stun:stun2.l.google.com:19302'
-        }, {
-          url: 'stun:stun3.l.google.com:19302'
-        }, {
-          url: 'stun:stun4.l.google.com:19302'
-        }, {
-          url: 'stun:stunserver.org'
-        }, {
-          url: 'stun:stun.softjoys.com'
-        }, {
-          url: 'stun:stun.voiparound.com'
-        }, {
-          url: 'stun:stun.voipbuster.com'
-        }, {
-          url: 'stun:stun.voipstunt.com'
-        }, {
-          url: 'stun:stun.voxgratia.org'
-        }, {
-          url: 'stun:stun.xten.com'
-        }, {
-          url: 'turn:numb.viagenie.ca',
-          credential: 'muazkh',
-          username: 'webrtc@live.com'
-        }, {
-          url: 'turn:192.158.29.39:3478?transport=udp',
-          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-          username: '28224511:1379330808'
-        }, {
-          url: 'turn:192.158.29.39:3478?transport=tcp',
-          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-          username: '28224511:1379330808'
-        }
+        },
       ]
     }
   },
