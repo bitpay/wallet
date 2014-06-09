973a62a2cfba6ac4202ca33afbf5c1c09098598e
Manuel Araoz <manuelaraoz@gmail.com>
2014-06-09 14:13:44 -0300

    remove beep

--- js/controllers/header.js
+++ js/controllers/header.js
@@ -62,9 +62,7 @@ angular.module('copayApp.controllers').controller('HeaderController',
           }
         }
         if (currentAddr) {
-          var beep = new Audio('sound/transaction.mp3');
           $notification.funds('Received fund', currentAddr, receivedFund);
-          beep.play();
         }
       }
     });
