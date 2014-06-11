a930b084b947327ddef54d8bff8fa50983c1bdc6
Yemel Jardi <angel.jardi@gmail.com>
2014-06-11 15:39:56 -0300

    Change wording of very weak password, add strength tip, add tests

--- js/directives.js
+++ js/directives.js
@@ -126,49 +126,30 @@ angular.module('copayApp.directives')
       restrict: 'EACM',
       require: 'ngModel',
       link: function(scope, element, attrs) {
-        var _grep = function(elems, callback, invert) {
-          var callbackInverse,
-              matches = [],
-              i = 0,
-              length = elems.length,
-              callbackExpect = !invert;
-
-          // Go through the array, only saving the items
-          // that pass the validator function
-          for (; i < length; i++) {
-            callbackInverse = !callback(elems[i], i);
-            if (callbackInverse !== callbackExpect) {
-              matches.push(elems[i]);
-            }
-          }
-
-          return matches;
-        };
-
         var strength = {
-          messages: ['too weak', 'weak', 'weak', 'medium', 'strong'],
+          messages: ['very weak', 'weak', 'weak', 'medium', 'strong'],
           colors: ['#c0392b', '#e74c3c', '#d35400', '#f39c12', '#27ae60'],
           mesureStrength: function (p) {
-            var _force = 0;
-            var _regex = /[$-/:-?{-~!"^_`\[\]]/g;
-            var _lowerLetters = /[a-z]+/.test(p);
-            var _upperLetters = /[A-Z]+/.test(p);
-            var _numbers = /[0-9]+/.test(p);
-            var _symbols = _regex.test(p);
-            var _flags = [_lowerLetters, _upperLetters, _numbers, _symbols];
-            var _passedMatches = _grep(_flags, function (el) { return el === true; }).length;
+            var force = 0;
+            var regex = /[$-/:-?{-~!"^_`\[\]]/g;
+            var lowerLetters = /[a-z]+/.test(p);
+            var upperLetters = /[A-Z]+/.test(p);
+            var numbers = /[0-9]+/.test(p);
+            var symbols = regex.test(p);
+            var flags = [lowerLetters, upperLetters, numbers, symbols];
+            var passedMatches = flags.filter(function (el) { return !!el; }).length;
             
-            _force += 2 * p.length + ((p.length >= 10) ? 1 : 0);
-            _force += _passedMatches * 10;
+            force = 2 * p.length + (p.length >= 10 ? 1 : 0);
+            force += passedMatches * 10;
             
             // penality (short password)
-            _force = (p.length <= 6) ? Math.min(_force, 10) : _force;
+            force = (p.length <= 6) ? Math.min(force, 10) : force;
             
             // penality (poor variety of characters)
-            _force = (_passedMatches == 1) ? Math.min(_force, 10) : _force;
-            _force = (_passedMatches == 2) ? Math.min(_force, 20) : _force;
-            _force = (_passedMatches == 3) ? Math.min(_force, 40) : _force;
-            return _force;
+            force = (passedMatches == 1) ? Math.min(force, 10) : force;
+            force = (passedMatches == 2) ? Math.min(force, 20) : force;
+            force = (passedMatches == 3) ? Math.min(force, 40) : force;
+            return force;
           },
           getColor: function (s) {
             var idx = 0;
