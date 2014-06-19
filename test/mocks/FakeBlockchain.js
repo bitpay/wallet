26c829f6de84a101bc2737f6d4ac33be18f2ddad
Yemel Jardi <angel.jardi@gmail.com>
2014-06-19 10:30:53 -0300

    Add validation and tests for spendUnconfirmed = false

--- test/mocks/FakeBlockchain.js
+++ test/mocks/FakeBlockchain.js
@@ -30,6 +30,20 @@ FakeBlockchain.prototype.getUnspent = function(addresses, cb) {
   }]);
 };
 
+FakeBlockchain.prototype.getUnspent2 = function(addresses, cb) {
+  if (!addresses || !addresses.length) return cb(null, []);
+  return cb(null, this.u || [{
+    'address': 'mji7zocy8QzYywQakwWf99w9bCT6orY1C1',
+    'txid': '0be0fb4579911be829e3077202e1ab47fcc12cf3ab8f8487ccceae768e1f95fa',
+    'vout': 0,
+    'ts': 1402323949,
+    'scriptPubKey': '21032ca453c1d9a93b7de8cf3d44d7bb8d52a45dbdf8fff63f69de4e51b740bb1da3ac',
+    'amount': 25.0001,
+    'confirmations': 1,
+    'confirmationsFromCache': false
+  }]);
+};
+
 FakeBlockchain.prototype.sendRawTransaction = function(rawtx, cb) {
   var txid = '0be0fb4579911be829e3077202e1ab47fcc12cf3ab8f8487ccceae768e1f95fa';
   return cb(txid);
