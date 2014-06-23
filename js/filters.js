88b9167f55e6498d0e139f4966fd3a9fc702ceb5
Yemel Jardi <angel.jardi@gmail.com>
2014-06-23 13:26:13 -0300

    Change default address list

--- js/filters.js
+++ js/filters.js
@@ -17,41 +17,16 @@ angular.module('copayApp.filters', [])
   })
   .filter('limitAddress', function() {
     return function(elements, showAll) {
-      var addrs = [];
-      if (elements.length > 0) {
-        if (showAll) {
-          return elements;
-        }
-        if (elements.length == 1) {
-          return elements;
-        }
-        else {
-          for (var i=0;i<elements.length;i++) {
-            if (!elements[i].isChange && (!elements[i].balance || elements[i].balance == 0)) {
-              addrs.push(elements[i]);
-              break;
-            }
-          }
-          
-          for (var i=0;i<elements.length;i++) {
-            if (elements[i].balance && elements[i].balance > 0) {
-              addrs.push(elements[i]);
-            }
-          }
-          return addrs; 
-        }
+      if (elements.length <= 1 || showAll) {
+        return elements;
       }
-    };
-  })
-  .filter('withoutFunds', function() {
-    return function(elements) {
-      var len = 0;
-      for (var i=0;i<elements.length;i++) {
-        if (!elements[i].balance || elements[i].balance == 0) {
-          len++;
-        }
-      }
-      return len;
+
+      // Show last 3 non-change addresses plus those with balance
+      var addrs = elements.filter(function(e, i) {
+        return (!e.isChange && i < 3) || (e.balance && e.balance > 0);
+      });
+
+      return addrs;
     };
   })
 ;
