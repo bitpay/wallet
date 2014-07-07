7b0350335c10158c096e44021f7fae43ea46e9ea
Matias Alejo Garcia <ematiu@gmail.com>
2014-07-07 16:57:57 -0300

    workaround bug of urihandler in firefox

--- js/services/uriHandler.js
+++ js/services/uriHandler.js
@@ -5,8 +5,7 @@ var UriHandler = function() {};
 UriHandler.prototype.register = function() {
   var base = window.location.origin + '/';
   var url = base + '#/uri_payment/%s';
-  navigator.registerProtocolHandler('bitcoin',
-    url, 'Copay');
+  // navigator.registerProtocolHandler('bitcoin', url, 'Copay');
 };
 
 angular.module('copayApp.services').value('uriHandler', new UriHandler());
