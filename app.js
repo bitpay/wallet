528205e541b0140953249c77beda679c6cd20de6
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2014-07-16 17:59:28 -0300

    Split index.html in many files smaller. New right column when the wallet is ready. Removed templates selector.

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
+app.use('/', express.static(__dirname + '/'));
+app.get('*', function(req, res) {
+  return res.sendfile('index.html');
+});
+
 app.start = function(port, callback) {
 
   app.set('port', port);
