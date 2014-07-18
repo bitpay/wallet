b51e8b3539a037b257d2a046e39ebc58822fd9f7
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2014-07-18 17:56:50 -0300

    password confirmation when joining and creating a wallet

--- js/directives.js
+++ js/directives.js
@@ -236,4 +236,20 @@ angular.module('copayApp.directives')
   }
 ])
 
+  .directive('match', function () {
+    return {
+      require: 'ngModel',
+      restrict: 'A',
+      scope: {
+          match: '='
+      },
+      link: function(scope, elem, attrs, ctrl) {
+          scope.$watch(function() {
+              return (ctrl.$pristine && angular.isUndefined(ctrl.$modelValue)) || scope.match === ctrl.$modelValue;
+          }, function(currentValue) {
+              ctrl.$setValidity('match', currentValue);
+          });
+      }
+    };
+  })
 ;
