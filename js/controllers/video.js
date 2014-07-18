c3255b17ba59e522f1818a12daa1f05a1dd05b5b
Yemel Jardi <angel.jardi@gmail.com>
2014-07-18 18:38:28 -0300

    Add copayer list at sidebar, separate snippet and clean video controller code

--- js/controllers/video.js
+++ js/controllers/video.js
@@ -3,16 +3,35 @@
 angular.module('copayApp.controllers').controller('VideoController', 
   function($scope, $rootScope, $sce) {
 
+  // Cached list of copayers
+  $scope.copayers = $rootScope.wallet.getRegisteredPeerIds();
+
+  $scope.copayersList = function() {
+    return $rootScope.wallet.getRegisteredPeerIds();
+  }
+
+  $scope.hasVideo = function(copayer) {
+    return $rootScope.videoInfo[copayer.peerId];
+  }
+
+  $scope.isConnected = function(copayer) {
+    return $rootScope.wallet.getOnlinePeerIDs().indexOf(copayer.peerId) != -1;
+  }
+
+  $scope.isBackupReady = function(copayer) {
+    return $rootScope.wallet.publicKeyRing.isBackupReady(copayer.copayerId);
+  }
+
   $scope.getVideoURL = function(copayer) {
     if (config.disableVideo) return;
 
-    var vi = $rootScope.videoInfo[copayer]
-      if (!vi) return;
+    var vi = $scope.videoInfo[copayer.peerId];
+    if (!vi) return;
 
-    if ($rootScope.wallet.getOnlinePeerIDs().indexOf(copayer) === -1) {
+    if ($scope.isConnected(copayer)) {
       // peer disconnected, remove his video
-      delete $rootScope.videoInfo[copayer]
-        return;
+      delete $rootScope.videoInfo[copayer.peerId];
+      return;
     }
 
     var encoded = vi.url;
