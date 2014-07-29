c3f055be89460800196745c88b28c086123e28d0
Yemel Jardi <angel.jardi@gmail.com>
2014-07-29 14:13:21 -0300

    Change routes to old hastag mode

--- js/services/uriHandler.js
+++ js/services/uriHandler.js
@@ -5,7 +5,10 @@ var UriHandler = function() {};
 UriHandler.prototype.register = function() {
   var base = window.location.origin + '/';
   var url = base + '#/uri-payment/%s';
-  navigator.registerProtocolHandler('bitcoin', url, 'Copay');
+
+  if(navigator.registerProtocolHandler) {
+    navigator.registerProtocolHandler('bitcoin', url, 'Copay');
+  }
 };
 
 angular.module('copayApp.services').value('uriHandler', new UriHandler());
