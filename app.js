c67c1e9efc4d295784532e737b33c4f3fed1d491
Christopher Jeffrey <chjjeffrey@gmail.com>
2014-07-30 19:14:29 -0700

    paypro: more debugging.

--- app.js
+++ app.js
@@ -12,6 +12,26 @@ app.start = function(port, callback) {
   app.set('port', port);
   app.use(express.static(__dirname));
 
+  if (process.env.USE_HTTPS) {
+    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+    var fs = require('fs');
+    var path = require('path');
+    var bc = path.dirname(require.resolve('bitcore/package.json'));
+    var server = require('https').createServer({
+      key: fs.readFileSync(bc + '/test/data/x509.key'),
+      cert: fs.readFileSync(bc + '/test/data/x509.crt')
+    });
+    var pserver = require(bc + '/examples/PayPro/server.js');
+    server.on('request', function(req, res) {
+      app(req, res);
+      pserver.app(res, res);
+    });
+    app.listen(port, function() {
+      callback('https://localhost:' + port);
+    });
+    return;
+  }
+
   app.listen(port, function() {
     callback('http://localhost:' + port);
   });
