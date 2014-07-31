e5325ff8f800d04866c307a18aea09257744c898
Matias Alejo Garcia <ematiu@gmail.com>
2014-07-30 21:19:39 -0300

    separate TxProposal to a different class. new tests

---
+++ js/models/core/TxProposal.js
@@ -0,0 +1,226 @@
+'use strict';
+
+var bitcore = require('bitcore');
+var util = bitcore.util;
+var Transaction = bitcore.Transaction;
+var BuilderMockV0 = require('./BuilderMockV0');;
+var TransactionBuilder = bitcore.TransactionBuilder;
+var Script = bitcore.Script;
+var Key = bitcore.Key;
+var buffertools = bitcore.buffertools;
+var preconditions = require('preconditions').instance();
+
+
+function TxProposal(opts) {
+  preconditions.checkArgument(opts);
+  preconditions.checkArgument(opts.inputChainPaths);
+  preconditions.checkArgument(opts.creator);
+  preconditions.checkArgument(opts.createdTs);
+  preconditions.checkArgument(opts.builder);
+
+
+  this.creator = opts.creator;
+  this.createdTs = opts.createdTs;
+  this.builder = opts.builder;
+  this.inputChainPaths = opts.inputChainPaths;
+
+  this._inputSignedBy = [];
+  this.seenBy = opts.seenBy || {};
+  this.signedBy = opts.signedBy || {};
+  this.rejectedBy = opts.rejectedBy || {};
+  this.sentTs = opts.sentTs || null;
+  this.sentTxid = opts.sentTxid || null;
+  this.comment = opts.comment || null;
+  this.readonly = opts.readonly || null;
+  this._updateSignedBy();
+}
+
+TxProposal.prototype.getId = function() {
+  return this.builder.build().getNormalizedHash().toString('hex');
+};
+
+TxProposal.prototype.toObj = function() {
+  var o = JSON.parse(JSON.stringify(this));
+  delete o['builder'];
+  o.builderObj = this.builder.toObj();
+  return o;
+};
+
+
+TxProposal.prototype.setSent = function(sentTxid) {
+  this.sentTxid = sentTxid;
+  this.sentTs = Date.now();
+};
+
+TxProposal.fromObj = function(o, forceOpts) {
+  preconditions.checkArgument(o.builderObj);
+  delete o['builder'];
+
+  try {
+    // force opts is requested.
+    for (var k in forceOpts) {
+      o.builderObj.opts[k] = forceOpts[k];
+    }
+    o.builder = TransactionBuilder.fromObj(o.builderObj);
+  } catch (e) {
+    if (!o.version) {
+      o.builder = new BuilderMockV0(o.builderObj);
+      o.readonly = 1;
+    };
+  }
+  var t = new TxProposal(o);
+  t._throwIfInvalid();
+  t._updateSignedBy();
+  return t;
+};
+
+
+
+TxProposal.prototype._formatKeys = function(allowedPubKeys) {
+  var keys = [];
+  for (var i in allowedPubKeys) {
+    if (!Buffer.isBuffer(allowedPubKeys[i]))
+      throw new Error('allowedPubKeys must be buffers');
+
+    var k = new Key();
+    k.public = allowedPubKeys[i];
+    keys.push[k];
+  };
+};
+
+TxProposal.prototype._verifySignatures = function(inKeys, scriptSig, txSigHash) {
+  preconditions.checkArgument(Buffer.isBuffer(txSigHash));
+
+  if (scriptSig.chunks[0] !== 0)
+    throw new Error('Invalid scriptSig');
+
+  var keys = this._formatKeys(inKeys);
+  var ret = [];
+  for (var i = 1; typeof ret === 'undefined' && i <= scriptSig.countSignatures(); i++) {
+    var chunk = scriptSig.chunks[i];
+    var sigRaw = new Buffer(chunk.slice(0, chunk.length - 1));
+    for (var j in keys) {
+      var k = keys[j];
+      if (k.verifySignatureSync(txSigHash, sigRaw)) {
+        ret.push(j);
+      }
+    }
+  }
+  return ret;
+};
+
+TxProposal.prototype._keysFromRedeemScript = function(s) {
+  var redeemScript =  new Script(s.chunks[s.chunks.length-1]);
+  if (!redeemScript)
+    throw new Error('Bad scriptSig');
+  var pubkeys = redeemScript.capture();
+  if (!pubkeys || !pubkeys.length)
+    throw new Error('Bad scriptSig');
+
+  return pubkeys;
+};
+
+TxProposal.prototype._updateSignedBy = function() {
+  this._inputSignedBy = [];
+
+  var tx = this.builder.build();
+  for (var i in tx.ins) {
+    var scriptSig = new Script(tx.ins[i].s);
+
+    var keys = this._keysFromRedeemScript(scriptSig);
+    var txSigHash = tx.hashForSignature(this.builder.inputMap[i].scriptPubKey, i, Transaction.SIGHASH_ALL);
+    var copayerIndex = this._verifySignatures(keys, scriptSig, txSigHash);
+    if (typeof copayerIndex === 'undefined')
+      throw new Error('Invalid signature');
+    this._inputSignedBy[i] = this._inputSignedBy[i] || {};
+    this._inputSignedBy[i][copayerIndex] = true;
+  };
+};
+
+TxProposal.prototype.isValid = function() {
+
+  if (this.builder.signhash && this.builder.signhash !== Transaction.SIGHASH_ALL) {
+    return false;
+  }
+
+  var tx = this.builder.build();
+  if (!tx.ins.length)
+    return false;
+
+  for (var i = 0; i < tx.ins.length; i++) {
+    var hashType = tx.getHashType(i);
+    if (hashType && hashType !== Transaction.SIGHASH_ALL) {
+      return false;
+    }
+  }
+
+  console.log('[TxProposal.js.145]'); //TODO
+
+  //Should be signed
+  var scriptSigs = this.builder.vanilla.scriptSigs;
+  if (!scriptSigs)
+    return false;
+
+
+  console.log('[TxProposal.js.153]'); //TODO
+  return true;
+};
+
+
+TxProposal.prototype._throwIfInvalid = function(allowedPubKeys) {
+  if (!this.isValid(allowedPubKeys))
+    throw new Error('Invalid tx proposal');
+};
+
+
+TxProposal.prototype.merge = function(incoming, allowedPubKeys) {
+  var ret = {};
+  ret.events = [];
+  incoming._throwIfInvalid(allowedPubKeys);
+
+  /* TODO */
+
+  /*
+     events.push({
+type: 'seen',
+cId: k,
+txId: ntxid
+});
+events.push({
+type: 'signed',
+cId: k,
+txId: ntxid
+});
+events.push({
+type: 'rejected',
+cId: k,
+txId: ntxid
+});
+ret.events = this.mergeMetadata(incoming);
+*/
+  ret.hasChanged = this.mergeBuilder(incoming);
+  return ret;
+};
+
+TxProposal.prototype.mergeBuilder = function(incoming) {
+  var b0 = this.builder;
+  var b1 = incoming.builder;
+
+  var before = JSON.stringify(b0.toObj());
+  b0.merge(b1);
+  var after = JSON.stringify(b0.toObj());
+  return after !== before;
+};
+
+
+//This should be on bitcore / Transaction
+TxProposal.prototype.countSignatures = function() {
+  var tx = this.builder.build();
+
+  var ret = 0;
+  for (var i in tx.ins) {
+    ret += tx.countInputSignatures(i);
+  }
+  return ret;
+};
+module.exports = TxProposal;
---
+++ test/test.TxProposal.js
@@ -0,0 +1,170 @@
+'use strict';
+
+var chai = chai || require('chai');
+var should = chai.should();
+var bitcore = bitcore || require('bitcore');
+var Transaction = bitcore.Transaction;
+var buffertools = bitcore.buffertools;
+var WalletKey = bitcore.WalletKey;
+var Key = bitcore.Key;
+var bignum = bitcore.Bignum;
+var Script = bitcore.Script;
+var TransactionBuilder = bitcore.TransactionBuilder;
+var util = bitcore.util;
+var networks = bitcore.networks;
+var sinon = require('sinon');
+try {
+  var copay = require('copay'); //browser
+} catch (e) {
+  var copay = require('../copay'); //node
+}
+
+var FakeBuilder = require('./mocks/FakeBuilder');
+var TxProposal = copay.TxProposal;
+
+var dummyProposal = new TxProposal({
+  creator: 1,
+  createdTs: 1,
+  builder: new FakeBuilder(),
+  inputChainPaths: ['m/1'],
+});
+
+var someKeys = ["03b39d61dc9a504b13ae480049c140dcffa23a6cc9c09d12d6d1f332fee5e18ca5","022929f515c5cf967474322468c3bd945bb6f281225b2c884b465680ef3052c07e"];
+
+describe('TxProposal', function() {
+  describe('new', function() {
+    it('should fail to create an instance with wrong arguments', function() {
+
+      (function() {
+        var txp = new TxProposal();
+      }).should.throw('Illegal Argument');
+
+      (function() {
+        var txp = new TxProposal({
+          creator: 1
+        });
+      }).should.throw('Illegal Argument');
+
+    });
+
+
+    it('should create an instance', function() {
+      var txp = new TxProposal({
+        creator: 1,
+        createdTs: 1,
+        builder: new FakeBuilder(),
+        inputChainPaths: 'm/1',
+      });
+      should.exist(txp);
+
+      txp.creator.should.equal(1);
+      should.exist(txp.builder);
+      txp.inputChainPaths.should.equal('m/1');
+    });
+  });
+  describe('#getId', function() {
+    it('should return id',function() {
+      var b = new FakeBuilder();
+      var spy = sinon.spy(b.tx, 'getNormalizedHash');
+      var txp = new TxProposal({
+        creator: 1,
+        createdTs: 1,
+        builder: b,
+        inputChainPaths: 'm/1',
+      });
+      txp.getId().should.equal('123456');;
+      sinon.assert.callCount(spy,1);
+    });
+  });
+  describe('#toObj', function() {
+    it('should return an object and remove builder',function() {
+      var b = new FakeBuilder();
+      var txp = new TxProposal({
+        creator: 1,
+        createdTs: 1,
+        builder: b,
+        inputChainPaths: 'm/1',
+      });
+      var o = txp.toObj();
+      should.exist(o);
+      o.creator.should.equal(1);
+      should.not.exist(o.builder);
+      should.exist(o.builderObj);
+    });
+  });
+  describe('#fromObj', function() {
+    it.skip('should create from Object',function() {
+      var b = new FakeBuilder();
+      var txp = TxProposal.fromObj({
+        creator: 1,
+        createdTs: 1,
+        builderObj: b.toObj(),
+        inputChainPaths: ['m/1'] ,
+      });
+      should.exist(txp);
+    });
+
+
+    it('should fail to create from wrong object',function() {
+      var b = new FakeBuilder();
+      (function(){var txp = TxProposal.fromObj({
+        creator: 1,
+        createdTs: 1,
+        builderObj: b.toObj(),
+        inputChainPaths: ['m/1'] ,
+      });}).should.throw('Invalid');
+    });
+
+
+  });
+
+  describe('#setSent', function() {
+    it('should set txid and timestamp',function() {
+      var now = Date.now();
+      var txp = dummyProposal;
+      txp.setSent('3a42');
+      txp.sentTs.should.gte(now);
+      txp.sentTxid.should.equal('3a42');
+    });
+  });
+
+
+  describe('Signature verification', function() {
+    var validScriptSig = '00483045022100a35a5cbe37e39caa62bf1c347eae9c72be827c190b31494b184943b3012757a8022008a1ff72a34a5bf2fc955aa5b6f8a4c32cb0fab7e54c212a5f6f645bb95b8ef10149304602210092347916c3c3e6f1692bf9447b973779c28ce9985baaa3940b483af573f464b4022100ab91062796ab8acb32a0fa90e00627db5be77d9722400b3ecfd9c5f34a8092b1014cad532103197599f6e209cefef07da2fddc6fe47715a70162c531ffff8e611cef23dfb70d210380a29968851f93af55e581c43d9ef9294577a439a3ca9fc2bc47d1ca2b3e9127210392dccb2ed470a45984811d6402fdca613c175f8f3e4eb8e2306e8ccd7d0aed032103a94351fecc4328bb683bf93a1aa67378374904eac5980c7966723a51897c56e32103e085eb6fa1f20b2722c16161144314070a2c316a9cae2489fd52ce5f63fff6e455ae';
+
+    var keyBuf = someKeys.map(function(hex){
+      return new Buffer(hex,'hex');
+    });
+    it('#_formatKeys',function() {
+      var txp = dummyProposal;
+      (function(){txp._formatKeys(someKeys);}).should.throw('buffers');
+      var res = txp._formatKeys(keyBuf);
+    });
+    it('#_verifyScriptSig arg checks',function() {
+      var txp = dummyProposal;
+      ( function() { txp._verifySignatures(
+        keyBuf,
+        new bitcore.Script(new Buffer('112233','hex')),
+        new Buffer('1a','hex')); } ).should.throw('script');
+    });
+    it('#_verifyScriptSig, no signatures',function() {
+      var txp = dummyProposal;
+      var ret =  txp._verifySignatures(
+        keyBuf,
+        new bitcore.Script(new Buffer(validScriptSig,'hex')),
+        new Buffer('1a','hex')
+      );
+      ret.length.should.equal(0);
+    });
+    it('#_verifyScriptSig, one signature',function() {
+      var txp = dummyProposal;
+      var ret =  txp._verifySignatures(
+        [new Buffer('0380a29968851f93af55e581c43d9ef9294577a439a3ca9fc2bc47d1ca2b3e9127','hex')],
+        new bitcore.Script(new Buffer(validScriptSig,'hex')),
+        new Buffer('1a','hex')
+      );
+      ret.length.should.equal(0);
+    });
+
+  });
+});
