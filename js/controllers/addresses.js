b60e8ed40753ba191ea9d9521e824a8dc168021c
Yemel Jardi <angel.jardi@gmail.com>
2014-08-13 13:34:47 -0300

    Add copy to clipboard button

--- js/controllers/addresses.js
+++ js/controllers/addresses.js
@@ -19,6 +19,12 @@ angular.module('copayApp.controllers').controller('AddressesController',
     $scope.openAddressModal = function(address) {
       var ModalInstanceCtrl = function ($scope, $modalInstance, address) {
         $scope.address = address;
+        $scope.isMobile = !!window.cordova;
+
+        $scope.mobileCopy = function(address) {
+          window.cordova.plugins.clipboard.copy(address);
+          window.plugins.toast.showShortBottom('Copied to clipboard');
+        }
 
         $scope.cancel = function () {
           $modalInstance.dismiss('cancel');
