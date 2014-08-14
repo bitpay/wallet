d8e0d50dcefaaa7884bd0cc15cd8dead7405c6c1
Matias Alejo Garcia <ematiu@gmail.com>
2014-08-14 18:25:53 -0400

    add WalletLock class

--- copay.js
+++ copay.js
@@ -15,6 +15,7 @@ var StorageLocalEncrypted = module.exports.StorageLocalEncrypted = require('./js
 
 module.exports.WalletFactory = require('./js/models/core/WalletFactory');
 module.exports.Wallet = require('./js/models/core/Wallet');
+module.exports.WalletLock = require('./js/models/core/WalletLock');
 module.exports.version = require('./version');
 
 // test hack :s, will fix 
