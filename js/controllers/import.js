db1b7d7c453159668d678ac0a3c059dcaf0d26e3
Matias Alejo Garcia <ematiu@gmail.com>
2014-08-21 11:50:40 -0400

    add advanced options to import

--- js/controllers/import.js
+++ js/controllers/import.js
@@ -7,10 +7,7 @@ angular.module('copayApp.controllers').controller('ImportController',
 
     $scope.title = 'Import a backup';
     $scope.importStatus = 'Importing wallet - Reading backup...';
-    var s = ($location.search()).skip;
-    if (s) {
-      $scope.skipFields = s.split(',');
-    }
+    $scope.hideAdv=true;
 
     var reader = new FileReader();
 
@@ -24,9 +21,20 @@ angular.module('copayApp.controllers').controller('ImportController',
         updateStatus('Importing wallet - Setting things up...');
         var w, errMsg;
 
+        var skipFields = [];
+        if ($scope.skipPublicKeyRing)
+          skipFields.push('publicKeyRing');
+
+        if ($scope.skipTxProposals)
+          skipFields.push('txProposals');
+
+        if ($scope.skipPrivateKey)
+          skipFields.push('privateKey');
+
+console.log('[import.js.36:skipFields:]',skipFields); //TODO
         // try to import encrypted wallet with passphrase
         try {
-          w = walletFactory.import(encryptedObj, passphrase, $scope.skipFields);
+          w = walletFactory.import(encryptedObj, passphrase, skipFields);
         } catch (e) {
           errMsg = e.message;
         }
