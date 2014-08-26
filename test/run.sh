ea94f67747424a5096cea9d1b67c74f5702b7403
Manuel Araoz <manuelaraoz@gmail.com>
2014-08-26 16:08:20 -0300

    make tests run with separate script

---
+++ test/run.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && \
+  cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && \
+  ./node_modules/karma/bin/karma start --single-run
