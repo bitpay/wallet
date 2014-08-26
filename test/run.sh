d6fb7280c84f18fc4afc47094914905d103e0d72
Manuel Araoz <manuelaraoz@gmail.com>
2014-08-26 16:41:12 -0300

    fix browser to firefox

--- test/run.sh
+++ test/run.sh
@@ -2,4 +2,4 @@
 
 node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && \
   cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && \
-  ./node_modules/karma/bin/karma start --single-run
+  ./node_modules/karma/bin/karma start --browsers Firefox --single-run
