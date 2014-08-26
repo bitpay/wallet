42296dc0e7d9c91cd1efb5d0fb4b22510fede180
Manuel Araoz <manuelaraoz@gmail.com>
2014-08-26 15:38:59 -0300

    setup two browsers

--- karma.conf.js
+++ karma.conf.js
@@ -102,7 +102,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['Chrome', 'Firefox'],
 
 
     // Continuous Integration mode
