921cb8dd90cedb8cb849cb2f6dfdba2f4efcecef
Manuel Araoz <manuelaraoz@gmail.com>
2014-09-03 11:33:45 -0300

    settings: refactor controllers

--- js/controllers/more.js
+++ js/controllers/more.js
@@ -4,6 +4,53 @@ angular.module('copayApp.controllers').controller('MoreController',
   function($scope, $rootScope, $location, backupService, walletFactory, controllerUtils, notification) {
     var w = $rootScope.wallet;
 
+
+    $scope.unitOpts = [{
+      name: 'Satoshis (100,000,000 satoshis = 1BTC)',
+      shortName: 'SAT',
+      value: 1,
+      decimals: 0
+    }, {
+      name: 'bits (1,000,000 bits = 1BTC)',
+      shortName: 'bits',
+      value: 100,
+      decimals: 2
+    }, {
+      name: 'mBTC (1,000 mBTC = 1BTC)',
+      shortName: 'mBTC',
+      value: 100000,
+      decimals: 5
+    }, {
+      name: 'BTC',
+      shortName: 'BTC',
+      value: 100000000,
+      decimals: 8
+    }];
+    $scope.selectedAlternative = {
+      name: config.alternativeName,
+      isoCode: config.alternativeIsoCode
+    };
+    $scope.alternativeOpts = rateService.isAvailable ?
+      rateService.listAlternatives() : [$scope.selectedAlternative];
+
+    rateService.whenAvailable(function() {
+      $scope.alternativeOpts = rateService.listAlternatives();
+      for (var ii in $scope.alternativeOpts) {
+        if (config.alternativeIsoCode === $scope.alternativeOpts[ii].isoCode) {
+          $scope.selectedAlternative = $scope.alternativeOpts[ii];
+        }
+      }
+    });
+
+
+    for (var ii in $scope.unitOpts) {
+      if (config.unitName === $scope.unitOpts[ii].shortName) {
+        $scope.selectedUnit = $scope.unitOpts[ii];
+        break;
+      }
+    }
+
+
     $scope.hideAdv = true;
     $scope.hidePriv = true;
     if (w)
