c66f770b0e049fe506b1909d3a13ee8c524533f4
Esteban Ordano <eordano@gmail.com>
2014-09-26 05:29:14 -0300

    Feature (identity): service to store and retrieve credentials

---
+++ js/util/crypto.js
@@ -0,0 +1,45 @@
+/**
+ * Small module for some helpers that wrap CryptoJS with some good practices.
+ */
+var sjcl = require('sjcl'),
+     log = require('../log.js'),
+       _ = require('underscore');
+
+var SALT = 'copay random string NWRlNmExMTE4NzIzYzYyYWMwODU1MTdkN';
+var SEPARATOR = '&';
+var defaultOptions = {
+  adata: '',
+  cipher: 'aes',
+  ks: 128,
+  iter: 2000,
+  mode: 'ccm',
+  ts: 64
+};
+
+module.exports = {
+
+  kdf: function(value1, value2) {
+    return sjcl.codec.base64.fromBits(sjcl.misc.pbkdf2(value1 + value2, SALT));
+  },
+
+  /**
+   * Encrypts symmetrically using a passphrase
+   */
+  encrypt: function(key, message) {
+    return sjcl.encrypt(key, message);
+  },
+
+  /**
+   * Decrypts symmetrically using a passphrase
+   */
+  decrypt: function(key, cypher) {
+    var output = {};
+    try {
+      return sjcl.decrypt(key, cypher);
+    } catch (e) {
+      log.error('Decryption failed due to error: ' + e.message);
+      return null;
+    }
+  }
+};
+
