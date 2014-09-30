57a718cdbd7bcdfa6936df8708303426904ae830
Ivan Socolsky <jungans@gmail.com>
2014-09-30 13:04:48 -0300

    Refactored copayers selection on create form

--- js/controllers/create.js
+++ js/controllers/create.js
@@ -1,37 +1,5 @@
 'use strict';
 
-
-var valid_pairs = {
-  '1,1': 112,
-  '1,2': 147,
-  '2,2': 220,
-  '1,3': 182,
-  '2,3': 256,
-  '3,3': 329,
-  '1,4': 216,
-  '2,4': 290,
-  '3,4': 363,
-  '4,4': 436,
-  '1,5': 250,
-  '2,5': 324,
-  '3,5': 398,
-  '4,5': 470,
-  '1,6': 284,
-  '2,6': 358,
-  '3,6': 432,
-  '1,7': 318,
-  '2,7': 392,
-  '3,7': 465,
-  '1,8': 353,
-  '2,8': 427,
-  '1,9': 387,
-  '2,9': 461,
-  '1,10': 421,
-  '2,10': 495,
-  '1,11': 455,
-  '1,12': 489
-};
-
 angular.module('copayApp.controllers').controller('CreateController',
   function($scope, $rootScope, $location, $timeout, walletFactory, controllerUtils, Passphrase, backupService, notification) {
     controllerUtils.redirIfLogged();
@@ -49,19 +17,12 @@ angular.module('copayApp.controllers').controller('CreateController',
     }
 
     $scope.totalCopayers = config.wallet.totalCopayers;
-    $scope.TCValues = [];
-    for (var n = 1; n <= config.limits.totalCopayers; n++)
-      $scope.TCValues.push(n);
+    $scope.TCValues = _.range(1, config.limits.totalCopayers + 1);
 
     var updateRCSelect = function(n) {
-      $scope.requiredCopayers = (valid_pairs[[parseInt(n / 2 + 1), n]] > 0 || config.networkName === 'testnet') ?
-        parseInt(n / 2 + 1) : 1;
-      $scope.RCValues = [];
-      for (var m = 1; m <= n; m++) {
-        if (valid_pairs[[m, n]] > 0 || config.networkName === 'testnet') {
-          $scope.RCValues.push(m);
-        }
-      }
+      var maxReq = copay.Wallet.getMaxRequiredCopayers(n);
+      $scope.RCValues = _.range(1, maxReq + 1);
+      $scope.requiredCopayers = Math.min(parseInt(n / 2 + 1), maxReq);
     };
 
     updateRCSelect($scope.totalCopayers);
