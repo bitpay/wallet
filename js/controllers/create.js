790eeeb2f935d2fbe3cbbad5fea1ae66229bc71b
ssotomayor <sansoto2003@yahoo.com.ar>
2014-10-10 11:47:16 -0300

    Fix for updating network being used.

--- js/controllers/create.js
+++ js/controllers/create.js
@@ -36,6 +36,10 @@ angular.module('copayApp.controllers').controller('CreateController',
       $scope.networkUrl = config.network[$scope.networkName].url;
     });
 
+    $scope.showNetwork = function(){
+      return $scope.networkUrl == config.network['testnet'] || $scope.networkUrl == config.network['livenet'];
+    };
+
     $scope.create = function(form) {
       if (form && form.$invalid) {
         notification.error('Error', 'Please enter the required fields');
