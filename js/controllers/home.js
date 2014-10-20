704925f8ca012be7b9c1f04980d40ac5dfff768f
Ivan Socolsky <jungans@gmail.com>
2014-10-20 10:50:03 -0300

    do not select wallet on 'ready' message

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -31,6 +31,7 @@ angular.module('copayApp.controllers').controller('HomeController', function($sc
         controllerUtils.onErrorDigest(
           $scope, (err.toString() || '').match('PNOTFOUND') ? 'Profile not found' : 'Unknown error');
       } else {
+        console.log('$rootScope.wallet -->', $rootScope.wallet, 'FIRST WALLET ->', firstWallet);
         controllerUtils.bindProfile($scope, iden, firstWallet);
       }
     });
