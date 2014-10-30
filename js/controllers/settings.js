422210037ddb4f20a19d9bffb4d2dac3d8f11b31
Matias Pando <mpando@bitpay.com>
2014-10-30 11:08:34 -0300

    Added drop down

--- js/controllers/settings.js
+++ js/controllers/settings.js
@@ -7,21 +7,42 @@ angular.module('copayApp.controllers').controller('SettingsController', function
   $scope.defaultLanguage = config.defaultLanguage || 'en';
   $scope.insightLivenet = config.network.livenet.url;
   $scope.insightTestnet = config.network.testnet.url;
+  $scope.defaultLogLevel = config.logLevel || 'log';
+
+
+
+  //$scope.availableLogLevels = logger.levels;
+
+  $scope.availableLogLevels = {
+    'debug': 0,
+    'info': 1,
+    'log': 2,
+    'warn': 3,
+    'error': 4,
+    'fatal': 5
+  };
+
+  console.log($scope.defaultLogLevel);
+  console.log($scope.availableLogLevels);
+
+  for (var key in $scope.availableLogLevels) {
+    console.log("Ele " + key + " -- " + $scope.availableLogLevels[key])
+  }
 
   $scope.availableStorages = [{
-    name: 'Insight',
-    pluginName: 'EncryptedInsightStorage',
-  }, {
-    name: 'Localstorage',
-    pluginName: 'EncryptedLocalStorage',
-  },
-  // {
-  //   name: 'GoogleDrive',
-  //   pluginName: 'GoogleDrive',
-  // }
+      name: 'Insight',
+      pluginName: 'EncryptedInsightStorage',
+    }, {
+      name: 'Localstorage',
+      pluginName: 'EncryptedLocalStorage',
+    },
+    // {
+    //   name: 'GoogleDrive',
+    //   pluginName: 'GoogleDrive',
+    // }
   ];
 
-  _.each($scope.availableStorages, function(v){
+  _.each($scope.availableStorages, function(v) {
     if (config.plugins[v.pluginName])
       $scope.selectedStorage = v;
   });
@@ -37,6 +58,13 @@ angular.module('copayApp.controllers').controller('SettingsController', function
   for (var ii in $scope.availableLanguages) {
     if ($scope.defaultLanguage === $scope.availableLanguages[ii].isoCode) {
       $scope.selectedLanguage = $scope.availableLanguages[ii];
+      break;
+    }
+  }
+
+  for (var ii in $scope.availableLogLevels) {
+    if ($scope.defaultLogLevel === $scope.availableLogLevels[ii]) {
+      $scope.selectedLogLevel = $scope.availableLogLevels[ii];
       break;
     }
   }
@@ -59,11 +87,14 @@ angular.module('copayApp.controllers').controller('SettingsController', function
     var plugins = {};
     plugins[$scope.selectedStorage.pluginName] = true;
 
+    logger.setLevel($scope.selectedLogLevel);
+
     localStorage.setItem('config', JSON.stringify({
       network: insightSettings,
       version: copay.version,
       defaultLanguage: $scope.selectedLanguage.isoCode,
       plugins: plugins,
+      logLevel: $scope.selectedLogLevel,
     }));
 
     // Go home reloading the application
