16c3d66f33fd6b229f2c5b204bb3bf480657f473
Matias Pando <mpando@bitpay.com>
2014-10-31 19:39:01 -0300

    Import profile

---
+++ js/controllers/importProfile.js
@@ -0,0 +1,91 @@
+'use strict';
+
+angular.module('copayApp.controllers').controller('ImportProfileController',
+  function($scope, $rootScope, $location, controllerUtils, notification, isMobile, pluginManager) {
+    controllerUtils.redirIfLogged();
+
+    $scope.title = 'Import a backup';
+    $scope.importStatus = 'Importing wallet - Reading backup...';
+    $scope.hideAdv = true;
+    $scope.is_iOS = isMobile.iOS();
+
+    var reader = new FileReader();
+
+    var updateStatus = function(status) {
+      $scope.importStatus = status;
+      $scope.$digest();
+    }
+
+    var _importBackup = function(str) {
+      var password = $scope.password;
+      updateStatus('Importing profile - Setting things up...');
+      // var skipFields = [];
+      // if ($scope.skipPublicKeyRing)
+      //   skipFields.push('publicKeyRing');
+      //
+      // if ($scope.skipTxProposals)
+      //   skipFields.push('txProposals');
+
+      copay.Identity.importFromEncryptedFullJson(str, password, {
+        pluginManager: pluginManager,
+        network: config.network,
+        networkName: config.networkName,
+        walletDefaults: config.wallet,
+        passphraseConfig: config.passphraseConfig,
+      }, function(err, iden) {
+        if (err && !iden) {
+          console.log('Error:' + err)
+          controllerUtils.onErrorDigest(
+            $scope, (err.toString() || '').match('BADSTR') ? 'Bad password or corrupt profile file' : 'Unknown error');
+        } else {
+          console.log('Success.....Profile imported successfully');
+          notification.info('Success', 'Profile imported successfully');
+          $location.path('/');
+        }
+      });
+    };
+
+    $scope.openFileDialog = function() {
+      if (window.cshell) {
+        return cshell.send('backup:import');
+      }
+      $scope.choosefile = !$scope.choosefile;
+    };
+
+    $scope.getFile = function() {
+      // If we use onloadend, we need to check the readyState.
+      reader.onloadend = function(evt) {
+        if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+          var encryptedObj = evt.target.result;
+          _importBackup(encryptedObj);
+        }
+      };
+    };
+
+    $scope.import = function(form) {
+      $scope.loading = true;
+
+      if (form.$invalid) {
+        $scope.loading = false;
+        notification.error('Error', 'There is an error in the form.');
+        return;
+      }
+
+      var backupFile = $scope.file;
+      var backupText = form.backupText.$modelValue;
+      var password = form.password.$modelValue;
+
+      if (!backupFile && !backupText) {
+        $scope.loading = false;
+        notification.error('Error', 'Please, select your backup file');
+        $scope.loading = false;
+        return;
+      }
+
+      if (backupFile) {
+        reader.readAsBinaryString(backupFile);
+      } else {
+        _importBackup(backupText);
+      }
+    };
+  });
