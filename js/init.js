ee8f78d961a3df3c27ac0830a6b0e7338f9414fc
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2014-11-25 15:09:36 -0300

    check if using cordova at init.js

--- js/init.js
+++ js/init.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.element(document).ready(function() {
-  // Init the app
-  angular.bootstrap(document, ['copayApp']);
+  var startAngular = function () {
+    angular.bootstrap(document, ['copayApp']);
+  };
+
+  if (window.cordova !== undefined) {
+    document.addEventListener('deviceready', function() {
+      setTimeout(function(){ navigator.splashscreen.hide(); }, 2000);
+      
+      startAngular();
+    });
+  } else {
+    startAngular();
+  }
+
 });
