d4e8feac40d4808a1995e37049fb49e18571d962
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2014-11-26 12:36:51 -0300

    Config for iOS. URI handler for bitcoin urls. Fix config.xml

--- js/init.js
+++ js/init.js
@@ -8,9 +8,18 @@ angular.element(document).ready(function() {
   if (window.cordova !== undefined) {
     document.addEventListener('deviceready', function() {
       setTimeout(function(){ navigator.splashscreen.hide(); }, 2000);
-      
+
+      function handleBitcoinURI(url) {
+        if (!url) return;
+        window.location = '#!/uri-payment/' + url;
+      }
+
+      window.plugins.webintent.getUri(handleBitcoinURI);
+      window.plugins.webintent.onNewIntent(handleBitcoinURI);
+      window.handleOpenURL = handleBitcoinURI;
+
       startAngular();
-    });
+    }, false);
   } else {
     startAngular();
   }
