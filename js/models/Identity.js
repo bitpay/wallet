63d6e60768256c307c2469cd9e9c0f25e06de429
Matias Alejo Garcia <ematiu@gmail.com>
2014-12-02 11:20:47 -0300

    fix Identity

--- js/models/Identity.js
+++ js/models/Identity.js
@@ -125,7 +125,7 @@ Identity.open = function(opts, cb) {
 
   var storage = opts.storage || opts.pluginManager.get('DB');
   storage.setCredentials(opts.email, opts.password, opts);
-  storage.getItem(Identity.getKeyForEmail(opts.email), function(err, data) {
+  storage.getItem(Identity.getKeyForEmail(opts.email), function(err, data, headers) {
     var exported;
     if (err) {
       return cb(err);
@@ -135,7 +135,7 @@ Identity.open = function(opts, cb) {
     } catch (e) {
       return cb(e);
     }
-    return cb(null, new Identity(_.extend(opts, exported)));
+    return cb(null, new Identity(_.extend(opts, exported)), headers);
   });
 };
 
@@ -577,6 +577,7 @@ Identity.prototype.createWallet = function(opts, cb) {
   self.wallets[w.getId()] = w;
   self.updateFocusedTimestamp(w.getId());
   self.bindWallet(w);
+  self.updateFocusedTimestamp(w.getId());
   self.storeWallet(w, function(err) {
     if (err) return cb(err);
 
@@ -643,11 +644,10 @@ Identity.prototype.deleteWallet = function(walletId, cb) {
   delete this.focusedTimestamps[walletId];
 
   this.storage.removeItem(Wallet.getStorageKey(walletId), function(err) {
-    if (err) {
-      return cb(err);
-    }
+    if (err) return cb(err);
     self.emitAndKeepAlive('deletedWallet', walletId);
     self.store(null, cb);
+    return cb();
   });
 };
 
