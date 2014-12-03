ce966eeef219de50c6722cb03cbea29f2646c11d
Matias Alejo Garcia <ematiu@gmail.com>
2014-12-03 09:24:52 -0300

    add tests to plugin.localstorage

--- js/plugins/LocalStorage.js
+++ js/plugins/LocalStorage.js
@@ -2,11 +2,16 @@
 var _ = require('lodash');
 var preconditions = require('preconditions').singleton();
 
-function LocalStorage() {
+function LocalStorage(opts) {
   this.type = 'DB';
+  opts = opts || {};
+  
 
-  preconditions.checkState(typeof localStorage !== 'undefined',
-                           'localstorage not available, cannot run plugin');
+  this.ls = opts.ls 
+    || ( (typeof localStorage !== 'undefined') ? localStorage : null );
+
+  preconditions.checkState(this.ls,
+    'localstorage not available, cannot run plugin');
 };
 
 LocalStorage.prototype.init = function() {
@@ -18,31 +23,31 @@ LocalStorage.prototype.setCredentials = function(email, password, opts) {
 };
 
 LocalStorage.prototype.getItem = function(k,cb) {
-  return cb(null, localStorage.getItem(k));
+  return cb(null, this.ls.getItem(k));
 };
 
 /**
  * Same as setItem, but fails if an item already exists
  */
 LocalStorage.prototype.createItem = function(name, value, callback) {
-  if (localStorage.getItem(name)) {
+  if (this.ls.getItem(name)) {
     return callback('EEXISTS');
   }
   return this.setItem(name, value, callback);
 };
 
 LocalStorage.prototype.setItem = function(k,v,cb) {
-  localStorage.setItem(k,v);
+  this.ls.setItem(k,v);
   return cb();
 };
 
 LocalStorage.prototype.removeItem = function(k,cb) { 
-  localStorage.removeItem(k);
+  this.ls.removeItem(k);
   return cb();
 };
 
 LocalStorage.prototype.clear = function(cb) { 
-  localStorage.clear();
+  this.ls.clear();
   return cb();
 };
 
