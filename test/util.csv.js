da89ba647be9fcf00bf038bfa87b4e1bc987f64a
Ivan Socolsky <jungans@gmail.com>
2014-12-03 18:08:44 -0300

    pointing to older moment.js version

--- test/util.csv.js
+++ test/util.csv.js
@@ -6,7 +6,7 @@ var sinon = sinon || require('sinon');
 var should = chai.should();
 
 var csv = require('../js/util/csv')
-moment = moment || require('moment');
+var moment = moment || require('moment');
 
 describe('csv utils', function() {
   it('should convert simple json', function(done) {
