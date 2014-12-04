3edec5e597a02e9b5eb93e547e25ea1cd3847402
Matias Alejo Garcia <ematiu@gmail.com>
2014-12-04 10:36:58 -0300

    simplify receive and wallet home tab

--- js/controllers/homeWallet.js
+++ js/controllers/homeWallet.js
@@ -4,13 +4,12 @@ angular.module('copayApp.controllers').controller('HomeWalletController', functi
   $scope.init = function() {
 
     $rootScope.title = 'Home';
-    $scope.addr = _.last($rootScope.wallet.getReceiveAddresses());
 
     // This is necesarry, since wallet can change in homeWallet, without running init() again.
-    $rootScope.$watch('wallet', function() {
-      if ($rootScope.wallet && $rootScope.wallet.isComplete()) {
-        $scope.addr = _.last($rootScope.wallet.getReceiveAddresses());
-      }
-    });
+    // $rootScope.$watch('wallet', function() {
+    //   if ($rootScope.wallet && $rootScope.wallet.isComplete()) {
+    //     $scope.addr = _.last($rootScope.wallet.getReceiveAddresses());
+    //   }
+    // });
   };
 });
