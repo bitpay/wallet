6659b938b5329208191c2f2b77422b5b49c45a6d
Matias Pando <mpando@bitpay.com>
2014-12-05 11:56:26 -0300

    Simple fixes

--- js/controllers/unsupported.js
+++ js/controllers/unsupported.js
@@ -2,13 +2,6 @@
 
 angular.module('copayApp.controllers').controller('UnsupportedController',
   function($scope, $location) {
-
-    var localStorage;
-    if (window.chrome && chrome.runtime && chrome.runtime.id) {
-      localStorage = chrome.storage.local;
-    } else {
-      localStorage = window.localStorage;
-    }
     if (localStorage && localStorage.length > 0) {
       $location.path('/');
     }
