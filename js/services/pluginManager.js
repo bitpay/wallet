b19f0eb8b74cdf113557c1c8b24379e068ccd519
Matias Alejo Garcia <ematiu@gmail.com>
2014-12-05 12:12:51 -0300

    angularLoad

--- js/services/pluginManager.js
+++ js/services/pluginManager.js
@@ -1,18 +1,6 @@
 'use strict';
 
-angular.module('copayApp.services').factory('pluginManager', function(angularLoad) {
+angular.module('copayApp.services').factory('pluginManager', function() {
   var pm = new copay.PluginManager(config);
-  var scripts = pm.scripts;
-
-  for(var ii in scripts){
-    var src = scripts[ii].src;
-
-    console.log('\tLoading ',src); //TODO
-    angularLoad.loadScript(src)
-      .then(scripts[ii].then || null)
-      .catch(function() {
-        throw new Error('Loading ' + src);
-      })
-  }
   return pm;
 });
