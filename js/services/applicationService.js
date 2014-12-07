eeab451bbcc7980b74e6c08db511c87c6b0d370e
Matias Alejo Garcia <ematiu@gmail.com>
2014-12-07 12:40:59 -0300

    replace href to ng-click in sidebars. add hack to close sidebars

--- js/services/applicationService.js
+++ js/services/applicationService.js
@@ -1,14 +1,13 @@
 'use strict';
 angular.module('copayApp.services')
-  .factory('applicationService', function($rootScope, $location, $timeout) {
+  .factory('applicationService', function($rootScope, $location, $timeout, go) {
     var root = {};
     var isChromeApp = window.chrome && chrome.runtime && chrome.runtime.id;
 
     root.restart = function() {
       if (1 || window.cordova !== undefined) {
         $rootScope.iden = $rootScope.wallet = undefined;
-        // NOP. no need to restart on cordova apps.
-        $location.path('/');
+        go.go('/');
         $timeout(function(){
           $rootScope.$digest();
         },1);
