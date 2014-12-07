0372c723fef292f0b092c5c1c75ec040e2924617
Matias Alejo Garcia <ematiu@gmail.com>
2014-12-06 21:07:26 -0300

    fix reload in cordova

--- js/services/applicationService.js
+++ js/services/applicationService.js
@@ -1,22 +1,28 @@
 'use strict';
 angular.module('copayApp.services')
-  .factory('applicationService', function() {
+  .factory('applicationService', function($rootScope, $location, $timeout) {
     var root = {};
     var isChromeApp = window.chrome && chrome.runtime && chrome.runtime.id;
 
     root.restart = function() {
-
-      // Go home reloading the application
-      var hashIndex = window.location.href.indexOf('#!/');
-      if (isChromeApp) {
-        chrome.runtime.reload();
+      if (window.cordova !== undefined) {
+        $rootScope.iden = $rootScope.wallet = undefined;
+        // NOP. no need to restart on cordova apps.
+        $location.path('/');
+        $timeout(function(){
+          $rootScope.$digest();
+        },1);
+      
       } else {
-        window.location = window.location.href.substr(0, hashIndex);
-      }
-    };
 
-    root.reload = function() {
-      window.location.reload();
+        // Go home reloading the application
+        var hashIndex = window.location.href.indexOf('#!/');
+        if (isChromeApp) {
+          chrome.runtime.reload();
+        } else {
+          window.location = window.location.href.substr(0, hashIndex);
+        }
+      }
     };
 
     return root;
