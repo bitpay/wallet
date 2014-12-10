71fbfe7429eaae174b862b47a4cf87d5661cf313
Matias Alejo Garcia <ematiu@gmail.com>
2014-12-10 18:18:28 -0300

    refactor tx proposal events

--- js/controllers/homeWallet.js
+++ js/controllers/homeWallet.js
@@ -2,94 +2,11 @@
 
 angular.module('copayApp.controllers').controller('HomeWalletController', function($scope, $rootScope, $timeout, $filter, $modal, rateService, notification, txStatus, identityService) {
   $scope.initHome = function() {
-    var w = $rootScope.wallet;
-
     $rootScope.title = 'Home';
-    $scope.rateService = rateService;
-    $scope.isRateAvailable = false;
-
+    var w = $rootScope.wallet;
     if (w.isShared())
       $scope.copayers = w.getRegisteredPeerIds();
-
-     w.on('txProposalEvent', _updateTxs);
-    _updateTxs();
-
-    rateService.whenAvailable(function() {
-      $scope.isRateAvailable = true;
-      $scope.$digest();
-    });
   };
-
-  // This is necessary, since wallet can change in homeWallet, 
-  // without running init() again.
-  
-  var removeWatch;
-  removeWatch = $rootScope.$watch('wallet.id', function(newWallet, oldWallet) {
-    if ($rootScope.wallet && $rootScope.wallet.isComplete() && newWallet !== oldWallet) {
-
-      if (removeWatch)
-        removeWatch();
-
-      if (oldWallet) {
-        var oldw = $rootScope.iden.getWalletById(oldWallet);
-        if (oldw)
-          oldw.removeListener('txProposalEvent', _updateTxs);
-      }
-
-
-      var w = $rootScope.wallet;
-      $rootScope.pendingTxCount = 0;
-      w.on('txProposalEvent', _updateTxs);
-      _updateTxs();
-    }
-  });
-
-    $scope.$on("$destroy", function() {
-    var w = $rootScope.wallet;
-    if (w) {
-      removeWatch();
-      w.removeListener('txProposalEvent', _updateTxs);
-    };
-  }); 
-
-  $scope.setAlternativeAmount = function(w, tx, cb) {
-    rateService.whenAvailable(function() {
-      _.each(tx.outs, function(out) {
-        var valueSat = out.valueSat * w.settings.unitToSatoshi;
-        out.alternativeAmount = $filter('noFractionNumber')(rateService.toFiat(valueSat, $scope.alternativeIsoCode), 2);
-        out.alternativeIsoCode = $scope.alternativeIsoCode;
-      });
-      if (cb) return cb(tx);
-    });
-  };
-
-  var _updateTxs = _.throttle(function() {
-    var w = $rootScope.wallet;
-    if (!w) return;
-
-    $scope.alternativeIsoCode = w.settings.alternativeIsoCode;
-    $scope.myId = w.getMyCopayerId();
-
-    var res = w.getPendingTxProposals();
-    _.each(res.txs, function(tx) {
-      $scope.setAlternativeAmount(w, tx);
-      if (tx.merchant) {
-        var url = tx.merchant.request_url;
-        var domain = /^(?:https?)?:\/\/([^\/:]+).*$/.exec(url)[1];
-        tx.merchant.domain = domain;
-      }
-      if (tx.outs) {
-        _.each(tx.outs, function(out) {
-          out.valueSat = out.value;
-          out.value = $filter('noFractionNumber')(out.value);
-        });
-      }
-    });
-    $scope.txps = res.txs;
-    $timeout(function(){
-      $scope.$digest();
-    },1)
-  }, 100);
 
   $scope.sign = function(ntxid) {
     var w = $rootScope.wallet;
@@ -97,24 +14,43 @@ angular.module('copayApp.controllers').controller('HomeWalletController', functi
     $scope.error = $scope.success = null;
     w.signAndSend(ntxid, function(err, id, status) {
       $scope.loading = false;
-      if (!txStatus.notify(status))
-        $scope.error = status;
-      _updateTxs();
+
+      if (err)
+        $scope.error = err;
+      else
+        txStatus.notify(status);
     });
   };
 
   $scope.reject = function(ntxid) {
     var w = $rootScope.wallet;
-    w.reject(ntxid);
-    txStatus.notify('txRejected');
-    _updateTxs();
+    w.reject(ntxid, function(err, status) {
+      if (err)
+        $scope.error = err;
+      else
+        txStatus.notify(status);
+    });
+  };
+
+
+  $scope.broadcast = function(ntxid) {
+    var w = $rootScope.wallet;
+    $scope.error = $scope.success = null;
+    $scope.loading = true;
+    w.issueTx(ntxid, function(err, txid, status) {
+      $scope.loading = false;
+
+      if (err)
+        $scope.error = err;
+
+      txStatus.notify(status);
+    });
   };
 
 
   $scope.openTxModal = function(tx) {
     var ModalInstanceCtrl = function($scope, $modalInstance) {
       $scope.tx = tx;
-
       $scope.getShortNetworkName = function() {
         var w = $rootScope.wallet;
         return w.getNetworkName().substring(0, 4);
@@ -131,7 +67,4 @@ angular.module('copayApp.controllers').controller('HomeWalletController', functi
       controller: ModalInstanceCtrl,
     });
   };
-
-
-
 });
