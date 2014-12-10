23f8295389646e589129997a1cc68012e03340c5
Matias Alejo Garcia <ematiu@gmail.com>
2014-12-10 00:02:12 -0300

    no html5 notifications

--- js/services/identityService.js
+++ js/services/identityService.js
@@ -1,7 +1,6 @@
 'use strict';
 angular.module('copayApp.services')
   .factory('identityService', function($rootScope, $location, $timeout, $filter, pluginManager, notification, pendingTxsService, balanceService, applicationService, go) {
-    notification.enableHtml5Mode(); // for chrome: if support, enable it
 
     // TODO:
     // * remove iden from rootScope
