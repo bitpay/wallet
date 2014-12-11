b9c51e1c4af1ce61563c00301a78b327b3a8a2b0
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2014-12-11 17:04:26 -0300

    Enable ngSwipe on mobile only. Disable ngSwipe if no identity loaded.

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('IndexController', function($scope, go) {
+angular.module('copayApp.controllers').controller('IndexController', function($scope, go, isCordova) {
   $scope.init = function() {
 
   };
 
   $scope.swipe = function(invert) {
-    go.swipe(invert);
+    if (isCordova) { 
+      go.swipe(invert);
+    }
   };
 
 });
