d9e793c8f62652cda3d13eb4281ac1e1f0abf47d
Ivan Socolsky <jungans@gmail.com>
2014-12-18 16:04:00 -0300

    give feedback to the user

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -1,12 +1,20 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('IndexController', function($scope, go, isCordova, identityService) {
+angular.module('copayApp.controllers').controller('IndexController', function($scope, go, isCordova, identityService, notification) {
   $scope.init = function() {
 
   };
 
-  $scope.resendVerificationEmail = function () {
-  	identityService.resendVerificationEmail(function () {});
+  $scope.resendVerificationEmail = function() {
+    identityService.resendVerificationEmail(function(err) {
+      if (err) {
+        notification.error('Could not send email', 'There was a problem sending the verification email.');
+        setTimeout(function() {
+          $scope.$digest();
+        }, 1);
+        return;
+      }
+    });
   };
 
   $scope.swipe = function(invert) {
