140b6a35516d4c12593c8f4d00389c4e2573fc80
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2014-12-19 16:05:52 -0300

    notify when email was re-send

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -1,19 +1,25 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('IndexController', function($scope, go, isCordova, identityService, notification) {
+angular.module('copayApp.controllers').controller('IndexController', function($scope, $timeout, go, isCordova, identityService, notification) {
   $scope.init = function() {
 
   };
 
   $scope.resendVerificationEmail = function() {
+    $scope.loading = true; 
     identityService.resendVerificationEmail(function(err) {
       if (err) {
-        notification.error('Could not send email', 'There was a problem sending the verification email.');
-        setTimeout(function() {
-          $scope.$digest();
-        }, 1);
-        return;
+        notification.error('Could not send email', 'There was a problem sending the verification email.'); 
       }
+      else {
+        notification.success('Email sent', 'Check your inbox and confirms the email');
+        $scope.hideReSendButton = true;
+      }
+      $scope.loading = null;
+      $timeout(function() {
+        $scope.$digest();
+      }, 1);
+      return;
     });
   };
 
