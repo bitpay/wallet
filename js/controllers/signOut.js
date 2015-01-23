352f8c76be0c1fc7d7ac35ce25d3acf6a1658cc7
Matias Pando <mpando@bitpay.com>
2015-01-23 10:48:46 -0300

    Remove console.log

--- js/controllers/signOut.js
+++ js/controllers/signOut.js
@@ -1,7 +1,5 @@
 angular.module('copayApp.controllers').controller('signOutController', function(identityService) {
 
-  console.log('En el controller del sign out');
-
   identityService.signout();
 
 });
