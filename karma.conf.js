74cda29f8d4d9619f06cb9f350de97304e578000
Matias Pando <mpando@bitpay.com>
2015-02-10 17:33:16 -0300

    Setting up karma coverage

--- karma.conf.js
+++ karma.conf.js
@@ -71,14 +71,19 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'js/controllers/*.js': ['coverage']
+    },
 
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
