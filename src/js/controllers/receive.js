a5f9e3167fc549d1ff988ca53670f3bb02a6c895
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2015-03-13 16:45:33 -0300

    Loading page. Setup pin page (delay, error, etc). Fix active menu. Add walletService to generate new address.

--- src/js/controllers/receive.js
+++ src/js/controllers/receive.js
@@ -1,23 +1,26 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('ReceiveController',
-  function($scope, $rootScope, $timeout, $modal, isCordova, isMobile) {
+angular.module('copayApp.controllers').controller('receiveController',
+  function($rootScope, $scope, $timeout, $modal, isCordova, isMobile, walletService, profileService) {
 
-    $scope.newAddr = function() {
-      var w = $rootScope.wallet;
-      var lastAddr = w.generateAddress(null);
-      $scope.setAddressList();
-      $scope.addr = lastAddr;
+    this.showAll = false;
+    this.isCordova = isCordova;
+
+    this.newAddr = function() {
+      var self = this;
+      walletService.createAddress(function(err, addr) {
+        self.addr = addr.address;
+      });
     };
 
-    $scope.copyAddress = function(addr) {
+    this.copyAddress = function(addr) {
       if (isCordova) {
         window.cordova.plugins.clipboard.copy('bitcoin:' + addr);
         window.plugins.toast.showShortCenter('Copied to clipboard');
       }
     };
 
-    $scope.shareAddress = function(addr) {
+    this.shareAddress = function(addr) {
       if (isCordova) {
         if (isMobile.Android() || isMobile.Windows()) {
           window.ignoreMobilePause = true;
@@ -26,34 +29,16 @@ angular.module('copayApp.controllers').controller('ReceiveController',
       }
     };
 
-    $scope.init = function() {
-      $rootScope.title = 'Receive';
-      $scope.showAll = false;
-      $scope.isCordova = isCordova;
-
-      var w = $rootScope.wallet;
-      var lastAddr = _.first(w.getAddressesOrdered());
-      var balance = w.balanceInfo.balanceByAddr;
-      $scope.setAddressList();
-
-      while (balance && balance[lastAddr] > 0) {
-        $scope.loading = true;
-        lastAddr = w.generateAddress(null);
-      };
-      $scope.loading = false;
-      $scope.addr = lastAddr;
-    };
-
-    $scope.openAddressModal = function(address) {
-      var scope = $scope;
-      var ModalInstanceCtrl = function($scope, $modalInstance, address) {
-        $scope.address = address;
-        $scope.isCordova = isCordova;
-        $scope.copyAddress = function(addr) {
+    this.openAddressModal = function(address) {
+      var self = this;
+      var ModalInstanceCtrl = function(self, $modalInstance, address) {
+        self.address = address;
+        self.isCordova = isCordova;
+        self.copyAddress = function(addr) {
           scope.copyAddress(addr);
         };
 
-        $scope.cancel = function() {
+        self.cancel = function() {
           $modalInstance.dismiss('cancel');
         };
       };
@@ -70,37 +55,10 @@ angular.module('copayApp.controllers').controller('ReceiveController',
       });
     };
 
-    $scope.toggleShowAll = function() {
-      $scope.showAll = !$scope.showAll;
-      $scope.setAddressList();
+    this.toggleShowAll = function() {
+      this.showAll = !this.showAll;
+      this.setAddressList();
     };
 
-    $scope.setAddressList = function() {
-      if ($scope.showAll) {
-        var w = $rootScope.wallet;
-        var balance = w.balanceInfo.balanceByAddr;
-
-        var addresses = w.getAddressesOrdered();
-        if (addresses) {
-          $scope.addrLength = addresses.length;
-
-          if (!$scope.showAll)
-            addresses = addresses.slice(0, 3);
-
-          var list = [];
-          _.each(addresses, function(address, index) {
-            list.push({
-              'index': index,
-              'address': address,
-              'balance': balance ? balance[address] : null,
-              'isChange': w.addressIsChange(address),
-            });
-          });
-          $scope.addresses = list;
-        }
-      } else {
-        $scope.addresses = [];
-      }
-    };
   }
 );
