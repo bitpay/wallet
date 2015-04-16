cc45d5d0811d5c4f785f1d5dc66b1a7cfb732e78
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2015-04-16 12:12:12 -0300

    Fix secret form validator

--- src/js/directives/directives.js
+++ src/js/directives/directives.js
@@ -127,7 +127,7 @@ angular.module('copayApp.directives')
       link: function(scope, elem, attrs, ctrl) {
         var validator = function(value) {
           if (value.length > 0) {
-            var m = value.match(/^[1-9A-HJ-NP-Za-km-z]{70,80}$/);
+            var m = value.match(/^[0-9A-HJ-NP-Za-km-z]{70,80}$/);
             ctrl.$setValidity('walletSecret', m ? true : false);
           }
           return value;
