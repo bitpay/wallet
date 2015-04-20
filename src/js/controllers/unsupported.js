e55efb1e772b266fbccb84a029549021ec0fc390
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2015-04-20 18:44:34 -0300

    Removes unnecessary files

--- src/js/controllers/unsupported.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('copayApp.controllers').controller('unsupportedController', function($state) {
-  if (localStorage && localStorage.length > 0) {
-    $state.go('signin');
-  }
-});
