2a5793b9cf5d39ce35b48522e691657b784606f9
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2015-04-23 12:05:26 -0300

    Fastclick again

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,12 +78,12 @@ module.exports = function(grunt) {
       },
       angular: {
         src: [
+          'bower_components/fastclick/lib/fastclick.js',
           'bower_components/qrcode-generator/js/qrcode.js',
           'bower_components/qrcode-decoder-js/lib/qrcode-decoder.js',
           'bower_components/moment/min/moment-with-locales.js',
           'bower_components/angular/angular.js',
           'bower_components/angular-ui-router/release/angular-ui-router.js',
-          'bower_components/angular-local-storage/dist/angular-local-storage.js',
           'bower_components/angular-foundation/mm-foundation.js',
           'bower_components/angular-foundation/mm-foundation-tpls.js',
           'bower_components/angular-animate/angular-animate.js',
