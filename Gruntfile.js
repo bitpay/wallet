c45e626a9299c1963fb6a0d389215357cf1fa4f8
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2015-05-04 19:23:18 -0300

    New slide up/down for different states

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,7 +84,6 @@ module.exports = function(grunt) {
           'bower_components/moment/min/moment-with-locales.js',
           'bower_components/angular/angular.js',
           'bower_components/angular-ui-router/release/angular-ui-router.js',
-          'bower_components/angular-foundation/mm-foundation.js',
           'bower_components/angular-foundation/mm-foundation-tpls.js',
           'bower_components/angular-animate/angular-animate.js',
           'bower_components/angular-moment/angular-moment.js',
