652f973454b448b3f5d85f320b15ebeb7f8a6a81
Matias Alejo Garcia <ematiu@gmail.com>
2015-05-08 12:02:11 -0300

    no future transactions

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -334,6 +334,9 @@ angular.module('copayApp.controllers').controller('indexController', function($r
     self.txHistoryPaging = txs[self.limitHistory] ? true : false;
     lodash.each(txs, function(tx) {
       tx.ts = tx.minedTs || tx.sentTs;
+      // no future transaction...
+      if (tx.ts > now) 
+        ts.ts = now;
       tx.rateTs = Math.floor((tx.ts || now) / 1000);
       tx.amountStr = profileService.formatAmount(tx.amount); //$filter('noFractionNumber')(
       if (c < self.limitHistory) {
