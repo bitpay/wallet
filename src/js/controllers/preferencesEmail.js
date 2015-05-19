837f6fced1eca4ae7d12a1d5acd26141093d8534
Matias Alejo Garcia <ematiu@gmail.com>
2015-05-19 14:10:47 -0300

    update to current BWC

--- src/js/controllers/preferencesEmail.js
+++ src/js/controllers/preferencesEmail.js
@@ -1,26 +1,29 @@
 'use strict';
 
 angular.module('copayApp.controllers').controller('preferencesEmailController',
-  function($scope, go,  profileService ,gettext, $log) {
+  function($scope, go, profileService, gettext, $log) {
     this.save = function(form) {
-      this.error=null;
+      this.error = null;
 
-      if (!form.$valid) {
+      if (!form.$valid && this.email) {
         this.error = gettext('Invalid email');
         return;
       }
 
-console.log('[preferencesEmail.js.12]', this.email); //TODO
       var fc = profileService.focusedClient;
-      this.saving =true;
-      fc.savePreferences({email:this.email}, function(err) {
-        fc.saving =false;
+      this.saving = true;
+      fc.savePreferences({
+        email: this.email
+      }, function(err) {
+        fc.saving = false;
         if (err) {
           $log.warn(err);
           $scope.$emit('Local/ClientError', err);
           return;
         }
-        go.walletHome();
+        $scope.$emit('Local/EmailUpdated', function(err){
+          go.path('preferences');
+        });
       });
     };
 
