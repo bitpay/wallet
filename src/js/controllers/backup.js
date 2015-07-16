2c6c576c5bed7742a2bb4420d1a331faea7f7729
Matias Alejo Garcia <ematiu@gmail.com>
2015-07-15 22:10:59 -0300

    export backup without signing capabilities

--- src/js/controllers/backup.js
+++ src/js/controllers/backup.js
@@ -11,7 +11,15 @@ angular.module('copayApp.controllers').controller('backupController',
     this.isEncrypted = fc.isPrivKeyEncrypted();
 
     this.downloadWalletBackup = function() {
-      backupService.walletDownload(this.password, function() {
+      var self = this;
+      var opts = {
+        noSign: $scope.noSign,
+      };
+      backupService.walletDownload(this.password, opts, function(err) {
+        if (err) {
+          self.error = true;
+          return ;
+        }
         $rootScope.$emit('Local/BackupDone');
         notification.success(gettext('Backup created'), gettext('Encrypted backup file saved'));
         go.walletHome();
@@ -19,19 +27,32 @@ angular.module('copayApp.controllers').controller('backupController',
     };
 
     this.getBackup = function() {
-      return backupService.walletExport(this.password);
+      var opts = {
+        noSign: $scope.noSign,
+      };
+
+      var ew = backupService.walletExport(this.password, opts);
+      if (!ew) {
+        this.error = true;
+      } else {
+        this.error = false;
+      }
+      return ew;
     };
 
     this.viewWalletBackup = function() {
       var self = this;
       $timeout(function() {
-        self.backupWalletPlainText = self.getBackup();
+        var ew = self.getBackup();
+        if (!ew) return;
+        self.backupWalletPlainText = ew;
         $rootScope.$emit('Local/BackupDone');
       }, 100);
     };
 
     this.copyWalletBackup = function() {
       var ew = this.getBackup();
+      if (!ew) return;
       window.cordova.plugins.clipboard.copy(ew);
       window.plugins.toast.showShortCenter('Copied to clipboard');
       $rootScope.$emit('Local/BackupDone');
@@ -48,6 +69,11 @@ angular.module('copayApp.controllers').controller('backupController',
         name = fc.alias + ' [' + name + ']';
       }
       var ew = this.getBackup();
+      if (!ew) return;
+
+      if( $scope.noSign) 
+        name = name + '(No Private Key)';
+
       var properties = {
         subject: 'Copay Wallet Backup: ' + name,
         body: 'Here is the encrypted backup of the wallet ' + name + ': \n\n' + ew + '\n\n To import this backup, copy all text between {...}, including the symbols {}',
