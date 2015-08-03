53842f211b478d5ccf68f147a8970267a5605683
Kosta Korenkov <7r0ggy@gmail.com>
2015-08-03 14:21:12 +0300

    Store the whole addon spec in addonManager

--- src/js/services/addonManager.js
+++ src/js/services/addonManager.js
@@ -1,23 +1,25 @@
 'use strict';
 
-angular.module('copayApp.services').provider('addonManager', function () {
-  var addonMenuItems = [];
-  var addonViews = [];
+angular.module('copayApp.services').provider('addonManager', function (lodash) {
+  var addons = [];
 
   this.registerAddon = function(addonSpec) {
-    addonMenuItems.push(addonSpec.menuItem);
-    addonViews.push(addonSpec.view);
+    addons.push(addonSpec);
   };
 
   this.$get = function() {
     var manager = {};
 
     manager.addonMenuItems = function() {
-      return addonMenuItems;
+      return lodash.map(addons, function(addonSpec) {
+        return addonSpec.menuItem;
+      });
     };
 
     manager.addonViews = function() {
-      return addonViews;
+      return lodash.map(addons, function(addonSpec) {
+        return addonSpec.view;
+      });
     };
 
     return manager;
