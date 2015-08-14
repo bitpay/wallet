f7b2b6af9d725c63df8eda083f9ccced7f45d66e
Matias Alejo Garcia <ematiu@gmail.com>
2015-08-14 14:13:53 -0300

    skips scan if wallet is incomplete with changing bws server

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -784,6 +784,7 @@ angular.module('copayApp.controllers').controller('indexController', function($r
 
   self.startScan = function(walletId) {
     var c = profileService.walletClients[walletId];
+    if (!c.isComplete()) return;
 
     if (self.walletId == walletId)
       self.setOngoingProcess('scanning', true);
