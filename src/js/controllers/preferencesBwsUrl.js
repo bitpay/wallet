83b2889b4a6b8879d42b0deba98de7a4e004c9b6
Matias Alejo Garcia <ematiu@gmail.com>
2015-09-08 09:01:49 -0300

    add devel aliases for BWS urls

--- src/js/controllers/preferencesBwsUrl.js
+++ src/js/controllers/preferencesBwsUrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('copayApp.controllers').controller('preferencesBwsUrlController',
-  function($scope, configService, isMobile, isCordova, go,  applicationService ) {
+  function($scope,$log, configService, isMobile, isCordova, go,  applicationService ) {
     this.isSafari = isMobile.Safari();
     this.isCordova = isCordova;
     this.error = null;
@@ -12,6 +12,27 @@ angular.module('copayApp.controllers').controller('preferencesBwsUrlController',
     this.bwsurl = config.bws.url;
 
     this.save = function() {
+
+      var bws;
+      switch (this.bwsurl) {
+        case 'prod':
+        case 'production':
+          bws = 'https://bws.bitpay.com/bws/api'
+          break;
+        case 'sta':
+        case 'staging':
+          bws = 'https://bws-staging.b-pay.net/bws/api'
+          break;
+        case 'loc':
+        case 'local':
+          bws = 'http://localhost:3232/bws/api'
+          break;
+      };
+      if (bws) {
+        $log.info('Using BWS URL Alias to ' + bws);
+        this.bwsurl = bws;
+      }
+
       var opts = {
         bws: {
           url: this.bwsurl,
