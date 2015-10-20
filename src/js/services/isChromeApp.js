39dd0c1f4aeb873908ce2dbe4cd94c33371d6bf8
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2015-10-20 12:54:37 -0300

    Fix

--- src/js/services/isChromeApp.js
+++ src/js/services/isChromeApp.js
@@ -1,5 +1,5 @@
 'use strict';
 
-angular.module('copayApp.services').factory('isChromeApp', function nodeWebkitFactory(nodeWebkit) {
+angular.module('copayApp.services').factory('isChromeApp', function(nodeWebkit) {
   return !!(window.chrome && chrome.runtime && chrome.runtime.id && !nodeWebkit.isDefined());
 });
