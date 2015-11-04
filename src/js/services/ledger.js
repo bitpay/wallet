8dcabbf027286c2310d696a0768d2693d3e6a3d6
Matias Alejo Garcia <ematiu@gmail.com>
2015-11-04 15:45:33 -0300

    import working

--- src/js/services/ledger.js
+++ src/js/services/ledger.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('copayApp.services')
-  .factory('ledger', function($log, bwcService, gettext) {
+  .factory('ledger', function($log, bwcService, gettext, hwWallet) {
     var root = {};
     var LEDGER_CHROME_ID = "kkdpmhnladdopljabkgpacgpliggeeaf";
 
