b1d4e6224cc104a28c336dc07973d3817ac64052
Gabriel Bazán <gabrielbazan7@hotmail.com>
2015-11-16 12:00:10 -0300

    license and profile creation in the same view

--- src/js/controllers/disclaimer.js
+++ src/js/controllers/disclaimer.js
@@ -1,29 +1,44 @@
 'use strict';
 
 angular.module('copayApp.controllers').controller('disclaimerController',
-  function($scope, $timeout, storageService, applicationService, gettextCatalog, isCordova, uxLanguage) {
+  function($scope, $timeout, $log, profileService, isCordova, storageService, gettextCatalog, uxLanguage, go) {
 
-    $scope.agree = function() {
+    $scope.create = function(noWallet) {
+      $scope.creatingProfile = true;
       if (isCordova) {
         window.plugins.spinnerDialog.show(null, gettextCatalog.getString('Loading...'), true);
       }
       $scope.loading = true;
       $timeout(function() {
         storageService.setCopayDisclaimerFlag(function(err) {
-          $timeout(function() {
-            if (isCordova) {
-              window.plugins.spinnerDialog.hide();
+          if (isCordova) {
+            window.plugins.spinnerDialog.hide();
+          }
+          profileService.create({
+            noWallet: noWallet
+          }, function(err) {
+            if (err) {
+              $scope.creatingProfile = false;
+              $log.warn(err);
+              $scope.error = err;
+              $scope.$apply();
+              $timeout(function() {
+                $scope.create(noWallet);
+              }, 3000);
             }
-            applicationService.restart();
-          }, 1000);
+          });
         });
       }, 100);
     };
-    
+
     $scope.init = function() {
       storageService.getCopayDisclaimerFlag(function(err, val) {
         $scope.lang = uxLanguage.currentLanguage;
         $scope.agreed = val;
+
+        if (profileService.profile) {
+          go.walletHome();
+        }
         $timeout(function() {
           $scope.$digest();
         }, 1);
