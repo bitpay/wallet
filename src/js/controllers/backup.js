3cd14985125631011252a875f9ba97ed5f180af2
Javier <javier@bitpay.com>
2015-11-20 14:38:29 -0300

    one view/controller

--- src/js/controllers/backup.js
+++ src/js/controllers/backup.js
@@ -1,11 +1,21 @@
 'use strict';
 
 angular.module('copayApp.controllers').controller('backupController',
-  function($rootScope, $scope, $timeout, $log, $compile, lodash, profileService, go, gettext, confirmDialog, notification, bwsError) {
+  function($rootScope, $scope, $timeout, $log, $state, $compile, go, lodash, profileService, bwcService, bwsError) {
 
     var self = this;
     var fc = profileService.focusedClient;
+    var customWords = [];
+    var mnemonic = null;
+    self.xPrivKey = null;
+    self.step1 = true;
+    self.step2 = false;
+    self.step3 = false;
+    self.step4 = false;
     self.deleted = false;
+    self.credentialsEncrypted = false;
+    self.selectComplete = false;
+    self.backupNoOk = false;
 
     if (fc.credentials && !fc.credentials.mnemonicEncrypted && !fc.credentials.mnemonic)
       self.deleted = true;
@@ -14,7 +24,52 @@ angular.module('copayApp.controllers').controller('backupController',
       self.credentialsEncrypted = true;
       passwordRequest();
     } else
-      setWords(fc.getMnemonic());
+      setWords(getMnemonic());
+
+    function getMnemonic() {
+      mnemonic = fc.getMnemonic();
+      self.xPrivKey = fc.credentials.xPrivKey;
+      profileService.lockFC();
+      return mnemonic;
+    }
+
+    self.goToStep2 = function() {
+      self.step1 = false;
+      self.step2 = true;
+      self.step3 = false;
+      self.step4 = false;
+      $timeout(function() {
+        $scope.$apply();
+      }, 1);
+    }
+
+    self.goToStep3 = function() {
+      if (self.mnemonicHasPassphrase) {
+        self.step1 = false;
+        self.step2 = false;
+        self.step3 = true;
+        self.step4 = false;
+      } else {
+        self.step1 = false;
+        self.step2 = false;
+        self.step3 = false;
+        self.step4 = true;
+      }
+
+      $timeout(function() {
+        $scope.$apply();
+      }, 1);
+    }
+
+    self.goToStep4 = function() {
+      self.step1 = false;
+      self.step2 = false;
+      self.step3 = false;
+      self.step4 = true;
+      $timeout(function() {
+        $scope.$apply();
+      }, 1);
+    }
 
     function setWords(words) {
       if (words) {
@@ -24,9 +79,20 @@ angular.module('copayApp.controllers').controller('backupController',
       }
     };
 
+    self.toggle = function() {
+      self.error = "";
+
+      if (self.credentialsEncrypted)
+        passwordRequest();
+
+      $timeout(function() {
+        $scope.$apply();
+      }, 1);
+    };
+
     function passwordRequest() {
       try {
-        setWords(fc.getMnemonic());
+        setWords(getMnemonic());
       } catch (e) {
         if (e.message && e.message.match(/encrypted/) && fc.isPrivKeyEncrypted()) {
 
@@ -42,9 +108,94 @@ angular.module('copayApp.controllers').controller('backupController',
             }
 
             self.credentialsEncrypted = false;
-            setWords(fc.getMnemonic());
+            setWords(getMnemonic());
+
+            $timeout(function() {
+              $scope.$apply();
+            }, 1);
           });
         }
+      }
+    }
+
+    self.enableButton = function(word) {
+      document.getElementById(word).disabled = false;
+      lodash.remove(customWords, function(v) {
+        return v == word;
+      });
+    }
+
+    self.disableButton = function(word) {
+      document.getElementById(word).disabled = true;
+      customWords.push(word);
+      self.addButton(word);
+    }
+
+    self.addButton = function(word) {
+      var btnhtml = '<button class="button radius tiny"' +
+        'ng-style="{\'background-color\':index.backgroundColor}"' +
+        'data-ng-click="wordsC.removeButton($event)" id="_' + word + '" > ' + word + ' </button>';
+      var temp = $compile(btnhtml)($scope);
+      angular.element(document.getElementById('addWord')).append(temp);
+      self.shouldContinue();
+    }
+
+    self.removeButton = function(event) {
+      var id = (event.target.id);
+      var element = document.getElementById(id);
+      element.remove();
+      self.enableButton(id.substring(1));
+      self.shouldContinue();
+    }
+
+    self.shouldContinue = function() {
+      if (customWords.length == 12)
+        self.selectComplete = true;
+      else
+        self.selectComplete = false;
+    }
+
+    self.confirm = function() {
+      self.backupNoOk = false;
+
+      var walletClient = bwcService.getClient();
+
+      try {
+        var formatedCustomWords = '';
+
+        lodash.each(customWords, function(d) {
+          return formatedCustomWords += ' ' + d;
+        });
+
+        var passphrase = $scope.passphrase || '';
+
+        walletClient.seedFromMnemonic(formatedCustomWords.trim(), {
+          network: fc.credentials.network,
+          passphrase: passphrase,
+          account: fc.credentials.account
+        })
+
+        if (walletClient.credentials.xPrivKey != self.xPrivKey)
+          throw 'Private key mismatch';
+
+      } catch (err) {
+        $log.debug('Failed to verify backup: ', err);
+        self.backupNoOk = true;
+        $timeout(function() {
+          $scope.$apply();
+        }, 1);
+        return;
+      }
+
+      console.log('OK');
+
+      var words = lodash.map(mnemonic.split(' '), function(d) {
+        return d;
+      });
+
+      if (lodash.isEqual(words, customWords)) {
+        $rootScope.$emit('Local/BackupDone');
+        go.walletHome();
       }
     }
   });
