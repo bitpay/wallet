a05d4a0cff1e795b45b7840289c8e5aa9e177932
Gabriel Bazán <gabrielbazan7@hotmail.com>
2015-12-23 12:35:47 -0300

    refactor

--- src/js/services/pushNotificationsService.js
+++ src/js/services/pushNotificationsService.js
@@ -3,8 +3,6 @@
    .factory('pushNotificationsService', function($http) {
      var root = {};
 
-     //"APA91bHCysW7fzE_ks5HPOu2BGr0G7T-aD5SPfzaSGIKuhp82gFUcopVPPck8EfnxgHyK_3QJ9FdFS8H2mjAILDv3jdrH6slJzmCwoszya9XdJz4Uv-cxkLDYXb7z086TD3uPgSLnMUO"
-
      root.subscribe = function(opts) {
        return $http.post('http://192.168.1.120:8000/subscribe', opts);
      }
