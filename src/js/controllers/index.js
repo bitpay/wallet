dfda50a1bc13bfb45ca8ecae01078e0e04997698
Matias Alejo Garcia <ematiu@gmail.com>
2016-01-20 10:49:34 -0300

    display error is touch id failed

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -1435,7 +1435,7 @@ angular.module('copayApp.controllers').controller('indexController', function($r
       },
       function(msg) {
         // ERROR
-        return cb(gettext('Invalid Touch ID'));
+        return cb(gettext('Error with Touch ID:') + msg);
       }
     );
   });
