8d153569885e81e9f0e326bae5bddd898c2d94c7
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-01-28 16:49:31 -0300

    Ref. Waiting for Copayers

--- src/js/controllers/copayers.js
+++ src/js/controllers/copayers.js
@@ -16,8 +16,6 @@ angular.module('copayApp.controllers').controller('copayersController',
         go.walletHome();
         return;
       }
-      self.loading = false;
-      self.isCordova = isCordova;
     };
 
     var _modalDeleteWallet = function() {
@@ -55,25 +53,23 @@ angular.module('copayApp.controllers').controller('copayersController',
 
     var _deleteWallet = function() {
       var fc = profileService.focusedClient;
-      $timeout(function() {
-        var fc = profileService.focusedClient;
-        var walletName = fc.credentials.walletName;
-
-        profileService.deleteWalletFC({}, function(err) {
-          if (err) {
-            this.error = err.message || err;
-            console.log(err);
-            $timeout(function() {
-              $scope.$digest();
-            });
-          } else {
-            go.walletHome();
-            $timeout(function() {
-              notification.success(gettextCatalog.getString('Success'), gettextCatalog.getString('The wallet "{{walletName}}" was deleted', {walletName: walletName}));
-            });
-          }
-        });
-      }, 100);
+      var walletName = fc.credentials.walletName;
+      profileService.deleteWalletFC({}, function(err) {
+        if (err) {
+          self.error = err.message || err;
+          $timeout(function() {
+            $scope.$digest();
+          });
+        } else {
+          go.walletHome();
+          $timeout(function() {
+            notification.success(
+                gettextCatalog.getString('Success'), 
+                gettextCatalog.getString('The wallet "{{walletName}}" was deleted', {walletName: walletName})
+            );
+          });
+        }
+      });
     };
 
     self.deleteWallet = function() {
