f6ba6115467b9002b69afc5afd389b2273f86bf5
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-01-29 17:00:57 -0300

    Support new proposals with array of outputs

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -517,7 +517,6 @@ angular.module('copayApp.controllers').controller('indexController', function($r
       message: 'test multi-output',
       fee: 1000,
       createdOn: new Date() / 1000,
-      type: 'multiple_output',
       outputs: []
     };
     function addOutput(n) {
