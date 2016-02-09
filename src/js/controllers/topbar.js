f3c97288900ab474f1524fb0289f09f6239097d4
Javier <javier@bitpay.com>
2016-02-09 15:10:25 -0300

    rename variables/event

--- src/js/controllers/topbar.js
+++ src/js/controllers/topbar.js
@@ -1,12 +1,14 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('topbarController', function(go) { 
+angular.module('copayApp.controllers').controller('topbarController', function(go, $rootScope) {
 
   this.goHome = function() {
+    $rootScope.$emit('Local/UpdateTxHistory');
     go.walletHome();
   };
 
   this.goPreferences = function() {
+    $rootScope.$emit('Local/UpdateTxHistory');
     go.preferences();
   };
 
