a77d817e5a8bfc27bd598409c93404a068798013
Andy Phillipson <andy@bitpay.com>
2016-02-16 16:56:14 -0500

    Correction for #3692, cloneDeep() must be used otherwise defaultConfig gets overwritten.

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -105,7 +105,7 @@ angular.module('copayApp.services').factory('configService', function(storageSer
   };
 
   root.set = function(newOpts, cb) {
-    var config = lodash.clone(defaultConfig);
+    var config = lodash.cloneDeep(defaultConfig);
     storageService.getConfig(function(err, oldOpts) {
       if (lodash.isString(oldOpts)) {
         oldOpts = JSON.parse(oldOpts);
