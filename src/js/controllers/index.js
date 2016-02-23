816b60b77642675b0ccc03e9b637bd553d7ba334
Gabriel Bazán <gabrielbazan7@hotmail.com>
2016-02-23 17:33:41 -0300

    fix no addressTo in txs

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -953,7 +953,7 @@ angular.module('copayApp.controllers').controller('indexController', function($r
 
       function computeSearchableString(tx){
         var addrbook = '';
-        if(self.addressbook[tx.addressTo]!= 'undefined') addrbook = self.addressbook[tx.addressTo] || '';
+        if(tx.addressTo && self.addressbook[tx.addressTo]!= 'undefined') addrbook = self.addressbook[tx.addressTo] || '';
         var searchableDate = computeSearchableDate(new Date(tx.time * 1000));
         var message = tx.message ? tx.message : '';
         var addressTo = tx.addressTo ? tx.addressTo : '';
