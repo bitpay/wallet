1a2fc465260500b9a8c7f56cf06e3ada3f766f16
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-02-29 18:14:47 -0300

    Adds more fee data to the confirmation popup

--- src/js/controllers/confirmTx.js
+++ src/js/controllers/confirmTx.js
@@ -1,6 +1,16 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('confirmTxController', function() {
+angular.module('copayApp.controllers').controller('confirmTxController', function(configService, feeService, rateService) {
+
+
+  this.processFee = function(amount, fee) {
+    var walletSettings = configService.getSync().wallet.settings;
+    var feeAlternativeIsoCode = walletSettings.alternativeIsoCode;
+
+    this.feeLevel = feeService.feeOpts[feeService.getCurrentFeeLevel()];
+    this.feeAlternativeStr = parseFloat((rateService.toFiat(fee, feeAlternativeIsoCode)).toFixed(2), 10) + ' ' + feeAlternativeIsoCode;
+    this.feeRateStr = (fee / amount * 100).toFixed(2) + '%' ;
+  };
 
   this.close = function(cb) {
     return cb();
