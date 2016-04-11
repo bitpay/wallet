59cc77e267ce0fd2d2d71de6481fe5c2d17bb078
Gabriel Bazán <gabrielbazan7@hotmail.com>
2016-04-11 16:07:37 -0300

    fix cordova is not define

--- src/js/controllers/preferencesGlobal.js
+++ src/js/controllers/preferencesGlobal.js
@@ -16,7 +16,7 @@ angular.module('copayApp.controllers').controller('preferencesGlobalController',
       this.usePushNotifications = isCordova && !isMobile.Windows();
       $scope.PNEnabledByUser = true;
       $scope.isIOS = isMobile.iOS();
-      if (!typeof cordova.plugins.diagnostic != undefined && $scope.isIOS) {
+      if ($scope.isIOS && !typeof cordova.plugins.diagnostic != undefined) {
         cordova.plugins.diagnostic.isRemoteNotificationsEnabled(function(isEnabled) {
           $scope.PNEnabledByUser = isEnabled;
         });
