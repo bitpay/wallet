ade2508b1b179ceb476cf6456eead492e74809ef
Marco Polci <mpolci@users.noreply.github.com>
2016-04-19 17:42:19 +0200

    Add error callback  to getFile() call (#4092)

--- src/js/services/fileStorage.js
+++ src/js/services/fileStorage.js
@@ -116,8 +116,8 @@ angular.module('copayApp.services')
           fileEntry.remove(function() {
             console.log('File removed.');
             return cb();
-          }, cb, cb);
-        });
+          }, cb);
+        }, cb);
       });
     };
 
