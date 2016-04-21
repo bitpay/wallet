1390c4534e1b08736237c45ace746e263ba9fc30
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-04-21 13:34:46 -0300

    Fix camera permissions for Android 6

--- src/js/directives/qrScanner.js
+++ src/js/directives/qrScanner.js
@@ -30,7 +30,7 @@ angular.module('copayApp.directives')
             window.ignoreMobilePause = true;
             window.plugins.spinnerDialog.show(null, gettextCatalog.getString('Preparing camera...'), true);
             $timeout(function() {
-              if (!isMobile.Windows()) {
+              if (isMobile.iOS()) {
                 cloudSky.zBar.scan({}, onSuccess, onError);
               } else {
                 cordova.plugins.barcodeScanner.scan(onSuccess, onError);
