4a59ec9085c59a43bda4ab7fe7fd621a3a0d6e57
Braydon Fuller <courier@braydon.com>
2016-04-21 14:50:20 -0400

    controllers: typo in walletHome

--- src/js/controllers/walletHome.js
+++ src/js/controllers/walletHome.js
@@ -1284,7 +1284,7 @@ angular.module('copayApp.controllers').controller('walletHomeController', functi
         }
 
         if (resp.amount == 0) {
-          self.error = gettext("Not enought funds for fee");
+          self.error = gettext("Not enough funds for fee");
           $scope.$apply();
           return;
         }
