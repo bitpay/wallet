d248f2dcd206978aae0e29bc545243b535a81147
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-04-21 15:00:10 -0300

    Bug/android camera (#4106)
    * Fix android camera
    
    * New release v1.10.4

--- src/js/directives/qrScanner.js
+++ src/js/directives/qrScanner.js
@@ -14,7 +14,7 @@ angular.module('copayApp.directives')
             if (isMobile.Windows() && result.cancelled) return;
 
             $timeout(function() {
-              var data = isMobile.Windows() ? result.text : result;
+              var data = isMobile.iOS() ? result : result.text;
               $scope.onScan({ data: data });
             }, 1000);
           };
