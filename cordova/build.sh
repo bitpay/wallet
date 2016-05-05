a165e80a34613b16ebc5fa29908422848bcee581
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-05-05 12:09:28 -0300

    Fix duplicated entry

--- cordova/build.sh
+++ cordova/build.sh
@@ -79,9 +79,6 @@ if [ ! -d $PROJECT ]; then
 
   echo "${OpenColor}${Green}* Installing plugins... ${CloseColor}"
 
-  cordova plugin add cordova-plugin-disable-bitcode
-  checkOK
-
   cordova plugin add https://github.com/florentvaldelievre/virtualartifacts-webIntent.git
   checkOK
 
