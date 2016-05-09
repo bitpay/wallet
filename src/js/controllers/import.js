7d30b596404c21ca75709256931f525a0ccfb65d
Kirvx <Kirvx@tutanota.com>
2016-05-09 15:52:23 +0200

    Seed -> Recovery Phrase ; Passphrase -> Password ; Password -> Spending Password

--- src/js/controllers/import.js
+++ src/js/controllers/import.js
@@ -51,7 +51,7 @@ angular.module('copayApp.controllers').controller('importController',
       try {
         str2 = sjcl.decrypt(self.password, str);
       } catch (e) {
-        err = gettext('Could not decrypt file, check your password');
+        err = gettext('Could not decrypt file, check your spending password');
         $log.warn(e);
       };
 
@@ -182,14 +182,14 @@ console.log('[import.js.84:xPrivKey:]',xPrivKey); //TODO
       this.error = null;
 
       if (!words) {
-        this.error = gettext('Please enter the seed words');
+        this.error = gettext('Please enter the recovery phrase');
       } else if (words.indexOf('xprv') == 0 || words.indexOf('tprv') == 0) {
         return _importExtendedPrivateKey(words, opts);
       } else {
         var wordList = words.split(/[\u3000\s]+/);
 
         if ((wordList.length % 3) != 0)
-          this.error = gettext('Wrong number of seed words:') + wordList.length;
+          this.error = gettext('Wrong number of recovery words:') + wordList.length;
       }
 
       if (this.error) {
