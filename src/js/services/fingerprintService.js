2668b11b5b7e3e15d68769da64259ea6bd5e6578
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-05-19 17:39:34 -0300

    Fix fingerprint service

--- src/js/services/fingerprintService.js
+++ src/js/services/fingerprintService.js
@@ -29,7 +29,7 @@ angular.module('copayApp.services').factory('fingerprintService', function(gette
   };
 
   root.check = function(client, cb) {
-    if (root.isAvailable()) {
+    if (root.isAvailable(client)) {
       requestTouchId(cb);
     } else {
       return cb();
