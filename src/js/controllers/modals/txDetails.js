c266d9a8ca0ca043c1a3e9f74f1a7b1273b2a04d
Gabriel Bazán <gabrielbazan7@hotmail.com>
2016-05-27 15:06:41 -0300

    tx-details refactor

--- src/js/controllers/modals/txDetails.js
+++ src/js/controllers/modals/txDetails.js
@@ -1,53 +1,50 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('txDetailsController', function($scope, $rootScope, $log, $filter, profileService, configService) {
+angular.module('copayApp.controllers').controller('txDetailsController', function($rootScope, $scope, $filter, profileService, nodeWebkit, configService,  gettextCatalog) {
 
-  var self = $scope.self;
-  var fc = profileService.focusedClient;
+    var self = $scope.self;
+    var fc = profileService.focusedClient;
+    var config = configService.getSync();
+    var configWallet = config.wallet;
+    var walletSettings = configWallet.settings;
 
-  $scope.settings = configService.getSync();
-  $scope.copayerId = fc.credentials.copayerId;
-  $scope.isShared = fc.credentials.n > 1;
+    $scope.alternativeIsoCode = walletSettings.alternativeIsoCode;
+    $scope.color = fc.backgroundColor;
+    $scope.copayerId = fc.credentials.copayerId;
+    $scope.isShared = fc.credentials.n > 1;
 
-  $scope.getAlternativeAmount = function() {
-    var satToBtc = 1 / 100000000;
-    fc.getFiatRate({
-      code: self.alternativeIsoCode,
-      ts: $scope.btx.time * 1000
-    }, function(err, res) {
-      if (err) {
-        $log.debug('Could not get historic rate');
-        return;
-      }
-      if (res && res.rate) {
-        var alternativeAmountBtc = ($scope.btx.amount * satToBtc).toFixed(8);
-        $scope.rateDate = res.fetchedOn;
-        $scope.rateStr = res.rate + ' ' + self.alternativeIsoCode;
-        $scope.alternativeAmountStr = $filter('noFractionNumber')(alternativeAmountBtc * res.rate, 2) + ' ' + self.alternativeIsoCode;
-        $scope.$apply();
-      }
-    });
-  };
+    $scope.getAlternativeAmount = function() {
+      var satToBtc = 1 / 100000000;
+      fc.getFiatRate({
+        code: $scope.alternativeIsoCode,
+        ts: $scope.btx.time * 1000
+      }, function(err, res) {
+        if (err) {
+          $log.debug('Could not get historic rate');
+          return;
+        }
+        if (res && res.rate) {
+          var alternativeAmountBtc = ($scope.btx.amount * satToBtc).toFixed(8);
+          $scope.rateDate = res.fetchedOn;
+          $scope.rateStr = res.rate + ' ' + $scope.alternativeIsoCode;
+          $scope.alternativeAmountStr = $filter('noFractionNumber')(alternativeAmountBtc * res.rate, 2) + ' ' + $scope.alternativeIsoCode;
+          $scope.$apply();
+        }
+      });
+    };
 
-  $scope.getAmount = function(amount) {
-    return self.getAmount(amount);
-  };
+    $scope.getShortNetworkName = function() {
+      var n = fc.credentials.network;
+      return n.substring(0, 4);
+    };
 
-  $scope.getUnitName = function() {
-    return self.getUnitName();
-  };
+    $scope.copyToClipboard = function(addr) {
+      if (!addr) return;
+      self.copyToClipboard(addr);
+    };
 
-  $scope.getShortNetworkName = function() {
-    var n = fc.credentials.network;
-    return n.substring(0, 4);
-  };
+    $scope.cancel = function() {
+      $scope.txDetailsModal.hide();
+    };
 
-  $scope.copyToClipboard = function(addr) {
-    if (!addr) return;
-    self.copyToClipboard(addr);
-  };
-
-  $scope.cancel = function() {
-    $scope.txDetailsModal.hide();
-  };
-});
+  });
