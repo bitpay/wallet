a1644a32d8d8adef93eb42fb801152e0732d065e
Matias Alejo Garcia <ematiu@gmail.com>
2016-05-31 18:56:02 -0300

    fixes in controllers

--- src/js/controllers/import.js
+++ src/js/controllers/import.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('copayApp.controllers').controller('importController',
-  function($scope, $rootScope, $location, $timeout, $log, profileService, configService, notification, go, sjcl, gettext, lodash, ledger, trezor, derivationPathHelper) {
+  function($scope, $rootScope, $location, $timeout, $log, profileService, configService, notification, go, sjcl, gettext, lodash, ledger, trezor, derivationPathHelper, platformInfo) {
 
     var isChromeApp = platformInfo.isChromeApp;
     var isDevel = platformInfo.isDevel;
