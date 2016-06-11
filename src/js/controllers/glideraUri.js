ecf16d61ea8c3bdc5dfafd84b7b3a86d34bfb809
Matias Alejo Garcia <ematiu@gmail.com>
2016-06-11 18:28:31 -0300

    Glidera working again. Rm ignoreMobilePause!

--- src/js/controllers/glideraUri.js
+++ src/js/controllers/glideraUri.js
@@ -1,40 +1,43 @@
 'use strict';
 angular.module('copayApp.controllers').controller('glideraUriController',
-  function($scope, $stateParams, $timeout, profileService, configService, glideraService, storageService, go) { 
+function($scope, $log, $stateParams, $timeout, profileService, configService, glideraService, storageService, go) {
 
-    this.submitOauthCode = function(code) {
-console.log('[glideraUri.js.5:code:]'+code); //TODO
-      var self = this;
-      var glideraTestnet = configService.getSync().glidera.testnet;
-      var network = glideraTestnet ? 'testnet' : 'livenet';
-      this.loading = true;
-      this.error = null;
-      $timeout(function() {
-        glideraService.getToken(code, function(err, data) {
-          self.loading = null;
-          if (err) {
-            self.error = err;
+  this.submitOauthCode = function(code) {
+    $log.debug('Glidera Oauth Code:' + code); 
+    var self = this;
+    var glideraTestnet = configService.getSync().glidera.testnet;
+    var network = glideraTestnet ? 'testnet' : 'livenet';
+    this.loading = true;
+    this.error = null;
+    $timeout(function() {
+      glideraService.getToken(code, function(err, data) {
+        self.loading = null;
+        if (err) {
+          self.error = err;
+          $timeout(function() {
+            $scope.$apply();
+          }, 100);
+        } else if (data && data.access_token) {
+          storageService.setGlideraToken(network, data.access_token, function() {
+            $scope.$emit('Local/GlideraUpdated', data.access_token);
             $timeout(function() {
-                $scope.$apply();
-              }, 100);
-          }
-          else if (data && data.access_token) {
-            storageService.setGlideraToken(network, data.access_token, function() {
-              $scope.$emit('Local/GlideraUpdated', data.access_token);
-              $timeout(function() {
-                go.path('glidera');
-                $scope.$apply();
-              }, 100);
-            });
-          }
-        });
-      }, 100);
-    };
+              go.path('glidera');
+              $scope.$apply();
+            }, 100);
+          });
+        }
+      });
+    }, 100);
+  };
 
-    this.checkCode = function() {
-console.log('[glideraUri.js.35:$stateParams:]' + JSON.stringify($stateParams)); //TODO
-      this.code = $stateParams.code;
-      this.submitOauthCode(this.code);
-    };
-
-  });
+  this.checkCode = function() {
+    if ($stateParams.url) {
+      var match = $stateParams.url.match(/code=(.+)/);
+      if (match && match[1]) {
+        this.code = match[1];
+        return this.submitOauthCode(this.code);
+      }
+    }
+    $log.error('Bad state: ' + JSON.stringify($stateParams));
+  }
+});
