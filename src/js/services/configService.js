3776062e16b1033c235c6c211f7ad8cb5361cc85
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-06-12 16:42:32 -0300

    Removes importLegacy. Enable Coinbase/Glidera for livenet. Fix undefined variable on buyCoinbase/Glidera

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -111,11 +111,11 @@ angular.module('copayApp.services').factory('configService', function(storageSer
 
       // Glidera
       // Disabled for testnet
-      configCache.glidera.testnet = true;
+      configCache.glidera.testnet = false;
 
       // Coinbase
       // Disabled for testnet
-      configCache.coinbase.testnet = true;
+      configCache.coinbase.testnet = false;
 
       $log.debug('Preferences read:', configCache)
       return cb(err, configCache);
