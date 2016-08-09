a668723b3f8702cfe879c1fb66fa9ff8a765223f
Matias Alejo Garcia <ematiu@gmail.com>
2016-08-09 20:55:31 -0300

    add images

--- src/js/appConfig.js
+++ src/js/appConfig.js
@@ -1,3 +1,3 @@
 window.version="2.4.2";
-window.commitHash="918dc2c";
-window.appConfig={"//":"        Modify it at app-template/","extraCSS":null,"version":"2.3.0","enabledExtensions":["coinbase","glidera","debitcard","amazon"],"name":"copay","nameCase":"Copay","replace":{"APP_NAME":"copay","CASED_APP_NAME":"Copay"}};
+window.commitHash="9a50261";
+window.appConfig={"//":"        Modify it at app-template/","extraCSS":null,"version":"2.3.0","enabledExtensions":["coinbase","glidera","debitcard","amazon"],"appName":"Bitpay App","appNameCase":"Copay","replace":{"APP_NAME":"copay","CASED_APP_NAME":"Copay"}};
