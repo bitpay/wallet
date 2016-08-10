23f7f56f3f646b4ca6e60cc0fb2a673d5f1218d6
Matias Alejo Garcia <ematiu@gmail.com>
2016-08-10 17:35:40 -0300

    refactor build scripts

--- src/js/controllers/sidebar.js
+++ src/js/controllers/sidebar.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('copayApp.controllers').controller('sidebarController',
-  function($rootScope, $timeout, $ionicScrollDelegate, lodash, profileService, configService, go, platformInfo) {
+  function($rootScope, $timeout, $ionicScrollDelegate, lodash, profileService, configService, go, platformInfo, $window) {
     var self = this;
     self.isWindowsPhoneApp = platformInfo.isWP && platformInfo.isCordova;
     self.walletSelection = false;
+
+    self.bitpayCardEnabled = $window.appConfig && $window.appConfig._enabledExtensions.debitcard;
+
 
     // wallet list change
     $rootScope.$on('Local/WalletListUpdated', function(event) {
