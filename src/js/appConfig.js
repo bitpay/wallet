11b2e60bfc7311b4eb27bf6c7d3a93069a094ea6
Matias Alejo Garcia <ematiu@gmail.com>
2016-08-09 21:30:18 -0300

    update w master

--- src/js/appConfig.js
+++ src/js/appConfig.js
@@ -1,3 +1,3 @@
-window.version="2.3.0";
-window.commitHash="42675cc";
-window.appConfig={"//":"        Modify it at app-template/","name":"copay","nameCase":"Copay","description":"Multisignature Wallet","version":"2.3.0","androidVersion":"230","_extraCSS":null,"_enabledExtensions":["coinbase","glidera","debitcard","amazon"]};
+window.version="2.5.0";
+window.commitHash="6e02066";
+window.appConfig={"//":"        Modify it at app-template/","name":"copay","nameCase":"Copay","description":"Multisignature Wallet","version":"2.5.0","androidVersionCode":"115","_extraCSS":null,"_enabledExtensions":["coinbase","glidera","debitcard","amazon"]};
