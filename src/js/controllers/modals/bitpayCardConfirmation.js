894c206204f5507d9adcc3431a7d935ccc6675ee
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-08-23 08:31:48 -0300

    Using state.go

--- src/js/controllers/modals/bitpayCardConfirmation.js
+++ src/js/controllers/modals/bitpayCardConfirmation.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('bitpayCardConfirmationController', function($scope, $timeout, $location, bitpayCardService) {
+angular.module('copayApp.controllers').controller('bitpayCardConfirmationController', function($scope, $timeout, $state, bitpayCardService) {
 
   $scope.ok = function() {
     bitpayCardService.logout(function() {
-      $location.path('/bitpayCard/main');
+      $state.go('bitpayCard.main');
     });
     $scope.cancel();
   };
