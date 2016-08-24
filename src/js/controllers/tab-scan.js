8867166f15eb7495e472af67453537ed32f581b1
Matias Alejo Garcia <ematiu@gmail.com>
2016-08-24 19:28:20 -0300

    qr Scanner

--- src/js/controllers/tab-scan.js
+++ src/js/controllers/tab-scan.js
@@ -1,18 +1,12 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('tabScanController', function($scope, $timeout, $ionicModal, $log, $ionicPopup, configService, gettextCatalog, platformInfo, bitcore, lodash, $state) {
+angular.module('copayApp.controllers').controller('tabScanController', function($scope, $timeout, $ionicModal, $log, $ionicPopup, configService, gettextCatalog, platformInfo, bitcore, lodash, $state, walletService) {
 
   var isCordova = platformInfo.isCordova;
   var isWP = platformInfo.isWP;
   var isIOS = platformInfo.isIOS;
 
-  var config = configService.getSync();
-  var configWallet = config.wallet;
-  var walletSettings = configWallet.settings;
 
-  var unitToSatoshi = walletSettings.unitToSatoshi;
-  var unitDecimals = walletSettings.unitDecimals;
-  var satToUnit = 1 / unitToSatoshi;
 
   var _showAlert = function(title, msg, cb) {
     $log.warn(title +  ":"+ msg);
@@ -27,63 +21,13 @@ angular.module('copayApp.controllers').controller('tabScanController', function(
   };
 
   var _dataScanned = function(data) {
-    var parsedData = _parseFromUri(data);
-
-    if (lodash.isEmpty(parsedData)) {
+    $log.debug('Scanned:' + data);
+    if (!walletService.redirFromUri(data)) {
+      $log.warn('Fail to process scanned data');
       _showAlert('Bad bitcoin address', 'Could not recognize the bitcoin address', function(res) {
         $scope.init();
       });
-      return;
     }
-
-    $state.go('confirm', parsedData);
-  };
-
-  var _parseFromUri = function(uri) {
-
-    function sanitizeUri(uri) {
-      // Fixes when a region uses comma to separate decimals
-      var regex = /[\?\&]amount=(\d+([\,\.]\d+)?)/i;
-      var match = regex.exec(uri);
-      if (!match || match.length === 0) {
-        return uri;
-      }
-      var value = match[0].replace(',', '.');
-      var newUri = uri.replace(regex, value);
-      return newUri;
-    };
-
-    var satToUnit = 1 / unitToSatoshi;
-
-    // URI extensions for Payment Protocol with non-backwards-compatible request
-    if ((/^bitcoin:\?r=[\w+]/).exec(uri)) {
-      // TODO: PAYPRO
-    } else {
-      uri = sanitizeUri(uri);
-
-      if (!bitcore.URI.isValid(uri)) {
-        return uri;
-      }
-      var parsed = new bitcore.URI(uri);
-
-      var addr = parsed.address ? parsed.address.toString() : '';
-      var message = parsed.message;
-
-      var amount = parsed.amount ?
-        (parsed.amount.toFixed(0) * satToUnit).toFixed(unitDecimals) : 0;
-
-
-      if (parsed.r) {
-        // TODO: PAYPRO
-      } else {
-        // TODO: message
-        return {
-          toAddress: addr,
-          toAmount: amount
-        };
-      }
-    }
-
   };
 
   var onSuccess = function(result) {
