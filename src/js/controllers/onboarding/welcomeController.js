7835694fb233c6170a68bc4c3711529191825aae
Gabriel Bazán <gabrielbazan7@hotmail.com>
2016-09-01 12:10:08 -0300

    import with Qr in onboarding

--- src/js/controllers/onboarding/welcomeController.js
+++ src/js/controllers/onboarding/welcomeController.js
@@ -2,6 +2,12 @@
 
 angular.module('copayApp.controllers').controller('welcomeController', function($scope, $state, $timeout, $log, $ionicPopup, profileService) {
 
+  $scope.goImport = function(code) {
+    $state.go('tabs.import', {
+      code: code
+    });
+  };
+
   $scope.createProfile = function() {
     $log.debug('Creating profile');
     profileService.createProfile(function(err) {
