3778c9f97039ebbf4d0a22f669b8d8aa4a4f1707
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-09-02 16:48:27 -0300

    Removes console.log

--- src/js/controllers/onboarding/notifications.js
+++ src/js/controllers/onboarding/notifications.js
@@ -1,7 +1,6 @@
 'use strict';
 
 angular.module('copayApp.controllers').controller('notificationsController', function($scope, $state, $stateParams, profileService) {
-console.log('[notifications.js:3]',$stateParams); //TODO
 
   $scope.walletId = $stateParams.walletId;
   $scope.allowNotif = function() {
