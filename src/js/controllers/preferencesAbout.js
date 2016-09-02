dbf0422148dd61edbfbdc54af3c080a817daca39
Matias Alejo Garcia <ematiu@gmail.com>
2016-09-02 14:04:19 -0300

    fix About

--- src/js/controllers/preferencesAbout.js
+++ src/js/controllers/preferencesAbout.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('copayApp.controllers').controller('preferencesAbout',
-  function($ionicNavBarDelegate, gettextCatalog) {
+  function($scope, $version, $ionicNavBarDelegate, gettextCatalog) {
     $ionicNavBarDelegate.title(gettextCatalog.getString('About Copay'));
+
+    $scope.version = $window.version;
+    $scope.commit = $window.commitHash;
   });
