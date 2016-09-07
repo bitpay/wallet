6ca8dbfbf536b7a40878adaf7a72468074f80f7e
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-09-07 10:25:27 -0300

    Fix openExtenalLink

--- src/js/controllers/onboarding/terms.js
+++ src/js/controllers/onboarding/terms.js
@@ -2,6 +2,7 @@
 
 angular.module('copayApp.controllers').controller('termsController', function($scope, $log, $state, uxLanguage, profileService, externalLinkService) {
   $scope.lang = uxLanguage.currentLanguage;
+  $scope.disclaimerUrl = $window.appConfig.disclaimerUrl;
 
   $scope.confirm = function() {
     profileService.setDisclaimerAccepted(function(err) {
