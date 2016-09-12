6d1c9dfeb98494086944a15f69a0e1d0b6b9d1e6
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-09-12 12:19:08 -0300

    Fix route

--- src/js/routes.js
+++ src/js/routes.js
@@ -507,7 +507,7 @@ angular.module('copayApp').config(function(historicLogProvider, $provide, $logPr
         }
       })
       .state('tabs.addressbook.add', {
-        url: '/addressbook/add',
+        url: '/add',
         views: {
           'tab-settings@tabs': {
             templateUrl: 'views/addressbook.add.html',
@@ -516,7 +516,7 @@ angular.module('copayApp').config(function(historicLogProvider, $provide, $logPr
         }
       })
       .state('tabs.addressbook.view', {
-        url: '/addressbook/view/:address',
+        url: '/view/:address',
         views: {
           'tab-settings@tabs': {
             templateUrl: 'views/addressbook.view.html',
