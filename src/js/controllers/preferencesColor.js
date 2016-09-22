4c1033d9c8278898c0b01b93122536385324bff9
Jamal Jackson <jamal@bitpay.com>
2016-09-22 15:36:44 -0400

    added color names

--- src/js/controllers/preferencesColor.js
+++ src/js/controllers/preferencesColor.js
@@ -17,6 +17,22 @@ angular.module('copayApp.controllers').controller('preferencesColorController', 
         '#FF599E',
         '#7A8C9E',
       ];
+      $scope.colorList = [
+        {color: "#DD4B39", name: "Cinnabar"},
+        {color: "#F38F12", name: "Carrot Orange"},
+        {color: "#FAA77F", name: "Light Salmon"},
+        {color: "#D0B136", name: "Metallic Gold"},
+        {color: "#9EDD72", name: "Feijoa"},
+        {color: "#29BB9C", name: "Shamrock"},
+        {color: "#019477", name: "Observatory"},
+        {color: "#77DADA", name: "Turquoise Blue"},
+        {color: "#4A90E2", name: "Cornflower Blue"},
+        {color: "#484ED3", name: "Free Speech Blue"},
+        {color: "#9B59B6", name: "Deep Lilac"},
+        {color: "#E856EF", name: "Free Speech Magenta"},
+        {color: "#FF599E", name: "Brilliant Rose"},
+        {color: "#7A8C9E", name: "Light Slate Grey"}
+      ];
 
   var wallet = profileService.getWallet($stateParams.walletId);
   $scope.wallet = wallet;
