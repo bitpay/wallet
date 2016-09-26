ec875f7b1b69d2fdcd4499a358d7f487ddeff609
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-09-26 18:26:41 -0300

    Hide nav-bar on amount/confirm and addressbook views

--- src/js/controllers/tabsController.js
+++ src/js/controllers/tabsController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('tabsController', function($log, $scope, $stateParams, $ionicModal, $timeout, incomingData) {
+angular.module('copayApp.controllers').controller('tabsController', function($rootScope, $log, $scope, $state, $stateParams, $timeout, incomingData, lodash) {
 
   $scope.onScan = function(data) {
     if (!incomingData.redir(data)) {
@@ -22,6 +22,28 @@ angular.module('copayApp.controllers').controller('tabsController', function($lo
     $timeout(function() {
       $scope.$apply();
     }, 1);
-  }
+  };
+
+  var hideTabsViews = [
+    'tabs.send.amount',
+    'tabs.send.confirm',
+    'tabs.send.addressbook',
+    'tabs.addressbook',
+    'tabs.addressbook.add',
+    'tabs.addressbook.view',
+  ];
+
+  $rootScope.$on('$ionicView.beforeEnter', function() {
+
+    $rootScope.hideTabs = false;
+
+    var currentState = $state.current.name;
+
+    lodash.each(hideTabsViews, function(view) {
+      if (currentState === view) {
+        $rootScope.hideTabs = true;
+      }
+    });
+  });
 
 });
