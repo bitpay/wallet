6334242107ae624625f07aa1a9f87832d87de206
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-09-26 14:31:36 -0300

    Fix title bar when changing tabs

--- src/js/controllers/preferencesAbout.js
+++ src/js/controllers/preferencesAbout.js
@@ -1,11 +1,9 @@
 'use strict';
 
 angular.module('copayApp.controllers').controller('preferencesAbout',
-  function($scope, $window, $ionicNavBarDelegate, gettextCatalog, externalLinkService) {
-    $scope.$on("$ionicView.beforeEnter", function(event, data){
-      $ionicNavBarDelegate.title(gettextCatalog.getString('About') + ' ' + $window.appConfig.nameCase);
-    });
+  function($scope, $window, gettextCatalog, externalLinkService) {
 
+    $scope.title = gettextCatalog.getString('About') + ' ' + $window.appConfig.nameCase;
     $scope.version = $window.version;
     $scope.commitHash = $window.commitHash;
     $scope.name = $window.appConfig.gitHubRepoName;
