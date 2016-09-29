f4f96d61778a14956134074eaf3c28bd1cca5f57
Gabriel Bazán <gabrielbazan7@hotmail.com>
2016-09-29 11:20:38 -0300

    experimental feature disabled by default

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -54,7 +54,7 @@ angular.module('copayApp.services').factory('configService', function(storageSer
     //Experimental Features
 
     recentTransactions: {
-      enabled: true
+      enabled: false //disabled by default
     },
 
     frequentlyUsed: {
