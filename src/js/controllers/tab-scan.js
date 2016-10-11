ae60e54b39ccd152e8608657b984c946a5fa0337
Jason Dreyzehner <jason@dreyzehner.com>
2016-10-11 02:59:21 -0400

    feat(scan): first iteration of full scan view implementation

--- src/js/controllers/tab-scan.js
+++ src/js/controllers/tab-scan.js
@@ -23,57 +23,71 @@ angular.module('copayApp.controllers').controller('tabScanController', function(
   }
 
   function _handleCapabilities(){
-    if(!$scope.scannerIsAvailable){
-      $scope.currentState = scannerStates.unavailable;
-    } else if($scope.scannerIsDenied){
-      $scope.currentState = scannerStates.denied;
-    } else if($scope.scannerIsRestricted){
-      $scope.currentState = scannerStates.denied;
-    } else if(!$scope.scannerHasPermission){
-      $scope.currentState = scannerStates.unauthorized;
-    } else if($scope.scannerHasPermission){
+    // always update the view
+    $timeout(function(){
+      if(!scannerService.isInitialized()){
+        $scope.currentState = scannerStates.loading;
+      } else if(!$scope.scannerIsAvailable){
+        $scope.currentState = scannerStates.unavailable;
+      } else if($scope.scannerIsDenied){
+        $scope.currentState = scannerStates.denied;
+      } else if($scope.scannerIsRestricted){
+        $scope.currentState = scannerStates.denied;
+      } else if(!$scope.scannerHasPermission){
+        $scope.currentState = scannerStates.unauthorized;
+      }
+      $log.debug('Scan view state set to: ' + $scope.currentState);
+    });
+  }
+
+  function _refreshScanView(){
+    _updateCapabilities();
+    _handleCapabilities();
+    if($scope.scannerHasPermission){
       activate();
     }
   }
-
-  function _initScanView(){
-    _updateCapabilities();
-    _handleCapabilities();
-  }
-
-  $scope.$on("$ionicView.beforeEnter", function() {
-    $scope.currentState = scannerStates.loading;
-  });
 
   // This could be much cleaner with a Promise API
   // (needs a polyfill for some platforms)
   $rootScope.$on('scannerServiceInitialized', function(){
     $log.debug('Scanner initialization finished, reinitializing scan view...');
-    _initScanView();
+    _refreshScanView();
   });
 
   $scope.$on("$ionicView.afterEnter", function() {
-    if(scannerService.isInitialized()){
-      _initScanView();
-    }
+    // try initializing and refreshing status any time the view is entered
+    scannerService.gentleInitialize();
   });
 
   function activate(){
     scannerService.activate(function(){
+      _updateCapabilities();
+      _handleCapabilities();
       $log.debug('Scanner activated, setting to visible...');
       $scope.currentState = scannerStates.visible;
-      scannerService.scan(function(err, contents){
-        if(err){
-          $log.debug('Scan canceled.');
-        } else if ($state.params.passthroughMode) {
-          $rootScope.scanResult = contents;
-          goBack();
-        } else {
-          handleSuccessfulScan(contents);
-        }
-      });
+        // pause to update the view
+        $timeout(function(){
+          scannerService.scan(function(err, contents){
+          if(err){
+            $log.debug('Scan canceled.');
+          } else if ($state.params.passthroughMode) {
+            $rootScope.scanResult = contents;
+            goBack();
+          } else {
+            handleSuccessfulScan(contents);
+          }
+          });
+        });
     });
   }
+  $scope.activate = activate;
+
+  $scope.authorize = function(){
+    scannerService.initialize(function(){
+      _refreshScanView();
+    });
+  };
 
   $scope.$on("$ionicView.afterLeave", function() {
     scannerService.deactivate();
@@ -83,6 +97,14 @@ angular.module('copayApp.controllers').controller('tabScanController', function(
     $log.debug('Scan returned: "' + contents + '"');
     incomingData.redir(contents);
   }
+
+  $scope.openSettings = function(){
+    scannerService.openSettings();
+  };
+
+  $scope.attemptToReactivate = function(){
+    scannerService.reinitialize();
+  };
 
   $scope.toggleLight = function(){
     scannerService.toggleLight(function(lightEnabled){
@@ -98,7 +120,7 @@ angular.module('copayApp.controllers').controller('tabScanController', function(
       $timeout(function(){
         $scope.cameraToggleActive = false;
         $log.debug('Camera toggle control deactivated.');
-      }, 600);
+      }, 200);
     });
   };
 
@@ -106,6 +128,9 @@ angular.module('copayApp.controllers').controller('tabScanController', function(
     return $state.params.passthroughMode;
   }
   function goBack(){
+    $ionicHistory.nextViewOptions({
+      disableAnimate: true
+    });
     $ionicHistory.backView().go();
   }
   $scope.goBack = goBack;
--- src/js/directives/qrScanner.js
+++ src/js/directives/qrScanner.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('copayApp.directives')
-  .directive('qrScanner', function($state, $rootScope, $log) {
+  .directive('qrScanner', function($state, $rootScope, $log, $ionicHistory) {
 
     return {
       restrict: 'E',
@@ -9,11 +9,14 @@ angular.module('copayApp.directives')
         onScan: "&"
       },
       replace: true,
-      template: '<a on-tap="openScanner()"><i class="icon ion-qr-scanner"></i></a>',
+      template: '<a on-tap="openScanner()" nav-transition="none"><i class="icon ion-qr-scanner"></i></a>',
       link: function(scope, el, attrs) {
 
         scope.openScanner = function() {
           $log.debug('Opening scanner by directive...');
+          $ionicHistory.nextViewOptions({
+            disableAnimate: true
+          });
           $state.go('scanner', { passthroughMode: 1 });
         };
 
