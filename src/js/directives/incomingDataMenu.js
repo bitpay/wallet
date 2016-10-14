801179bfb59774065a4084fac1805d7760105a95
Marty Alcala <marty@bitpay.com>
2016-10-14 15:21:16 -0400

    prelim website scan handler ui

--- src/js/directives/incomingDataMenu.js
+++ src/js/directives/incomingDataMenu.js
@@ -7,8 +7,15 @@ angular.module('copayApp.directives')
       templateUrl: 'views/includes/incomingDataMenu.html',
       link: function(scope, element, attrs) {
         $rootScope.$on('incomingDataMenu.showMenu', function() {
-          scope.showMenu = true;
+          console.log('in showMenu handler');
+          $timeout(function() {
+            console.log('in showMenu timeout');
+            scope.showMenu = true;
+          });
         });
+        $timeout(function() {
+          scope.showMenu = true;
+        }, 2000);
         scope.$watch('showMenu', function() {
           console.log('scope.showMenu', scope.showMenu);
           if(!scope.showMenu) {
