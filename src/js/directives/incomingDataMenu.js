e9aaec3c68b90566174cfc34d9b2d53ef2ba9a82
Marty Alcala <marty@bitpay.com>
2016-10-18 15:45:09 -0400

    prelim bitcoin address support for incoming data menu

--- src/js/directives/incomingDataMenu.js
+++ src/js/directives/incomingDataMenu.js
@@ -1,15 +1,18 @@
 'use strict';
 
 angular.module('copayApp.directives')
-  .directive('incomingDataMenu', function($timeout, $rootScope) {
+  .directive('incomingDataMenu', function($timeout, $rootScope, bitcore) {
     return {
       restrict: 'E',
       templateUrl: 'views/includes/incomingDataMenu.html',
       link: function(scope, element, attrs) {
         $rootScope.$on('incomingDataMenu.showMenu', function(event, data) {
           $timeout(function() {
-            scope.data = data;
+            scope.data = data.data;
+            scope.type = data.type;
             scope.showMenu = true;
+
+            console.log('data', data);
           });
         });
         scope.hide = function() {
