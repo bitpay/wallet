ce5cff9c71df4a4fe565e384fe39b30e6dfa13f4
Jamal Jackson <jamal@bitpay.com>
2016-10-18 20:56:08 -0400

    added logic to constantly check for a user allowing push notification permissions to continue on onbaording, or use continue button

--- src/js/controllers/onboarding/notifications.js
+++ src/js/controllers/onboarding/notifications.js
@@ -1,14 +1,29 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('notificationsController', function($scope, $state, $timeout, $stateParams, $ionicConfig, profileService, configService) {
+angular.module('copayApp.controllers').controller('notificationsController', function($scope, $state, $timeout, $stateParams, $ionicConfig, profileService, configService, $interval) {
 
   $ionicConfig.views.swipeBackEnabled(false);
   $scope.walletId = $stateParams.walletId;
 
   $scope.allowNotif = function() {
+    $scope.notificationDialogOpen = true;
     $timeout(function() {
       profileService.pushNotificationsInit();
     });
+    $scope.notificationPromise = $interval(function() {
+      PushNotification.hasPermission(function(data) {
+        if (data.isEnabled) {
+          $interval.cancel($scope.notificationPromise);
+          $state.go('onboarding.backupRequest', {
+            walletId: $scope.walletId
+          });
+        }
+      });
+    }, 100);
+  }
+
+  $scope.continue = function() {
+    $interval.cancel($scope.notificationPromise);
     $state.go('onboarding.backupRequest', {
       walletId: $scope.walletId
     });
