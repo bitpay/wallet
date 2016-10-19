62a6a732ed42ee707a125d24a4f08e7d17cf6442
Marty Alcala <marty@bitpay.com>
2016-10-19 15:23:54 -0400

    reset https flag on new scan

--- src/js/directives/actionSheet.js
+++ src/js/directives/actionSheet.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('copayApp.directives')
-  .directive('actionSheet', function($rootScope) {
+  .directive('actionSheet', function($rootScope, $timeout) {
     return {
       restrict: 'E',
       templateUrl: 'views/includes/actionSheet.html',
@@ -10,6 +10,13 @@ angular.module('copayApp.directives')
         show: '=actionSheetShow',
       },
       link: function(scope, element, attrs) {
+        scope.$watch('show', function() {
+          if(scope.show) {
+            $timeout(function() { scope.revealMenu = true; }, 100);
+          } else {
+            scope.revealMenu = false;
+          }
+        });
         scope.hide = function() {
           scope.show = false;
           $rootScope.$broadcast('incomingDataMenu.menuHidden');
