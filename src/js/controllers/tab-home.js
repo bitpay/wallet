f7624bcc9bf12ec0ceb64cd2544392ee99150b88
Javier <javier@bitpay.com>
2016-10-22 17:43:05 -0300

    fix routes and params

--- src/js/controllers/tab-home.js
+++ src/js/controllers/tab-home.js
@@ -68,7 +68,10 @@ angular.module('copayApp.controllers').controller('tabHomeController',
 
         $scope.wallet = wallet;
         $scope.btx = lodash.cloneDeep(tx);
-        $state.go('tabs.wallet.tx-details', {tx: $scope.btx, wallet: $scope.wallet});
+        $state.transitionTo('tabs.wallet.tx-details', {
+          txid: $scope.btx.txid,
+          walletId: $scope.walletId
+        });
 
         walletService.getTxNote(wallet, n.txid, function(err, note) {
           if (err) $log.warn('Could not fetch transaction note: ' + err);
