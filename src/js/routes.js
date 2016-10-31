204fa35a19def63eb459832acce5b703218deb19
Javier Donadio <tanojaja@gmail.com>
2016-10-31 16:46:45 -0300

    remote unnecessary email route

--- src/js/routes.js
+++ src/js/routes.js
@@ -276,7 +276,9 @@ angular.module('copayApp').config(function(historicLogProvider, $provide, $logPr
             templateUrl: 'views/confirm.html'
           }
         },
-        params: { paypro: null }
+        params: {
+          paypro: null
+        }
       })
       .state('tabs.send.addressbook', {
         url: '/addressbook/add/:fromSendTab/:addressbookEntry',
@@ -462,15 +464,6 @@ angular.module('copayApp').config(function(historicLogProvider, $provide, $logPr
           'tab-settings@tabs': {
             controller: 'preferencesColorController',
             templateUrl: 'views/preferencesColor.html'
-          }
-        }
-      })
-      .state('tabs.preferences.preferencesEmail', {
-        url: '/preferencesEmail',
-        views: {
-          'tab-settings@tabs': {
-            controller: 'preferencesEmailController',
-            templateUrl: 'views/preferencesEmail.html'
           }
         }
       })
@@ -892,7 +885,9 @@ angular.module('copayApp').config(function(historicLogProvider, $provide, $logPr
             templateUrl: 'views/confirm.html'
           }
         },
-        params: { paypro: null }
+        params: {
+          paypro: null
+        }
       })
       .state('tabs.bitpayCard.preferences', {
         url: '/preferences',
