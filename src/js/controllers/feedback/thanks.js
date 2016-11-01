75da9b2e9323782deae154e5ead357e13e88d456
Javier Donadio <tanojaja@gmail.com>
2016-11-01 15:35:52 -0300

    share wallet

--- src/js/controllers/feedback/thanks.js
+++ src/js/controllers/feedback/thanks.js
@@ -1,6 +1,12 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('thanksController', function($scope, $state, $stateParams) {
+angular.module('copayApp.controllers').controller('thanksController', function($scope, $state, $stateParams, platformInfo) {
   $scope.score = parseInt($stateParams.score);
   $scope.skip = $stateParams.skip && $scope.score == 5;
+
+  $scope.$on("$ionicView.beforeEnter", function(event, data) {
+    if (platformInfo.isCordova) {
+      window.plugins.socialsharing.share('https://bitpay.com/wallet', null, null, null);
+    }
+  });
 });
