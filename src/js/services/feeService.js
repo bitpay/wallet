b8b5af9fc4105aa38afd7ae1380734f063814058
Javier Donadio <tanojaja@gmail.com>
2016-11-03 18:27:59 -0300

    fix fee service

--- src/js/services/feeService.js
+++ src/js/services/feeService.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('copayApp.services').factory('feeService', function($log, $stateParams, bwcService, walletService, configService, gettext, lodash, txFormatService) {
+angular.module('copayApp.services').factory('feeService', function($log, $stateParams, bwcService, walletService, configService, gettext, lodash, txFormatService, profileService, gettextCatalog) {
   var root = {};
 
   // Constant fee options to translate
@@ -15,45 +15,45 @@ angular.module('copayApp.services').factory('feeService', function($log, $stateP
     return configService.getSync().wallet.settings.feeLevel || 'normal';
   };
 
-  root.getCurrentFeeValue = function(wallet, cb) {
+  root.getCurrentFeeValue = function(network, cb) {
+    network = network || 'livenet';
     var feeLevel = root.getCurrentFeeLevel();
 
-    wallet.getFeeLevels(wallet.credentials.network, function(err, levels) {
-      if (err)
-        return cb({
-          message: 'Could not get dynamic fee'
-        });
+    root.getFeeLevels(function(err, levels) {
+      if (err) return cb(err);
 
-      var feeLevelValue = lodash.find(levels, {
+      var feeLevelValue = lodash.find(levels[network], {
         level: feeLevel
       });
-      if (!feeLevelValue || feeLevelValue.feePerKB == null)
+
+      if (!feeLevelValue || !feeLevelValue.feePerKB)
         return cb({
-          message: 'Could not get dynamic fee for level: ' + feeLevel
+          message: gettextCatalog.getString('Could not get dynamic fee for level: ') + feeLevel
         });
 
       var fee = feeLevelValue.feePerKB;
       $log.debug('Dynamic fee: ' + feeLevel + ' ' + fee + ' SAT');
+
       return cb(null, fee);
     });
   };
 
   root.getFeeLevels = function(cb) {
     var walletClient = bwcService.getClient();
-
     var unitName = configService.getSync().wallet.settings.unitName;
 
     walletClient.getFeeLevels('livenet', function(errLivenet, levelsLivenet) {
       walletClient.getFeeLevels('testnet', function(errTestnet, levelsTestnet) {
-        if (errLivenet || errTestnet) $log.debug('Could not get dynamic fee');
-        else {
+        if (errLivenet || errTestnet) {
+          return cb(gettextCatalog.getString('Could not get dynamic fee'));
+        } else {
           for (var i = 0; i < 4; i++) {
             levelsLivenet[i]['feePerKBUnit'] = txFormatService.formatAmount(levelsLivenet[i].feePerKB) + ' ' + unitName;
             levelsTestnet[i]['feePerKBUnit'] = txFormatService.formatAmount(levelsTestnet[i].feePerKB) + ' ' + unitName;
           }
         }
 
-        return cb({
+        return cb(null, {
           'livenet': levelsLivenet,
           'testnet': levelsTestnet
         });
