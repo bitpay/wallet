1f82b2398249bb1bec9ecfcf66de18127923dea5
Gabriel Bazán <gabrielbazan7@hotmail.com>
2016-11-03 13:39:01 -0300

    styling

--- src/js/controllers/feedback/sendFeedback.js
+++ src/js/controllers/feedback/sendFeedback.js
@@ -5,23 +5,23 @@ angular.module('copayApp.controllers').controller('sendFeedbackController', func
   switch ($scope.score) {
     case 1:
       $scope.reaction = gettextCatalog.getString("Ouch!");
-      $scope.comment = gettextCatalog.getString("There's obviously something we're doing wrong. Is there anything we could do to improve your experience?");
+      $scope.comment = gettextCatalog.getString("There's obviously something we're doing wrong.");
       break;
     case 2:
       $scope.reaction = gettextCatalog.getString("Oh no!");
-      $scope.comment = gettextCatalog.getString("There's obviously something we're doing wrong. Is there anything we could do to improve your experience?");
+      $scope.comment = gettextCatalog.getString("There's obviously something we're doing wrong.");
       break;
     case 3:
       $scope.reaction = gettextCatalog.getString("Thanks!");
-      $scope.comment = gettextCatalog.getString("We're always looking for ways to improve BitPay wallet. Is there anything we could do to improve your experience?");
+      $scope.comment = gettextCatalog.getString("We're always looking for ways to improve BitPay wallet.");
       break;
     case 4:
       $scope.reaction = gettextCatalog.getString("Thanks!");
-      $scope.comment = gettextCatalog.getString("That's exciting to hear. We'd love to earn that fifth star from you - how could we improve your experience?");
+      $scope.comment = gettextCatalog.getString("That's exciting to hear. We'd love to earn that fifth star from you");
       break;
     case 5:
       $scope.reaction = gettextCatalog.getString("Feedback!");
-      $scope.comment = gettextCatalog.getString("We're always looking for ways to improve BitPay wallet. Is there anything we could do to improve your experience?");
+      $scope.comment = gettextCatalog.getString("We're always looking for ways to improve BitPay wallet.");
       break;
   }
 
