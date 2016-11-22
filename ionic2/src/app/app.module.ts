07e98ad841b4e6f7547b530fba2e117d416d39bf
Marty Alcala <marty@bitpay.com>
2016-11-22 17:45:38 -0500

    remove comments

--- ionic2/src/app/app.module.ts
+++ ionic2/src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-// import { AboutPage } from '../pages/about/about';
-// import { ContactPage } from '../pages/contact/contact';
-// import { HomePage } from '../pages/home/home';
-// import { TabsPage } from '../pages/tabs/tabs';
 import { PAGES } from '../pages/pages';
 
 @NgModule({
