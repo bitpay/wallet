4100f3ba5f610755d6395f8337939d0688c3b792
Marty Alcala <marty@bitpay.com>
2016-12-02 11:55:12 -0500

    prelim incomingData conversion to ionic2

---
+++ ionic2/src/services/incoming-data.service.ts
@@ -0,0 +1,170 @@
+import { Injectable } from '@angular/core';
+import { Logger } from 'angular2-logger/core';
+import { ScannerService } from './scanner.service';;
+
+@Injectable()
+export class IncomingDataService {
+
+  win: any = window;
+  bitcore: any = this.win.bitcore;
+
+  payproService: any = {
+    getPayProDetails: (url, callback) => {}
+  };
+
+  constructor(public logger: Logger, public scannerService: ScannerService) {}
+
+  showMenu (data) {
+    //$rootScope.$broadcast('incomingDataMenu.showMenu', data);
+  }
+
+  redir(data) {
+    this.logger.debug('Processing incoming data: ' + data);
+
+    // data extensions for Payment Protocol with non-backwards-compatible request
+    if ((/^bitcoin:\?r=[\w+]/).exec(data)) {
+      data = decodeURIComponent(data.replace('bitcoin:?r=', ''));
+      $state.go('tabs.send', {}, {'reload': true, 'notify': $state.current.name == 'tabs.send' ? false : true}).then(function() {
+        $state.transitionTo('tabs.send.confirm', {paypro: data});
+      });
+      return true;
+    }
+
+    data = this.sanitizeUri(data);
+
+    // BIP21
+    if (this.bitcore.URI.isValid(data)) {
+      var parsed = new this.bitcore.URI(data);
+
+      var addr = parsed.address ? parsed.address.toString() : '';
+      var message = parsed.message;
+
+      var amount = parsed.amount ?  parsed.amount : '';
+
+      if (parsed.r) {
+        this.payproService.getPayProDetails(parsed.r, function(err, details) {
+          this.handlePayPro(details);
+        });
+      } else {
+        $state.go('tabs.send', {}, {'reload': true, 'notify': $state.current.name == 'tabs.send' ? false : true});
+        // Timeout is required to enable the "Back" button
+        setTimeout(function() {
+          if (amount) {
+            $state.transitionTo('tabs.send.confirm', {toAmount: amount, toAddress: addr, description:message});
+          } else {
+            $state.transitionTo('tabs.send.amount', {toAddress: addr});
+          }
+        }, 100);
+      }
+      return true;
+
+    // Plain URL
+    } else if (/^https?:\/\//.test(data)) {
+
+      this.payproService.getPayProDetails(data, function(err, details) {
+        if(err) {
+          this.showMenu({data: data, type: 'url'});
+          return;
+        }
+        this.handlePayPro(details);
+        return true;
+      });
+      // Plain Address
+    } else if (this.bitcore.Address.isValid(data, 'livenet') || this.bitcore.Address.isValid(data, 'testnet')) {
+      if($state.includes('tabs.scan')) {
+        this.showMenu({data: data, type: 'bitcoinAddress'});
+      } else {
+        this.goToAmountPage(data);
+      }
+    } else if (data && data.indexOf(this.win.appConfig.name + '://glidera') === 0) {
+        return $state.go('uriglidera', {url: data});
+    } else if (data && data.indexOf(this.win.appConfig.name + '://coinbase') === 0) {
+        return $state.go('uricoinbase', {url: data});
+
+      // BitPayCard Authentication
+    } else if (data && data.indexOf(this.win.appConfig.name + '://') === 0) {
+        var secret = this.getParameterByName('secret', data);
+        var email = this.getParameterByName('email', data);
+        var otp = this.getParameterByName('otp', data);
+        $state.go('tabs.home', {}, {'reload': true, 'notify': $state.current.name == 'tabs.home' ? false : true}).then(function() {
+          $state.transitionTo('tabs.bitpayCardIntro', {
+            secret: secret,
+            email: email,
+            otp: otp
+          });
+        });
+        return true;
+
+    // Join
+    } else if (data && data.match(/^copay:[0-9A-HJ-NP-Za-km-z]{70,80}$/)) {
+      $state.go('tabs.home', {}, {'reload': true, 'notify': $state.current.name == 'tabs.home' ? false : true}).then(function() {
+        $state.transitionTo('tabs.add.join', {url: data});
+      });
+      return true;
+
+    // Old join
+    } else if (data && data.match(/^[0-9A-HJ-NP-Za-km-z]{70,80}$/)) {
+      $state.go('tabs.home', {}, {'reload': true, 'notify': $state.current.name == 'tabs.home' ? false : true}).then(function() {
+        $state.transitionTo('tabs.add.join', {url: data});
+      });
+      return true;
+    } else {
+
+      if($state.includes('tabs.scan')) {
+        this.showMenu({data: data, type: 'text'});
+      }
+    }
+
+    return false;
+
+  }
+
+  sanitizeUri(data) {
+    // Fixes when a region uses comma to separate decimals
+    var regex = /[\?\&]amount=(\d+([\,\.]\d+)?)/i;
+    var match = regex.exec(data);
+    if (!match || match.length === 0) {
+      return data;
+    }
+    var value = match[0].replace(',', '.');
+    var newUri = data.replace(regex, value);
+
+    // mobile devices, uris like copay://glidera
+    newUri.replace('://', ':');
+
+    return newUri;
+  }
+
+  getParameterByName(name, url) {
+    if (!url) return;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+
+  goToAmountPage(toAddress) {
+    $state.go('tabs.send', {}, {'reload': true, 'notify': $state.current.name == 'tabs.send' ? false : true});
+    setTimeout(function() {
+      $state.transitionTo('tabs.send.amount', {toAddress: toAddress});
+    }, 100);
+  }
+
+  handlePayPro(payProDetails){
+    var stateParams = {
+      toAmount: payProDetails.amount,
+      toAddress: payProDetails.toAddress,
+      description: payProDetails.memo,
+      paypro: payProDetails
+    };
+    this.scannerService.pausePreview();
+    $state.go('tabs.send', {}, {'reload': true, 'notify': $state.current.name == 'tabs.send' ? false : true}).then(function() {
+      setTimeout(function() {
+        $state.transitionTo('tabs.send.confirm', stateParams);
+      });
+    });
+  }
+
+}
--- ionic2/src/services/services.ts
+++ ionic2/src/services/services.ts
@@ -1,7 +1,9 @@
+import { IncomingDataService } from './incoming-data.service';
 import { PlatformInfo } from './platform-info.service';
 import { ScannerService } from './scanner.service';
 
 export const SERVICES = [
+  IncomingDataService,
   PlatformInfo,
   ScannerService
 ];
