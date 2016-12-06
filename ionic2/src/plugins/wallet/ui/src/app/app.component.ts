8d4b8ac5f3b2452b887b61a3a24a1221160e9886
Jason Dreyzehner <jason@dreyzehner.com>
2016-12-06 18:41:20 -0500

    feat(plugins): init simserver tooling, add wallet plugin

---
+++ ionic2/src/plugins/wallet/ui/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
+
+import { HomePage } from '../pages/home/home';
+
+
+@Component({
+  template: `<ion-nav [root]="rootPage"></ion-nav>`
+})
+export class MyApp {
+  rootPage = HomePage;
+
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+      Splashscreen.hide();
+    });
+  }
+}
