af960555b4b5a41ca0f3d5ca67af1b35520e4fce
Gabriel Bazán <gabrielbazan7@hotmail.com>
2016-12-08 16:00:03 -0300

    adding language detection and language preferences

--- src/js/controllers/translators.js
+++ src/js/controllers/translators.js
@@ -2,7 +2,7 @@
 
 angular.module('copayApp.controllers').controller('translatorsController',
   function($scope, externalLinkService) {
-    $scope.openExternalLink = function(url, target) {
-      externalLinkService.open(url, target);
+    $scope.openExternalLink = function(url, optIn, title, message, okText, cancelText) {
+      externalLinkService.open(url, optIn, title, message, okText, cancelText);
     };
   });
