cbb7ba0de1e057995acb630cffcc814313d4da06
Marty Alcala <marty@bitpay.com>
2016-12-08 11:06:53 -0500

    remove console.log

--- ionic2/src/components/components.ts
+++ ionic2/src/components/components.ts
@@ -1,8 +1,6 @@
 import { IncomingDataMenuComponent } from './incoming-data-menu/incoming-data-menu.component';
 import { ActionSheetComponent } from './action-sheet/action-sheet.component';
 
-console.log('ActionSheetComponent', ActionSheetComponent);
-
 export const COMPONENTS = [
   ActionSheetComponent,
   IncomingDataMenuComponent
