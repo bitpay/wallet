a03df7f83094964bea87b319e38ce97b13f95bac
Marty Alcala <marty@bitpay.com>
2016-12-08 11:18:20 -0500

    add appConfig service

--- ionic2/src/services/incoming-data.service.ts
+++ ionic2/src/services/incoming-data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs/Rx';
 import { Logger } from 'angular2-logger/core';
 
+import { AppConfigService } from './app-config.service';
 import { ScannerService } from './scanner.service';
 
 import bwc from 'bitcore-wallet-client/index';
@@ -16,10 +17,12 @@ export class IncomingDataService {
 
   win: any = window;
   bitcore: any = bwc.Bitcore;
+  appName: string = 'bitpay';
 
   actionSheetSubject: Subject<any> = new Subject<any>();
   actionSheetObservable: Observable<any> = this.actionSheetSubject.asObservable();
 
+  // placeholder for now
   payproService: any = {
     getPayProDetails: (url, callback) => {
       return callback('not paypro');
@@ -27,13 +30,10 @@ export class IncomingDataService {
   };
 
   constructor(
+    public appConfig: AppConfigService,
     public logger: Logger,
     public scannerService: ScannerService
-  ) {
-    this.win.appConfig = {
-      name: 'appName'
-    };
-  }
+  ) {}
 
   parseBip21(data) {
     let parsed = new this.bitcore.URI(data);
@@ -95,12 +95,12 @@ export class IncomingDataService {
         // Plain Address
       } else if (this.bitcore.Address.isValid(data, 'livenet') || this.bitcore.Address.isValid(data, 'testnet')) {
         return resolve({type: 'bitcoinAddress', parsedData: data});
-      } else if (data && data.indexOf(this.win.appConfig.name + '://glidera') === 0) {
+      } else if (data && data.indexOf(this.appConfig.appName + '://glidera') === 0) {
         return resolve({type: 'glidera', parsedData: data});
-      } else if (data && data.indexOf(this.win.appConfig.name + '://coinbase') === 0) {
+      } else if (data && data.indexOf(this.appConfig.appName + '://coinbase') === 0) {
         return resolve({type: 'coinbase', parsedData: data});
         // BitPayCard Authentication
-      } else if (data && data.indexOf(this.win.appConfig.name + '://') === 0) {
+      } else if (data && data.indexOf(this.appConfig.appName + '://') === 0) {
         let parsedData = this.parseBitpayCardData(data);
         return resolve({type: 'bitpayCard', parsedData: parsedData});
       // Join
@@ -121,107 +121,6 @@ export class IncomingDataService {
 
   menuHidden () {
     this.actionSheetSubject.next({action: 'hide'});
-  }
-
-  redir(data) {
-    // this.logger.debug('Processing incoming data: ' + data);
-    //
-    // // data extensions for Payment Protocol with non-backwards-compatible request
-    // if ((/^bitcoin:\?r=[\w+]/).exec(data)) {
-    //   data = decodeURIComponent(data.replace('bitcoin:?r=', ''));
-    //   // $state.go('tabs.send', {}, {'reload': true, 'notify': $state.current.name == 'tabs.send' ? false : true}).then(function() {
-    //   //   $state.transitionTo('tabs.send.confirm', {paypro: data});
-    //   // });
-    //   return true;
-    // }
-    //
-    // data = this.sanitizeUri(data);
-    //
-    // // BIP21
-    // if (this.bitcore.URI.isValid(data)) {
-    //   let parsed = new this.bitcore.URI(data);
-    //
-    //   //let addr = parsed.address ? parsed.address.toString() : '';
-    //   //let message = parsed.message;
-    //
-    //   let amount = parsed.amount ?  parsed.amount : '';
-    //
-    //   if (parsed.r) {
-    //     this.payproService.getPayProDetails(parsed.r, function(err, details) {
-    //       this.handlePayPro(details);
-    //     });
-    //   } else {
-    //     //$state.go('tabs.send', {}, {'reload': true, 'notify': $state.current.name == 'tabs.send' ? false : true});
-    //     // Timeout is required to enable the "Back" button
-    //     setTimeout(function() {
-    //       if (amount) {
-    //         //$state.transitionTo('tabs.send.confirm', {toAmount: amount, toAddress: addr, description:message});
-    //       } else {
-    //         //$state.transitionTo('tabs.send.amount', {toAddress: addr});
-    //       }
-    //     }, 100);
-    //   }
-    //   return true;
-    //
-    // // Plain URL
-    // } else if (/^https?:\/\//.test(data)) {
-    //
-    //   this.payproService.getPayProDetails(data, function(err, details) {
-    //     if(err) {
-    //       this.showMenu({data: data, type: 'url'});
-    //       return;
-    //     }
-    //     this.handlePayPro(details);
-    //     return true;
-    //   });
-    //   // Plain Address
-    // } else if (this.bitcore.Address.isValid(data, 'livenet') || this.bitcore.Address.isValid(data, 'testnet')) {
-    //   //if($state.includes('tabs.scan')) {
-    //     this.showMenu({data: data, type: 'bitcoinAddress'});
-    //   // } else {
-    //   //   this.goToAmountPage(data);
-    //   // }
-    // } else if (data && data.indexOf(this.win.appConfig.name + '://glidera') === 0) {
-    //     //return $state.go('uriglidera', {url: data});
-    // } else if (data && data.indexOf(this.win.appConfig.name + '://coinbase') === 0) {
-    //     //return $state.go('uricoinbase', {url: data});
-    //
-    //   // BitPayCard Authentication
-    // } else if (data && data.indexOf(this.win.appConfig.name + '://') === 0) {
-    //     //let secret = this.getParameterByName('secret', data);
-    //     //let email = this.getParameterByName('email', data);
-    //     //let otp = this.getParameterByName('otp', data);
-    //     // $state.go('tabs.home', {}, {'reload': true, 'notify': $state.current.name == 'tabs.home' ? false : true}).then(function() {
-    //     //   $state.transitionTo('tabs.bitpayCardIntro', {
-    //     //     secret: secret,
-    //     //     email: email,
-    //     //     otp: otp
-    //     //   });
-    //     // });
-    //     return true;
-    //
-    // // Join
-    // } else if (data && data.match(/^copay:[0-9A-HJ-NP-Za-km-z]{70,80}$/)) {
-    //   // $state.go('tabs.home', {}, {'reload': true, 'notify': $state.current.name == 'tabs.home' ? false : true}).then(function() {
-    //   //   $state.transitionTo('tabs.add.join', {url: data});
-    //   // });
-    //   return true;
-    //
-    // // Old join
-    // } else if (data && data.match(/^[0-9A-HJ-NP-Za-km-z]{70,80}$/)) {
-    //   // $state.go('tabs.home', {}, {'reload': true, 'notify': $state.current.name == 'tabs.home' ? false : true}).then(function() {
-    //   //   $state.transitionTo('tabs.add.join', {url: data});
-    //   // });
-    //   return true;
-    // } else {
-    //
-    //   //if($state.includes('tabs.scan')) {
-    //     this.showMenu({data: data, type: 'text'});
-    //   //}
-    // }
-    //
-    // return false;
-
   }
 
   sanitizeUri(data) {
@@ -248,28 +147,6 @@ export class IncomingDataService {
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
-  }
-
-  goToAmountPage(toAddress) {
-    //$state.go('tabs.send', {}, {'reload': true, 'notify': $state.current.name == 'tabs.send' ? false : true});
-    setTimeout(function() {
-      //$state.transitionTo('tabs.send.amount', {toAddress: toAddress});
-    }, 100);
-  }
-
-  handlePayPro(payProDetails){
-    // let stateParams = {
-    //   toAmount: payProDetails.amount,
-    //   toAddress: payProDetails.toAddress,
-    //   description: payProDetails.memo,
-    //   paypro: payProDetails
-    // };
-    this.scannerService.pausePreview();
-    // $state.go('tabs.send', {}, {'reload': true, 'notify': $state.current.name == 'tabs.send' ? false : true}).then(function() {
-    //   setTimeout(function() {
-    //     $state.transitionTo('tabs.send.confirm', stateParams);
-    //   });
-    // });
   }
 
 }
--- ionic2/src/services/services.ts
+++ ionic2/src/services/services.ts
@@ -1,8 +1,10 @@
+import { AppConfigService } from './app-config.service';
 import { IncomingDataService } from './incoming-data.service';
 import { PlatformInfo } from './platform-info.service';
 import { ScannerService } from './scanner.service';
 
 export const SERVICES = [
+  AppConfigService,
   IncomingDataService,
   PlatformInfo,
   ScannerService
