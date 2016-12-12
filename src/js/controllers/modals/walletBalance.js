7851188979396905ff20648b5664b38b32794ff0
Andy Phillipson <andy@bitpay.com>
2016-12-12 09:49:21 -0500

    Code cleanup.

--- src/js/controllers/modals/walletBalance.js
+++ src/js/controllers/modals/walletBalance.js
@@ -1,24 +1,9 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('walletBalanceController', function($scope, $timeout, $ionicHistory, gettextCatalog, configService, feeService, ongoingProcess, popupService) {
-
-  ongoingProcess.set('gettingFeeLevels', true);
-  feeService.getFeeLevels(function(err, levels) {
-    ongoingProcess.set('gettingFeeLevels', false);
-    if (err) {
-      popupService.showAlert(gettextCatalog.getString('Error'), err);
-      return;
-    }
-    $scope.feeLevels = levels;
-    $scope.$apply();
-  });
+angular.module('copayApp.controllers').controller('walletBalanceController', function($scope) {
 
   $scope.close = function() {
     $scope.walletBalanceModal.hide();
   };
 
-  $scope.$on("$ionicView.enter", function(event, data) {
-    $scope.feeOpts = feeService.feeOpts;
-    $scope.currentFeeLevel = feeService.getCurrentFeeLevel();
-  });
 });
