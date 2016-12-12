f4e79b372272d8f1534c26e1b99a54f967af8f6b
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2016-12-12 18:28:58 -0300

    Fix tab-settings and preferences language

--- src/js/controllers/tab-settings.js
+++ src/js/controllers/tab-settings.js
@@ -3,8 +3,6 @@
 angular.module('copayApp.controllers').controller('tabSettingsController', function($scope, $window, $ionicModal, $log, lodash, uxLanguage, platformInfo, profileService, feeService, configService, externalLinkService, bitpayCardService, storageService, glideraService) {
 
   var updateConfig = function() {
-
-    var config = configService.getSync();
     var isCordova = platformInfo.isCordova;
     var isWP = platformInfo.isWP;
     var isWindowsPhoneApp = platformInfo.isWP && isCordova;
@@ -13,21 +11,39 @@ angular.module('copayApp.controllers').controller('tabSettingsController', funct
     $scope.isCordova = isCordova;
 
     $scope.appName = $window.appConfig.nameCase;
-
-    $scope.unitName = config.wallet.settings.unitName;
     $scope.currentLanguageName = uxLanguage.getCurrentLanguageName();
-    $scope.selectedAlternative = {
-      name: config.wallet.settings.alternativeName,
-      isoCode: config.wallet.settings.alternativeIsoCode
-    };
     $scope.feeOpts = feeService.feeOpts;
     $scope.currentFeeLevel = feeService.getCurrentFeeLevel();
-
+    
     $scope.wallets = profileService.getWallets();
+    
+    configService.whenAvailable(function(config) {
+      $scope.unitName = config.wallet.settings.unitName;
+      $scope.selectedAlternative = {
+        name: config.wallet.settings.alternativeName,
+        isoCode: config.wallet.settings.alternativeIsoCode
+      };
 
-    $scope.bitpayCardEnabled = config.bitpayCard.enabled;
+      $scope.bitpayCardEnabled = config.bitpayCard.enabled;
+      $scope.glideraEnabled = config.glidera.enabled && !isWindowsPhoneApp;
 
-    $scope.glideraEnabled = config.glidera.enabled && !isWindowsPhoneApp;
+      if ($scope.bitpayCardEnabled) {
+        bitpayCardService.getBitpayDebitCards(function(err, data) {
+          if (err) $log.error(err);
+          if (!lodash.isEmpty(data)) {
+            $scope.bitpayCards = true;
+          }
+        });  
+      }
+
+      if ($scope.glideraEnabled) {
+        storageService.getGlideraToken(glideraService.getEnvironment(), function(err, token) {
+          if (err) $log.error(err);
+          $scope.glideraToken = token;
+        });
+      }
+
+    });
   };
 
   $scope.openExternalLink = function(url, optIn, title, message, okText, cancelText) {
@@ -36,18 +52,6 @@ angular.module('copayApp.controllers').controller('tabSettingsController', funct
 
   $scope.$on("$ionicView.beforeEnter", function(event, data) {
     updateConfig();
-
-    bitpayCardService.getBitpayDebitCards(function(err, data) {
-      if (err) $log.error(err);
-      if (!lodash.isEmpty(data)) {
-        $scope.bitpayCards = true;
-      }
-    });
-
-    storageService.getGlideraToken(glideraService.getEnvironment(), function(err, token) {
-      if (err) $log.error(err);
-      $scope.glideraToken = token;
-    });
   });
 
 });
