9234a575ec690fb6c381dddd4987139217a3802a
Gabriel Bazán <gabrielbazan7@hotmail.com>
2016-12-27 16:16:55 -0300

    improve design on payment protocol view

--- src/js/controllers/modals/paypro.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('copayApp.controllers').controller('payproController', function($scope) {
-  var self = $scope.self;
-
-  $scope.cancel = function() {
-    $scope.payproModal.hide();
-  };
-});
