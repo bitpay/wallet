1fcf0c64f501924cfadf3d3b1133ebf46166fa48
Gabriel Bazán <gabrielbazan7@hotmail.com>
2016-12-29 11:22:50 -0300

    adding UTF8 encoding support script

--- Gruntfile.js
+++ Gruntfile.js
@@ -115,6 +115,7 @@ module.exports = function(grunt) {
       angular: {
         src: [
           'bower_components/qrcode-generator/js/qrcode.js',
+          'bower_components/qrcode-generator/js/qrcode_UTF8.js',
           'bower_components/moment/min/moment-with-locales.js',
           'bower_components/angular-moment/angular-moment.js',
           'bower_components/ng-lodash/build/ng-lodash.js',
@@ -221,8 +222,8 @@ module.exports = function(grunt) {
         macPlist: {
           'CFBundleURLTypes': [
             {
-              'CFBundleURLName' : 'URI Handler',
-              'CFBundleURLSchemes' : ['bitcoin', '<%= pkg.name %>']
+              'CFBundleURLName': 'URI Handler',
+              'CFBundleURLSchemes': ['bitcoin', '<%= pkg.name %>']
             }
           ]
         }
