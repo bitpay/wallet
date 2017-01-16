a6b59a17e5b1bde4c6088339e5bf585e998d1e37
JDonadio <tanojaja@gmail.com>
2017-01-16 16:25:12 -0300

    update slide text button

--- src/js/controllers/confirm.js
+++ src/js/controllers/confirm.js
@@ -53,7 +53,29 @@ angular.module('copayApp.controllers').controller('confirmController', function(
     else $scope.network = (new bitcore.Address($scope.toAddress)).network.name;
     resetValues();
     setwallets();
+    applyButtonText();
   });
+
+  function applyButtonText(multisig) {
+    $scope.buttonText = $scope.isCordova ? gettextCatalog.getString('Slide') + ' ' : gettextCatalog.getString('Click') + ' ';
+
+    if ($scope.paypro) {
+      $scope.buttonText += gettextCatalog.getString('to pay');
+      return;
+    }
+
+    if ($scope.isGlidera || $scope.isGiftCard || $scope.cardId) {
+      $scope.buttonText += gettextCatalog.getString('to complete');
+      return;
+    }
+
+    if (multisig) {
+      $scope.buttonText += gettextCatalog.getString('to accept');
+      return;
+    }
+
+    $scope.buttonText += gettextCatalog.getString('to send');
+  };
 
   function setwallets() {
     $scope.wallets = profileService.getWallets({
@@ -268,6 +290,8 @@ angular.module('copayApp.controllers').controller('confirmController', function(
       $scope.getSendMaxInfo();
     } else
       setWallet(wallet);
+
+    applyButtonText(wallet.credentials.n > 1);
   };
 
   $scope.showDescriptionPopup = function() {
