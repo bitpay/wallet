84fd51b7089486b72df611b5d2e335101404e18a
Matias Alejo Garcia <ematiu@gmail.com>
2017-01-18 13:15:46 -0300

    add wallet indicator in preferences

--- src/js/controllers/preferencesAdvancedController.js
+++ src/js/controllers/preferencesAdvancedController.js
@@ -3,6 +3,9 @@
 angular.module('copayApp.controllers').controller('preferencesAdvancedController', function($scope, $timeout, $stateParams, profileService) {
   var wallet = profileService.getWallet($stateParams.walletId);
   $scope.network = wallet.network;
+  $scope.wallet = wallet;
+
+
 
   $timeout(function() {
     $scope.$apply();
