e4858e46eb75b21650d812d47591857d3951e1d6
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2017-01-25 16:27:51 -0300

    Show cards inmedatly after pairing (mobile issue)

--- src/js/controllers/tab-home.js
+++ src/js/controllers/tab-home.js
@@ -292,6 +292,9 @@ angular.module('copayApp.controllers').controller('tabHomeController',
           return;
         }
         $scope.bitpayCards = data;
+        $timeout(function() {
+          $scope.$digest();
+        }, 100);
       });
       bitpayCardService.getBitpayDebitCardsHistory(null, function(err, data) {
         if (err) return;
