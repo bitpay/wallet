a037c2b75232b9cd1dbe5e5c735b77d1e95ec9d9
Andy Phillipson <andy@bitpay.com>
2017-04-14 18:34:39 -0400

    Add missing service reference.

--- src/js/controllers/bitpayCardIntro.js
+++ src/js/controllers/bitpayCardIntro.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('copayApp.controllers').controller('bitpayCardIntroController', function($scope, $log, $state, $ionicHistory, storageService, externalLinkService, bitpayCardService, gettextCatalog, popupService, bitpayAccountService) {
+angular.module('copayApp.controllers').controller('bitpayCardIntroController', function($scope, $log, $state, $ionicHistory, storageService, externalLinkService, bitpayCardService, gettextCatalog, popupService, bitpayService, bitpayAccountService) {
 
   $scope.$on("$ionicView.beforeEnter", function(event, data) {
     if (data.stateParams && data.stateParams.secret) {
