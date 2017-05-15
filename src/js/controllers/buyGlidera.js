d5b0f5c7f38b005af6ed169d8fd4acc82d3f660c
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2017-05-15 14:26:49 -0300

    Disable swipe to back to avoid cancel while confirming

--- src/js/controllers/buyGlidera.js
+++ src/js/controllers/buyGlidera.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('buyGlideraController', function($scope, $log, $state, $timeout, $ionicHistory, lodash, glideraService, popupService, profileService, ongoingProcess, walletService, platformInfo, txFormatService) {
+angular.module('copayApp.controllers').controller('buyGlideraController', function($scope, $log, $state, $timeout, $ionicHistory, $ionicConfig, lodash, glideraService, popupService, profileService, ongoingProcess, walletService, platformInfo, txFormatService) {
 
   var amount;
   var currency;
@@ -34,6 +34,14 @@ angular.module('copayApp.controllers').controller('buyGlideraController', functi
       $scope.sendStatus = showName;
     }
   };
+
+  $scope.$on("$ionicView.beforeLeave", function(event, data) {
+    $ionicConfig.views.swipeBackEnabled(true);
+  });
+
+  $scope.$on("$ionicView.enter", function(event, data) {
+    $ionicConfig.views.swipeBackEnabled(false);
+  });
 
   $scope.$on("$ionicView.beforeEnter", function(event, data) {
     $scope.isFiat = data.stateParams.currency != 'bits' && data.stateParams.currency != 'BTC' ? true : false;
