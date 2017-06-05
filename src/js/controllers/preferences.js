80bc9a839fae6e86940cfa264d1216588d8b92b8
Gabriel Bazán <gabrielbazan7@hotmail.com>
2017-06-05 09:31:38 -0300

    fix spending password toggle

--- src/js/controllers/preferences.js
+++ src/js/controllers/preferences.js
@@ -3,11 +3,7 @@
 angular.module('copayApp.controllers').controller('preferencesController',
   function($scope, $rootScope, $timeout, $log, $ionicHistory, configService, profileService, fingerprintService, walletService) {
     var wallet;
-    var walletId; 
-
-    $scope.encryptEnabled = {
-      value: walletService.isEncrypted(wallet)
-    };
+    var walletId;
 
     $scope.hiddenBalanceChange = function() {
       var opts = {
@@ -22,6 +18,7 @@ angular.module('copayApp.controllers').controller('preferencesController',
 
     $scope.encryptChange = function() {
       if (!wallet) return;
+      console.log($scope.encryptEnabled.value);
       var val = $scope.encryptEnabled.value;
 
       if (val && !walletService.isEncrypted(wallet)) {
@@ -80,7 +77,7 @@ angular.module('copayApp.controllers').controller('preferencesController',
       wallet = profileService.getWallet(data.stateParams.walletId);
       walletId = wallet.credentials.walletId;
       $scope.wallet = wallet;
-      
+
       $scope.externalSource = null;
 
       if (!wallet)
@@ -90,6 +87,10 @@ angular.module('copayApp.controllers').controller('preferencesController',
 
       $scope.hiddenBalance = {
         value: $scope.wallet.balanceHidden
+      };
+
+      $scope.encryptEnabled = {
+        value: walletService.isEncrypted(wallet)
       };
 
       $scope.touchIdAvailable = fingerprintService.isAvailable();
