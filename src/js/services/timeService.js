46e430d2a5a413a5ccb8869cf0d21e84082591e6
Andy Phillipson <andy@bitpay.com>
2017-06-08 10:38:58 -0400

    Fixed layout, relative time. Standardize on use of timeService.

--- src/js/services/timeService.js
+++ src/js/services/timeService.js
@@ -5,20 +5,20 @@ angular.module('copayApp.services').factory('timeService', function() {
 
   root.withinSameMonth = function(time1, time2) {
     if (!time1 || !time2) return false;
-    var date1 = new Date(time1 * 1000);
-    var date2 = new Date(time2 * 1000);
-    return getMonthYear(date1) === getMonthYear(date2);
+    var date1 = new Date(time1);
+    var date2 = new Date(time2);
+    return root.getMonthYear(date1) === root.getMonthYear(date2);
   }
 
   root.withinPastDay = function(time) {
     var now = new Date();
-    var date = new Date(time * 1000);
+    var date = new Date(time);
     return (now.getTime() - date.getTime()) < (1000 * 60 * 60 * 24);
   };
 
   root.isDateInCurrentMonth = function(date) {
     var now = new Date();
-    return getMonthYear(now) === getMonthYear(date);
+    return root.getMonthYear(now) === root.getMonthYear(date);
   };
 
   root.getMonthYear = function(date) {
