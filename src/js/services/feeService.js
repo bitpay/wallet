e4799d639a2b6abeb934422b9853e841433292c7
Matias Alejo Garcia <ematiu@gmail.com>
2017-06-22 13:44:49 -0300

    add low amount in txhistory/details and balance

--- src/js/services/feeService.js
+++ src/js/services/feeService.js
@@ -4,7 +4,6 @@ angular.module('copayApp.services').factory('feeService', function($log, $timeou
   var root = {};
 
   var CACHE_TIME_TS = 60; // 1 min
-  var LOW_AMOUNT_RATIO = 0.15; //Ratio low amount warning (econ fee/amount)
 
   // Constant fee options to translate
   root.feeOpts = {
@@ -82,52 +81,6 @@ angular.module('copayApp.services').factory('feeService', function($log, $timeou
     });
   };
 
-
-  // These 2 functions were taken from
-  // https://github.com/bitpay/bitcore-wallet-service/blob/master/lib/model/txproposal.js#L243
-
-  function getEstimatedSizeForSingleInput(wallet) {
-    switch (wallet.credentials.addressType) {
-      case 'P2PKH':
-        return 147;
-      default:
-      case 'P2SH':
-        return wallet.m * 72 + wallet.n * 36 + 44;
-    }
-  };
-
-
-  function getEstimatedSize(wallet) {
-    // Note: found empirically based on all multisig P2SH inputs and within m & n allowed limits.
-    var safetyMargin = 0.02;
-
-    var overhead = 4 + 4 + 9 + 9;
-    var inputSize = getEstimatedSizeForSingleInput(wallet);
-    var outputSize = 34;
-    var nbInputs = 1; //Assume 1 input
-    var nbOutputs = 2; // Assume 2 outputs
-
-    var size = overhead + inputSize * nbInputs + outputSize * nbOutputs;
-    return parseInt((size * (1 + safetyMargin)).toFixed(0));
-  };
-
-
-  // Approx utxo amount, from which the uxto is economically redeemable  
-  root.getLowAmount = function(wallet, cb) {
-    root.getFeeLevels(function(err, levels) {
-      if (err) return cb(err);
-
-      var lowLevelRate = (lodash.find(levels[wallet.network], {
-        level: 'economy',
-      }).feePerKB / 1000).toFixed(0);
-
-      var size = getEstimatedSize(wallet);
-
-      var minFee = size * lowLevelRate;
-
-      return cb(null, parseInt(minFee / (LOW_AMOUNT_RATIO)));
-    });
-  };
 
   return root;
 });
