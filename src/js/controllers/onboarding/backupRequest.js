8c6e9e4386bc0310da4c75372fe58550d949df35
bitlox <dana.coe@bitlox.com>
2017-07-12 13:57:35 +0100

    Views edited, mostly onboarding

--- src/js/controllers/onboarding/backupRequest.js
+++ src/js/controllers/onboarding/backupRequest.js
@@ -15,7 +15,7 @@ angular.module('copayApp.controllers').controller('backupRequestController', fun
   $scope.openPopup = function() {
 
     var title = gettextCatalog.getString('Watch out!');
-    var message = gettextCatalog.getString('If this device is replaced or this app is deleted, neither you nor BitPay can recover your funds without a backup.');
+    var message = gettextCatalog.getString('If this device is replaced or this app is deleted, neither you nor anyone else can recover your funds without a backup.');
     var okText = gettextCatalog.getString('I understand');
     var cancelText = gettextCatalog.getString('Go back');
     popupService.showConfirm(title, message, okText, cancelText, function(val) {
