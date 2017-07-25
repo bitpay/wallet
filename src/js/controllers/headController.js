f9dccaf9ae5ee15a3610eb8320c227455d19434c
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2017-07-25 14:11:03 -0300

    Remove all files

--- src/js/controllers/headController.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('copayApp.controllers').controller('headController', function($scope, appConfigService, $log) {
-  $scope.appConfig = appConfigService;
-  $log.info('Running head controller:' + appConfigService.nameCase)
-});
--- src/js/services/logHeader.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('copayApp.services')
-  .factory('logHeader', function($window, appConfigService, $log, platformInfo) {
-    $log.info(appConfigService.nameCase + ' v' + $window.version + ' #' + $window.commitHash);
-    $log.info('Client: ' + JSON.stringify(platformInfo));
-    return {};
-  });
