3e35f4ebe4fc333be901a46d71db3bef3b2a50a8
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2017-07-27 13:03:11 -0300

    New about page

---
+++ src/pages/about/about.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html',
+})
+export class AboutPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AboutPage');
+  }
+
+}
