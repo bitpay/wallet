85a986445002a80f395337e7f1a7e57f23f68a5b
Gabriel Bazán <gabrielbazan7@hotmail.com>
2017-07-31 10:41:57 -0300

    cleaning gh-pages branch

--- js/main.js
+++
@@ -1,6 +0,0 @@
-$(document)
-  .foundation()
-  .ready(function() {
-  	if(window.location.hash.match('uri-payment')) { window.location.href = '/in/app' + window.location.hash;}
-	$('header').fadeIn('slow');
-  });
