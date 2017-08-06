d0be7c84d38093d26a2425ec8ad7f661a74885f9
dave <dave>
2017-08-06 20:08:52 +0800

    custom networks

--- src/js/bitlox/util/customNetworks.constants.js
+++ src/js/bitlox/util/customNetworks.constants.js
@@ -1,34 +1,52 @@
-(function(window, angular) {
-    'use strict';
+(function (window, angular) {
+  'use strict'
 
-    angular.module('app.util')
+  angular.module('app.util')
         .constant('CUSTOMNETWORKS', {
-        livenet: {
-          network: 'livenet',
-          name: 'livenet',
-          alias: 'Bitcoin',
-          code: 'btc',
-          symbol: 'BTC',
-          derivationCoinPath: "0",
-          pubkeyhash: 0x00,
-          privatekey: 0x80,
-          scripthash: 0x05,
-          xpubkey: 0x0488b21e,
-          xprivkey: 0x0488ade4,
-          networkMagic: 0xf9beb4d9,
-          port: 8333,
-          bwsUrl: 'https://bws.bitlox.com/bws/api/',
-          explorer: 'https://bitlox.io/',
-          dnsSeeds: [
-            'seed.bitcoin.sipa.be',
-            'dnsseed.bluematt.me',
-            'dnsseed.bitcoin.dashjr.org',
-            'seed.bitcoinstats.com',
-            'seed.bitnodes.io',
-            'bitseed.xf2.org'
-          ]          
-          
-        }}
-)
+          deuscoin: {
+            'network': 'deuscoin',
+            'name': 'deuscoin',
+            'alias': 'Deuscoin',
+            'code': 'deus',
+            'symbol': 'DEUS',
+            'derivationCoinPath': 146,
+            'pubkeyhash': 0x1e,
+            'privatekey': 0x80,
+            'scripthash': 0x23,
+            'xpubkey': 0x0488b21e,
+            'xprivkey': 0x0488ade4,
+            'bwsUrl': 'https://bws.deuscoin.org/bws/api',
+            'port': 19697,
+            'networkMagic': 0x9ee8bc5a,
+            'explorer': 'https://explorer.deuscoin.org/'
 
-})(window, window.angular);
+          },
+          livenet: {
+            network: 'livenet',
+            name: 'livenet',
+            alias: 'Bitcoin',
+            code: 'btc',
+            symbol: 'BTC',
+            derivationCoinPath: '0',
+            pubkeyhash: 0x00,
+            privatekey: 0x80,
+            scripthash: 0x05,
+            xpubkey: 0x0488b21e,
+            xprivkey: 0x0488ade4,
+            networkMagic: 0xf9beb4d9,
+            port: 8333,
+            bwsUrl: 'https://bws.bitlox.com/bws/api/',
+            explorer: 'https://bitlox.io/',
+            dnsSeeds: [
+              'seed.bitcoin.sipa.be',
+              'dnsseed.bluematt.me',
+              'dnsseed.bitcoin.dashjr.org',
+              'seed.bitcoinstats.com',
+              'seed.bitnodes.io',
+              'bitseed.xf2.org'
+            ]
+
+          }
+        }
+)
+})(window, window.angular)
