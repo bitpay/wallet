3d5d64ff1454a63acaa7bc270e0e80f3fbc7ab19
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2017-08-15 09:52:20 -0300

    Removes travis for now. Ref app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
-import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule, Http } from '@angular/http';
+import { ErrorHandler, NgModule, APP_INITIALIZER } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 
+/* Native modules */
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Toast } from '@ionic-native/toast';
+import { Clipboard } from '@ionic-native/clipboard';
+import { SocialSharing } from '@ionic-native/social-sharing';
+
+/* Modules */
 import { NgLoggerModule, Logger, Level } from '@nsalaun/ng-logger';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslatePoHttpLoader } from '@biesbjerg/ngx-translate-po-http-loader';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
 
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { Copay } from './app.component';
+/* Copay App */
+import { CopayApp } from './app.component';
 
+/* Pages */
 import { HomePage } from '../pages/home/home';
 import { ReceivePage } from '../pages/receive/receive';
 import { SendPage } from '../pages/send/send';
@@ -18,9 +29,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { AboutPage } from '../pages/about/about';
 import { TermsOfUsePage } from '../pages/terms-of-use/terms-of-use';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-
+/* Providers */
 import { WalletProvider } from '../providers/wallet/wallet';
 import { PersistenceProvider, persistenceProviderFactory } from '../providers/persistence/persistence';
 import { AppProvider } from '../providers/app/app';
@@ -32,7 +41,7 @@ export function createTranslateLoader(http: Http) {
 
 @NgModule({
   declarations: [
-    Copay,
+    CopayApp,
     HomePage,
     ReceivePage,
     SendPage,
@@ -45,18 +54,19 @@ export function createTranslateLoader(http: Http) {
     BrowserModule,
     HttpModule,
     NgLoggerModule.forRoot(Level.LOG),
+    NgxQRCodeModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
+        useFactory: createTranslateLoader,
         deps: [Http]
       }
     }),
-    IonicModule.forRoot(Copay)
+    IonicModule.forRoot(CopayApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    Copay,
+    CopayApp,
     HomePage,
     ReceivePage,
     SendPage,
@@ -68,9 +78,20 @@ export function createTranslateLoader(http: Http) {
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    Clipboard,
+    Toast,
+    SocialSharing,
+    {
+      provide: ErrorHandler,
+      useClass: IonicErrorHandler
+    },
+    {
+      provide: PersistenceProvider,
+      useFactory: persistenceProviderFactory,
+      deps: [PlatformProvider, Logger],
+      multi: true
+    },
     WalletProvider,
-    { provide: PersistenceProvider, useFactory: persistenceProviderFactory, deps: [PlatformProvider, Logger], multi: true },
     AppProvider,
     PlatformProvider
   ]
