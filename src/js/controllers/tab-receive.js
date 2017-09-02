cronnelo/copay

remove various hardcoded bitcoin or altcoin references

src/js/controllers/tab-receive.js
@@ -137,8 +137,8 @@ angular.module('copayApp.controllers').controller('tabReceiveController', functi
     $scope.showWallets = true;
   };
 
-  $scope.copyToClipboard = function() {
-    if ($scope.isCordova) return 'bitcoin:' + $scope.addr;
+  $scope.copyToClipboard = function(network) {
+    if ($scope.isCordova) return network+':' + $scope.addr;
     else return $scope.addr;
   }
 });

bitpay/wallet

src/js/controllers/tab-receive.js
@@ -137,8 +137,8 @@ angular.module('copayApp.controllers').controller('tabReceiveController', functi
     $scope.showWallets = true;
   };
 
-  $scope.shareAddress = function() {
+  $scope.shareAddress = function(network) {
     if (!$scope.isCordova) return;
-    window.plugins.socialsharing.share('bitcoin:' + $scope.addr, null, null, null);
+    window.plugins.socialsharing.share(network+':' + $scope.addr, null, null, null);
   }
 });