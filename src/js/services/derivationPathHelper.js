0e4a629e68cadf13c62288b577082e6965bb9f57
hiram <hiramabiff@eastgate.com>
2017-09-21 20:18:31 +0800

    add getStatic to custom networks in more places"

--- src/js/services/derivationPathHelper.js
+++ src/js/services/derivationPathHelper.js
@@ -7,58 +7,56 @@ angular.module('copayApp.services').factory('derivationPathHelper', ['lodash', '
   root.defaultTestnet = "m/44'/1'/0'";
 
   root.getDefault = function(networkName) {
-    return customNetworks.getAll().then(function(CUSTOMNETWORKS) {
-      for (var i in CUSTOMNETWORKS) {
-        if(i === networkName) {
-          return "m/44'/"+CUSTOMNETWORKS[i].derivationCoinPath+"'/0'";
-        }
+    var CUSTOMNETWORKS = customNetworks.getStatic()
+    for (var i in CUSTOMNETWORKS) {
+      if(i === networkName) {
+        return "m/44'/"+CUSTOMNETWORKS[i].derivationCoinPath+"'/0'";
       }
-      return null;      
-    })
+    }
+    return null;      
   }
 
 
   root.parse = function(str) {
-    return customNetworks.getAll().then(function(CUSTOMNETWORKS) {
-      var arr = str.split('/');
+    var CUSTOMNETWORKS = customNetworks.getStatic()
+    var arr = str.split('/');
 
-      var ret = {};
+    var ret = {};
 
-      if (arr[0] != 'm')
-        return false;
+    if (arr[0] != 'm')
+      return false;
 
-      switch (arr[1]) {
-        case "44'":
-          ret.derivationStrategy = 'BIP44';
-          break;
-        case "45'":
-          return {
-            derivationStrategy: 'BIP45',
-            networkName: 'livenet',
-            account: 0,
-          }
-          break;
-        case "48'":
-          ret.derivationStrategy = 'BIP48';
-          break;
-        default:
-          return false;
-      };
-
-      ret.networkName = '';
-
-      for(var i in CUSTOMNETWORKS) {
-        if(CUSTOMNETWORKS[i].derivationCoinPath+"'" === arr[2]) {
-          ret.networkName = CUSTOMNETWORKS[i].name
+    switch (arr[1]) {
+      case "44'":
+        ret.derivationStrategy = 'BIP44';
+        break;
+      case "45'":
+        return {
+          derivationStrategy: 'BIP45',
+          networkName: 'livenet',
+          account: 0,
         }
-      }
-
-      var match = arr[3].match(/(\d+)'/);
-      if (!match)
+        break;
+      case "48'":
+        ret.derivationStrategy = 'BIP48';
+        break;
+      default:
         return false;
-      ret.account = +match[1]
-      return ret;
-    })
+    };
+
+    ret.networkName = '';
+
+    for(var i in CUSTOMNETWORKS) {
+      if(CUSTOMNETWORKS[i].derivationCoinPath+"'" === arr[2]) {
+        ret.networkName = CUSTOMNETWORKS[i].name
+      }
+    }
+
+    var match = arr[3].match(/(\d+)'/);
+    if (!match)
+      return false;
+    ret.account = +match[1]
+    return ret;
   };
 
   return root;
