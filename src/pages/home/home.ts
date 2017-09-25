fe9d4e7345537b31f36693b5911f0032f665ce53
Gabriel Bazán <gabrielbazan7@hotmail.com>
2017-09-25 11:48:55 -0300

    fixes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ProfileProvider } from '../../providers/profile/profile';
-import { PopupProvider } from '../../providers/popup/popup';
 
 @Component({
   selector: 'page-home',
@@ -12,8 +11,7 @@ export class HomePage {
 
   constructor(
     public navCtrl: NavController,
-    private profile: ProfileProvider,
-    private popup: PopupProvider,
+    private profile: ProfileProvider
   ) {
   }
 
@@ -21,11 +19,5 @@ export class HomePage {
     console.log('ionViewDidLoad HomePage');
     this.wallets = this.profile.bind();
     console.log('[home.ts:20]', this.wallets); //TODO
-  }
-
-  openPopup() {
-    //this.popup._ionicAlert('Hola mi nombre es gabriel', 'si, ese es mi nombre', 'Si');
-    //this.popup._ionicConfirm('hola aceptas este dinerillo?', 'dinero gratis', 'Si', 'cancel');
-    //this.popup._ionicPrompt('Ingrese un valor', 'Ingreselo por favor', 'Si', 'no', null);
   }
 }
