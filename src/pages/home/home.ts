414e1911ece4d4c0a9ea812b560f020d35710168
Gabriel Bazán <gabrielbazan7@hotmail.com>
2017-09-25 11:39:29 -0300

    Feat: popup provider added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ProfileProvider } from '../../providers/profile/profile';
+import { PopupProvider } from '../../providers/popup/popup';
 
 @Component({
   selector: 'page-home',
@@ -11,13 +12,20 @@ export class HomePage {
 
   constructor(
     public navCtrl: NavController,
-    private profile: ProfileProvider
+    private profile: ProfileProvider,
+    private popup: PopupProvider,
   ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
     this.wallets = this.profile.bind();
-    console.log('[home.ts:20]',this.wallets); //TODO
+    console.log('[home.ts:20]', this.wallets); //TODO
+  }
+
+  openPopup() {
+    //this.popup._ionicAlert('Hola mi nombre es gabriel', 'si, ese es mi nombre', 'Si');
+    //this.popup._ionicConfirm('hola aceptas este dinerillo?', 'dinero gratis', 'Si', 'cancel');
+    //this.popup._ionicPrompt('Ingrese un valor', 'Ingreselo por favor', 'Si', 'no', null);
   }
 }
