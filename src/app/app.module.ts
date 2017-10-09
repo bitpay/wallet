a326b887dcd8712d1616f25628691f2557344022
JDonadio <tanojaja@gmail.com>
2017-10-09 12:39:26 -0300

    show form fields depending on wallet key selected - ref name/folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import { CopayApp } from './app.component';
 
 /* Pages */
 import { AddPage } from '../pages/add/add';
-import { SingleWalletPage } from '../pages/add/single-wallet/single-wallet';
+import { SingleWalletPage } from '../pages/add/create-wallet/single-wallet/single-wallet';
 import { BackupRequestPage } from '../pages/onboarding/backup-request/backup-request';
 import { DisclaimerPage } from '../pages/onboarding/disclaimer/disclaimer';
 import { EmailPage } from '../pages/onboarding/email/email';
