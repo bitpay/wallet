7455440abf1f7f1971580f71a95e5c15dadcaee8
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2017-10-20 16:12:03 -0300

    Fix onboarding. Clear styling

--- src/pages/onboarding/email/email.ts
+++ src/pages/onboarding/email/email.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ActionSheetController } from 'ionic-angular';
+import { Logger } from '@nsalaun/ng-logger';
 
 import { BackupRequestPage } from '../backup-request/backup-request';
 
@@ -11,12 +12,21 @@ export class EmailPage {
   public data: any;
   public showConfirmForm: boolean;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public actionSheet: ActionSheetController) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public actionSheet: ActionSheetController,
+    private log: Logger
+  ) {
     this.data = {
       accept: true,
       email: '',
     };
     this.showConfirmForm = false;
+  }
+
+  ionViewDidLoad() {
+    this.log.info('ionViewDidLoad EmailPage');
   }
 
   skip() {
@@ -39,6 +49,7 @@ export class EmailPage {
   }
 
   showConfirm() {
+    // TODO Fix form validation
     if (!this.data.email) return;
     this.showConfirmForm = !this.showConfirmForm;
   }
