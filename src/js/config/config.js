baee08d2a2819f2c3af289b1f6f7b4dec59d6351
user <user@users-iMac.local>
2017-10-25 16:27:06 +0900

    change color

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -1,25 +1,25 @@
 var DEFAULT_CONFIG = {
     networkName: 'ventas',
     coin: 'ven',
-    bwsurl: 'http://bws-m.bitchk.com/bws/api',
+    bwsurl: 'http://bws.bitchk.com/bws/api',
     showMoney: false,
     networks: ['livenet', 'ventas', 'litecoin']
 }
 var COIN_CONFIG = {
     btc: {
-        bwsurl: 'http://bws-m.bitchk.com/bws/api',
+        bwsurl: 'http://bws.bitchk.com/bws/api',
         insight: 'https://insight.bitpay.com'
     },
     ltc: {
-        bwsurl: 'http://bws-m.bitchk.com/bws/api',
+        bwsurl: 'http://bws.bitchk.com/bws/api',
         insight: 'http://insight-ltc.bitchk.com/insight'
     },
     ven: {
-        bwsurl: 'http://bws-m.bitchk.com/bws/api',
+        bwsurl: 'http://bws.bitchk.com/bws/api',
         insight: 'http://insight.ventasnu.com'
     },
     tbc: {
-        bwsurl: 'http://bws-m.bitchk.com/bws/api',
-        insight: 'http://insight.teracoex.com'
+        bwsurl: 'http://bws.bitchk.com/bws/api',
+        insight: 'http://insight.teracoex.com/insight'
     },
 }
--- src/js/config/config.prod.js
+++ src/js/config/config.prod.js
@@ -1,25 +1,29 @@
 var DEFAULT_CONFIG = {
     networkName: 'ventas',
     coin: 'ven',
-    bwsurl: 'http://bws-m.bitchk.com/bws/api',
+    bwsurl: 'http://bws.bitchk.com/bws/api',
     showMoney: false,
     networks: ['livenet', 'ventas', 'litecoin']
 }
 var COIN_CONFIG = {
     btc: {
-        bwsurl: 'http://bws-m.bitchk.com/bws/api',
-        insight: 'https://insight.bitpay.com'
+        bwsurl: 'http://bws.bitchk.com/bws/api',
+        insight: 'https://insight.bitpay.com',
+        network: 'litvenet'
     },
     ltc: {
-        bwsurl: 'http://bws-m.bitchk.com/bws/api',
-        insight: 'http://localhost:3001/insight'
+        bwsurl: 'http://bws.bitchk.com/bws/api',
+        insight: 'http://localhost:3001/insight',
+        network: 'ltcnet'
     },
     ven: {
-        bwsurl: 'http://bws-m.bitchk.com/bws/api',
-        insight: 'http://insight.ventasnu.com'
+        bwsurl: 'http://bws.bitchk.com/bws/api',
+        insight: 'http://insight.ventasnu.com',
+        network: 'ventas'
     },
     tbc: {
-        bwsurl: 'http://bws-m.bitchk.com/bws/api',
-        insight: 'http://insight.teracoex.com'
+        bwsurl: 'http://bws.bitchk.com/bws/api',
+        insight: 'http://insight.teracoex.com/insight',
+        network: 'terabit'
     },
 }
