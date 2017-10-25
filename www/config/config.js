b29706cc965bd4edc0979708ed65af9d86adca84
user <user@users-iMac.local>
2017-10-25 14:10:44 +0900

    version

--- www/config/config.js
+++ www/config/config.js
@@ -1,2 +1,11 @@
 var DEPLOY_SERVER = 'http://wallet.bitchk.com';
 var REMOTE_LOAD = true;
+var CFG = {
+    "defult": {
+        DEPLOY_SERVER: 'http://wallet.bitchk.com',
+        REMOTE_LOAD: true;
+    },
+    "3.7.2": {
+
+    }
+};
