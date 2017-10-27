d0b9b759ea59672bebd91970d6f4154b565a48dc
alex v <alexvazquezmusic@me.com>
2017-10-28 01:20:14 +0200

    NAV: Use cryptoid as block explorer

--- src/js/controllers/tx-details.js
+++ src/js/controllers/tx-details.js
@@ -166,11 +166,11 @@ angular.module('copayApp.controllers').controller('txDetailsController', functio
 
   $scope.viewOnBlockchain = function() {
     var btx = $scope.btx;
-    var url = 'https://' + ($scope.getShortNetworkName() == 'test' ? 'test-' : '') + 'insight.bitpay.com/tx/' + btx.txid;
+    var url = 'https://chainz.cryptoid.info/nav/tx.dws?' + btx.txid;
     var optIn = true;
     var title = null;
-    var message = gettextCatalog.getString('View Transaction on Insight');
-    var okText = gettextCatalog.getString('Open Insight');
+    var message = gettextCatalog.getString('View Transaction on Block Explorer');
+    var okText = gettextCatalog.getString('Open cryptoID');
     var cancelText = gettextCatalog.getString('Go Back');
     externalLinkService.open(url, optIn, title, message, okText, cancelText);
   };
