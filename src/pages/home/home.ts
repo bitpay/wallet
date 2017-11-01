7a08ad45b124b14356e569b67e2273edd5b46fd5
JDonadio <tanojaja@gmail.com>
2017-11-01 15:07:00 -0300

    remove logic from view - add translates and styles

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,7 @@ import { ReleaseProvider } from '../../providers/release/release';
 import { WalletProvider } from '../../providers/wallet/wallet';
 import { BwcErrorProvider } from '../../providers/bwc-error/bwc-error';
 import * as _ from 'lodash';
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-home',
@@ -33,6 +34,10 @@ export class HomePage {
   private updateAllWallets(): void {
     _.each(this.wallets, (wallet: any) => {
       this.walletProvider.getStatus(wallet, {}).then((status: any) => {
+        const balanceStr = status.totalBalanceStr ? wallet.status.totalBalanceStr : '';
+        const cachedBalanceStr = wallet.cachedBalance ? wallet.cachedBalance : '';
+        const cachedBalanceUpdateOn = wallet.cachedBalanceUpdatedOn ? ' - ' + moment(wallet.cachedBalanceUpdatedOn * 1000).fromNow() : '';
+        wallet.statusStr = balanceStr || cachedBalanceStr + cachedBalanceUpdateOn;
         wallet.status = status;
         this.profileProvider.setLastKnownBalance(wallet.id, wallet.status.totalBalanceStr);
       }).catch((err) => {
