2ed76da586333c50253bdb53652a4d941babe96e
Craig <craig.b.macgregor@gmail.com>
2017-11-03 15:34:41 +1300

    chrome app dimensions adjusted to match desktop

--- chrome-app/initial.js
+++ chrome-app/initial.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     'bounds': {
-      'width': 400,
-      'height': 600
+      'width': 650,
+      'height': 450
     }
   });
 });
