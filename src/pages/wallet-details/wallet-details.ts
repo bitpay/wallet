ac6e633b88b20f97cf23af6226d83adce1683182
JDonadio <tanojaja@gmail.com>
2017-11-03 11:21:56 -0300

    add navigation with param: wallet

--- src/pages/wallet-details/wallet-details.ts
+++ src/pages/wallet-details/wallet-details.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-wallet-details',
   templateUrl: 'wallet-details.html'
 })
 export class WalletDetailsPage {
-  
+  public wallet: any;
+
+  constructor(
+    private navParams: NavParams,
+  ) {
+    this.wallet = this.navParams.data.wallet;
+  }
+
+  ionViewDidEnter() {}
 }
