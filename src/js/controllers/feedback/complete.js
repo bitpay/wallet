94ee59aca6c80827dc5b52dc4edffbf668db750f
Matt <matt@encrypt-s.com>
2017-11-06 15:36:40 +1300

    updated a variable comparison so it passes with 'navpay' instead of 'copay'. fixes a sharing issue

--- src/js/controllers/feedback/complete.js
+++ src/js/controllers/feedback/complete.js
@@ -7,7 +7,7 @@ angular.module('copayApp.controllers').controller('completeController', function
   });
 
   var defaults = configService.getDefaults();
-  var downloadUrl = appConfigService.name == 'copay' ? defaults.download.copay.url : defaults.download.bitpay.url;
+  var downloadUrl = appConfigService.name == 'navpay' ? defaults.download.copay.url : defaults.download.bitpay.url;
 
   function quickFeedback(cb) {
     window.plugins.spinnerDialog.show();
