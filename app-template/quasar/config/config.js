00dc19938a23a2b6a63597c1a1553138de05ea20
user <>
2017-11-20 21:48:36 +0900

    change default coin

--- app-template/quasar/config/config.js
+++ app-template/quasar/config/config.js
@@ -6,51 +6,6 @@ var DEFAULT_CONFIG = {
     networks: ['quasar']
 }
 var COIN_CONFIG = {
-    btc: {
-        bwsurl: 'https://bws.bitchk.com/bws/api',
-        insight: 'http://insight-btc.bitchk.com',
-        network: 'litvenet',
-        coin: 'btc',
-        displayName: "BTC",
-        enable: true,
-        coinName: "Bitcoin"
-    },
-    ltc: {
-        bwsurl: 'http://bws.bitchk.com/bws/api',
-        insight: 'http://insight-ltc.bitchk.com/insight',
-        network: 'litecoin',
-        coin: 'ltc',
-        displayName: "LTC",
-        enable: true,
-        coinName: "Litecoin"
-    },
-    ven: {
-        bwsurl: 'https://bws.bitchk.com/bws/api',
-        insight: 'http://insight.ventasnu.com',
-        coin: 'ven',
-        displayName: "VEN",
-        coinName: "VENTAS",
-        network: "ventas",
-        enable: true,
-    },
-    yng: {
-        bwsurl: 'https://bws.bitchk.com/bws/api',
-        insight: 'https://insight-yng.bitchk.com',
-        coin: 'yng',
-        displayName: "YNG",
-        coinName: "YangCoin",
-        network: "yangcoin",
-        enable: true,
-    },
-    qct: {
-        bwsurl: 'https://bws.bitchk.com/bws/api',
-        insight: 'https://insight-qct.bitchk.com',
-        coin: 'qct',
-        displayName: "QCT",
-        coinName: "QCity",
-        network: "qctcoin",
-        enable: true,
-    },
     qac: {
         bwsurl: 'https://bws-qac.bitchk.com/bws/api',
         insight: 'https://insight-qac.bitchk.com',
@@ -58,24 +13,6 @@ var COIN_CONFIG = {
         displayName: "QAC",
         coinName: "Quasar",
         network: "quasar",
-        enable: true,
-    },
-    pax: {
-        bwsurl: 'https://bws-pax.bitchk.com/bws/api',
-        insight: 'https://insight-pax.bitchk.com',
-        coin: 'pax',
-        displayName: "PAX",
-        coinName: "Paxcoin",
-        network: "paxcoin",
-        enable: true,
-    },
-    ssc: {
-        bwsurl: 'https://bws-ssc.bitchk.com/bws/api',
-        insight: 'https://insight-ssc.bitchk.com',
-        coin: 'ssc',
-        displayName: "SSC",
-        coinName: "SearchCoin",
-        network: "searchcoin",
         enable: true,
     }
 }
