75b495fe3a4ce81c1152b58ac4a2caee93f01a6a
Gabriel Bazán <gabrielbazan7@hotmail.com>
2018-01-12 13:15:09 -0300

    [V4] FIX: onboarding tour btc price

--- src/pages/onboarding/tour/tour.ts
+++ src/pages/onboarding/tour/tour.ts
@@ -2,8 +2,14 @@ import { Component, ViewChild } from '@angular/core';
 import { NavController, LoadingController, Slides, Navbar } from 'ionic-angular';
 import { Logger } from '@nsalaun/ng-logger';
 
+//pages
 import { CollectEmailPage } from '../collect-email/collect-email';
+
+//providers
 import { ProfileProvider } from '../../../providers/profile/profile';
+import { RateProvider } from '../../../providers/rate/rate';
+import { TxFormatProvider } from '../../../providers/tx-format/tx-format';
+import { OnGoingProcessProvider } from '../../../providers/on-going-process/on-going-process';
 
 @Component({
   selector: 'page-tour',
@@ -13,49 +19,56 @@ export class TourPage {
   @ViewChild(Slides) slides: Slides;
   @ViewChild(Navbar) navBar: Navbar;
 
-  public currentIndex: number = 0;
+  public localCurrencySymbol: string;
+  public localCurrencyPerBtc: string;
+  public currentIndex: number;
 
   constructor(
     public navCtrl: NavController,
     public loadingCtrl: LoadingController,
     private logger: Logger,
-    private profileProvider: ProfileProvider
+    private profileProvider: ProfileProvider,
+    private rateProvider: RateProvider,
+    private txFormatProvider: TxFormatProvider,
+    private onGoingProcessProvider: OnGoingProcessProvider
   ) {
+    this.currentIndex = 0;
+    this.rateProvider.whenRatesAvailable().then(() => {
+      let btcAmount = 1;
+      this.localCurrencySymbol = '$'
+      this.localCurrencyPerBtc = this.txFormatProvider.formatAlternativeStr('btc', btcAmount * 1e8);
+    });
   }
 
   ionViewDidLoad() {
     this.logger.info('ionViewDidLoad TourPage');
   }
 
-  ngOnInit() {
+  ionViewWillEnter() {
     this.navBar.backButtonClick = (e: UIEvent) => {
       this.slidePrev();
     }
   }
 
-  slideChanged() {
+  public slideChanged(): void {
     this.currentIndex = this.slides.getActiveIndex();
   }
 
-  slidePrev() {
+  public slidePrev(): void {
     if (this.currentIndex == 0) this.navCtrl.pop();
     else {
       this.slides.slidePrev();
     }
   }
 
-  slideNext() {
+  public slideNext(): void {
     this.slides.slideNext();
   }
 
-  createDefaultWallet() {
-    let loading = this.loadingCtrl.create({
-      content: 'Creating Personal Wallet...'
-    });
-    loading.present();
-
+  public createDefaultWallet(): void {
+    this.onGoingProcessProvider.set('creatingWallet', true);
     this.profileProvider.createDefaultWallet().then((wallet) => {
-      loading.dismiss();
+      this.onGoingProcessProvider.set('creatingWallet', false);
       this.navCtrl.push(CollectEmailPage, { walletId: wallet.id });
     })
   }
