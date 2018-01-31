cc8d75a2f5df227a03b2940fd2cca585e4a7a26b
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2018-01-31 19:35:26 -0300

    Add settings for Coinbase

--- src/pages/integrations/coinbase/coinbase-settings/coinbase-settings.ts
+++ src/pages/integrations/coinbase/coinbase-settings/coinbase-settings.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { ViewController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import * as _ from 'lodash';
 
+import { Logger } from '../../../../providers/logger/logger';
+import { OnGoingProcessProvider } from '../../../../providers/on-going-process/on-going-process';
+import { PopupProvider } from '../../../../providers/popup/popup';
 import { CoinbaseProvider } from '../../../../providers/coinbase/coinbase';
 
 @Component({
@@ -9,11 +13,58 @@ import { CoinbaseProvider } from '../../../../providers/coinbase/coinbase';
 })
 export class CoinbaseSettingsPage {
 
+  public coinbaseAccount: any;
+  public coinbaseUser: any;
+
   constructor(
-    public viewCtrl: ViewController,
-    public coinbaseProvider: CoinbaseProvider,
-    public navParams: NavParams
+    private navCtrl: NavController,
+    private onGoingProcessProvider: OnGoingProcessProvider,
+    private popupProvider: PopupProvider,
+    private logger: Logger,
+    private coinbaseProvider: CoinbaseProvider
   ) {
   }
+
+  ionViewDidLoad() {
+    this.onGoingProcessProvider.set('connectingCoinbase', true);
+    this.coinbaseProvider.init((err, data) => {
+      if (err || _.isEmpty(data)) {
+        this.onGoingProcessProvider.set('connectingCoinbase', false);
+        if (err) {
+          this.logger.error(err);
+          let errorId = err.errors ? err.errors[0].id : null;
+          err = err.errors ? err.errors[0].message : err;
+          this.popupProvider.ionicAlert('Error connecting to Coinbase', err).then(() => {
+            if (errorId == 'revoked_token') {
+              this.coinbaseProvider.logout();
+              this.navCtrl.popToRoot();
+            }
+          });
+        }
+        return;
+      }
+      let accessToken = data.accessToken;
+      let accountId = data.accountId;
+      this.coinbaseProvider.getAccount(accessToken, accountId, (err, account) => {
+        this.onGoingProcessProvider.set('connectingCoinbase', false);
+        this.coinbaseAccount = account.data[0];
+      });
+      this.coinbaseProvider.getCurrentUser(accessToken, (err, user) => {
+        this.coinbaseUser = user.data;
+      });
+    });
+  }
+
+  public revokeToken() {
+    this.popupProvider.ionicConfirm(
+      'Coinbase',
+      'Are you sure you would like to log out of your Coinbase account?'
+    ).then((res) => {
+      if (res) {
+        this.coinbaseProvider.logout();
+        this.navCtrl.popToRoot();
+      }
+    });
+  };
 
 }
--- src/pages/integrations/glidera/glidera-settings/glidera-settings.ts
+++ src/pages/integrations/glidera/glidera-settings/glidera-settings.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { NavParams, NavController, ModalController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { Logger } from '../../../../providers/logger/logger';
 
-import { HomePage } from '../../../home/home';
 import { GlideraProvider } from '../../../../providers/glidera/glidera';
 import { PopupProvider } from '../../../../providers/popup/popup';
 
@@ -15,7 +14,6 @@ export class GlideraSettingsPage {
   public account: any;
 
   constructor(
-    private navParams: NavParams,
     private navCtrl: NavController,
     private popupProvider: PopupProvider,
     private logger: Logger,
@@ -43,17 +41,6 @@ export class GlideraSettingsPage {
     msg = (msg && msg.errors) ? msg.errors[0].message : msg;
     this.popupProvider.ionicAlert(title, msg).then(() => {
       this.navCtrl.pop();
-    });
-  }
-
-  private showError = function (title: string, msg: any): Promise<any> {
-    return new Promise((resolve, reject) => {
-      title = title || 'Error'; // TODO: gettextCatalog
-      this.logger.error(msg);
-      msg = (msg && msg.errors) ? msg.errors[0].message : msg;
-      this.popupProvider.ionicAlert(title, msg).then(() => {
-        return resolve();
-      });
     });
   }
 
