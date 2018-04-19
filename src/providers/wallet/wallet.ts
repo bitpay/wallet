ffc78a39dd1da8f322b4bf4987fb0a6ff8578d5f
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2018-04-19 11:02:35 -0300

    Fix: removes async function when creating new address

--- src/providers/wallet/wallet.ts
+++ src/providers/wallet/wallet.ts
@@ -379,10 +379,9 @@ export class WalletProvider {
         if (err) {
           let prefix = this.translate.instant('Could not create address');
           if (err instanceof this.errors.CONNECTION_ERROR || (err.message && err.message.match(/5../))) {
-            this.logger.warn('Could not create address', err);
-            return setTimeout(() => {
-              this.createAddress(wallet);
-            }, 5000);
+            this.bwcErrorProvider.cb(err, prefix).then((msg) => {
+              return reject(msg);
+            });
           } else if (
             err instanceof this.errors.MAIN_ADDRESS_GAP_REACHED || (err.message && err.message == 'MAIN_ADDRESS_GAP_REACHED')
           ) {
