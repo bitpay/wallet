501650a52954c48e42defb85e8ad2cd72c22251f
Marty Alcala <marty@bitpay.com>
2018-05-08 16:40:35 -0400

    simplify page tests

--- src/pages/send/confirm/confirm.spec.ts
+++ src/pages/send/confirm/confirm.spec.ts
@@ -12,39 +12,6 @@ import { TestUtils } from '../../../test';
 
 import { ConfirmPage } from './confirm';
 
-import { AddressBookProvider } from '../../../providers/address-book/address-book';
-import { AppIdentityProvider } from '../../../providers/app-identity/app-identity';
-import { AppProvider } from '../../../providers/app/app';
-import { BitPayCardProvider } from '../../../providers/bitpay-card/bitpay-card';
-import { BitPayProvider } from '../../../providers/bitpay/bitpay';
-import { ExternalLinkProvider } from '../../../providers/external-link/external-link';
-import { FeeProvider } from '../../../providers/fee/fee';
-import { FeedbackProvider } from '../../../providers/feedback/feedback';
-import { FilterProvider } from '../../../providers/filter/filter';
-import { HomeIntegrationsProvider } from '../../../providers/home-integrations/home-integrations';
-import { IncomingDataProvider } from '../../../providers/incoming-data/incoming-data';
-import { LanguageProvider } from '../../../providers/language/language';
-import { NodeWebkitProvider } from '../../../providers/node-webkit/node-webkit';
-import { OnGoingProcessProvider } from '../../../providers/on-going-process/on-going-process';
-import { PayproProvider } from '../../../providers/paypro/paypro';
-import { PlatformProvider } from '../../../providers/platform/platform';
-import { PopupProvider } from '../../../providers/popup/popup';
-import { PushNotificationsProvider } from '../../../providers/push-notifications/push-notifications';
-import { RateProvider } from '../../../providers/rate/rate';
-import { ReleaseProvider } from '../../../providers/release/release';
-import { ReplaceParametersProvider } from '../../../providers/replace-parameters/replace-parameters';
-import { ScanProvider } from '../../../providers/scan/scan';
-import { TouchIdProvider } from '../../../providers/touchid/touchid';
-import { TxConfirmNotificationProvider } from '../../../providers/tx-confirm-notification/tx-confirm-notification';
-import { TxFormatProvider } from '../../../providers/tx-format/tx-format';
-import { WalletProvider } from '../../../providers/wallet/wallet';
-import { BwcErrorProvider } from './../../../providers/bwc-error/bwc-error';
-import { BwcProvider } from './../../../providers/bwc/bwc';
-import { ConfigProvider } from './../../../providers/config/config';
-import { Logger } from './../../../providers/logger/logger';
-import { PersistenceProvider } from './../../../providers/persistence/persistence';
-import { ProfileProvider } from './../../../providers/profile/profile';
-
 describe('ConfirmPage', () => {
   let fixture: ComponentFixture<ConfirmPage>;
   let instance: any;
@@ -52,40 +19,7 @@ describe('ConfirmPage', () => {
 
   beforeEach(
     async(() =>
-      TestUtils.configurePageTestingModule([ConfirmPage], {
-        providers: [
-          AddressBookProvider,
-          AppIdentityProvider,
-          BitPayCardProvider,
-          BitPayProvider,
-          BwcProvider,
-          BwcErrorProvider,
-          ConfigProvider,
-          ExternalLinkProvider,
-          FeedbackProvider,
-          FeeProvider,
-          FilterProvider,
-          HomeIntegrationsProvider,
-          IncomingDataProvider,
-          LanguageProvider,
-          Logger,
-          NodeWebkitProvider,
-          OnGoingProcessProvider,
-          PayproProvider,
-          PersistenceProvider,
-          PopupProvider,
-          ProfileProvider,
-          PushNotificationsProvider,
-          RateProvider,
-          ReleaseProvider,
-          ReplaceParametersProvider,
-          ScanProvider,
-          TouchIdProvider,
-          TxConfirmNotificationProvider,
-          TxFormatProvider,
-          WalletProvider
-        ]
-      }).then(testEnv => {
+      TestUtils.configurePageTestingModule([ConfirmPage]).then(testEnv => {
         fixture = testEnv.fixture;
         instance = testEnv.instance;
         testBed = testEnv.testBed;
@@ -157,8 +91,8 @@ describe('ConfirmPage', () => {
     describe('chooseFeeLevel', () => {
       it('should display a fee modal', () => {
         const modal = {
-          present: () => { },
-          onDidDismiss: () => { }
+          present: () => {},
+          onDidDismiss: () => {}
         };
         const presentSpy = spyOn(modal, 'present');
         instance.modalCtrl.create = () => modal;
@@ -259,9 +193,6 @@ describe('ConfirmPage', () => {
         await instance.onlyPublish();
         expect(setErrorSpy).toHaveBeenCalled();
       });
-    });
-    describe('publishAndSign', () => {
-      it('should work', () => { });
     });
   });
 });
--- src/pages/wallet-details/wallet-details.spec.ts
+++ src/pages/wallet-details/wallet-details.spec.ts
@@ -5,45 +5,10 @@ import {
   TestBed
 } from '@angular/core/testing';
 
-import { Platform } from 'ionic-angular';
-import { Subject } from 'rxjs';
-
 import { TestUtils } from '../../test';
 
-import { WalletDetailsPage } from './wallet-details';
-
-import { AddressBookProvider } from '../../providers/address-book/address-book';
-import { AppIdentityProvider } from '../../providers/app-identity/app-identity';
-import { AppProvider } from '../../providers/app/app';
-import { BitPayCardProvider } from '../../providers/bitpay-card/bitpay-card';
-import { BitPayProvider } from '../../providers/bitpay/bitpay';
-import { ExternalLinkProvider } from '../../providers/external-link/external-link';
-import { FeeProvider } from '../../providers/fee/fee';
-import { FeedbackProvider } from '../../providers/feedback/feedback';
-import { FilterProvider } from '../../providers/filter/filter';
-import { HomeIntegrationsProvider } from '../../providers/home-integrations/home-integrations';
-import { IncomingDataProvider } from '../../providers/incoming-data/incoming-data';
-import { LanguageProvider } from '../../providers/language/language';
-import { NodeWebkitProvider } from '../../providers/node-webkit/node-webkit';
-import { OnGoingProcessProvider } from '../../providers/on-going-process/on-going-process';
-import { PayproProvider } from '../../providers/paypro/paypro';
-import { PlatformProvider } from '../../providers/platform/platform';
-import { PopupProvider } from '../../providers/popup/popup';
-import { PushNotificationsProvider } from '../../providers/push-notifications/push-notifications';
-import { RateProvider } from '../../providers/rate/rate';
-import { ReleaseProvider } from '../../providers/release/release';
-import { ReplaceParametersProvider } from '../../providers/replace-parameters/replace-parameters';
-import { ScanProvider } from '../../providers/scan/scan';
-import { TimeProvider } from '../../providers/time/time';
-import { TouchIdProvider } from '../../providers/touchid/touchid';
-import { TxFormatProvider } from '../../providers/tx-format/tx-format';
-import { WalletProvider } from '../../providers/wallet/wallet';
-import { BwcErrorProvider } from './../../providers/bwc-error/bwc-error';
-import { BwcProvider } from './../../providers/bwc/bwc';
-import { ConfigProvider } from './../../providers/config/config';
-import { Logger } from './../../providers/logger/logger';
-import { PersistenceProvider } from './../../providers/persistence/persistence';
 import { ProfileProvider } from './../../providers/profile/profile';
+import { WalletDetailsPage } from './wallet-details';
 
 describe('WalletDetailsPage', () => {
   let fixture: ComponentFixture<WalletDetailsPage>;
@@ -62,45 +27,14 @@ describe('WalletDetailsPage', () => {
         isPrivKeyEncrypted: () => true
       };
       spyOn(ProfileProvider.prototype, 'getWallet').and.returnValue(mockWallet);
-      return TestUtils.configurePageTestingModule([WalletDetailsPage], {
-        providers: [
-          AddressBookProvider,
-          AppIdentityProvider,
-          BitPayCardProvider,
-          BitPayProvider,
-          BwcProvider,
-          BwcErrorProvider,
-          ConfigProvider,
-          ExternalLinkProvider,
-          FeedbackProvider,
-          FeeProvider,
-          FilterProvider,
-          HomeIntegrationsProvider,
-          IncomingDataProvider,
-          LanguageProvider,
-          Logger,
-          NodeWebkitProvider,
-          OnGoingProcessProvider,
-          PayproProvider,
-          PersistenceProvider,
-          PopupProvider,
-          ProfileProvider,
-          PushNotificationsProvider,
-          RateProvider,
-          ReleaseProvider,
-          ReplaceParametersProvider,
-          ScanProvider,
-          TimeProvider,
-          TouchIdProvider,
-          TxFormatProvider,
-          WalletProvider
-        ]
-      }).then(testEnv => {
-        fixture = testEnv.fixture;
-        instance = testEnv.instance;
-        testBed = testEnv.testBed;
-        fixture.detectChanges();
-      });
+      return TestUtils.configurePageTestingModule([WalletDetailsPage]).then(
+        testEnv => {
+          fixture = testEnv.fixture;
+          instance = testEnv.instance;
+          testBed = testEnv.testBed;
+          fixture.detectChanges();
+        }
+      );
     })
   );
   afterEach(() => {
@@ -160,7 +94,7 @@ describe('WalletDetailsPage', () => {
     describe('showHistory', () => {
       it('should add the next page of transactions to the list', () => {
         instance.currentPage = 0;
-        instance.wallet.completeHistory = new Array(11).map(() => { });
+        instance.wallet.completeHistory = new Array(11).map(() => {});
         const spy = spyOn(instance, 'groupHistory');
         instance.showHistory();
         expect(instance.history.length).toBe(10);
