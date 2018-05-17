340fc42848abc05857fcd34c1116f4e468e47043
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2018-05-17 18:32:39 -0300

    Replaces termsOfUse view by link to website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -111,7 +111,6 @@ import { FingerprintModalPage } from '../pages/fingerprint/fingerprint';
 import { PinModalPage } from '../pages/pin/pin';
 import { AboutPage } from '../pages/settings/about/about';
 import { SessionLogPage } from '../pages/settings/about/session-log/session-log';
-import { TermsOfUsePage } from '../pages/settings/about/terms-of-use/terms-of-use';
 import { AddressbookAddPage } from '../pages/settings/addressbook/add/add';
 import { AddressbookPage } from '../pages/settings/addressbook/addressbook';
 import { AddressbookViewPage } from '../pages/settings/addressbook/view/view';
@@ -247,7 +246,6 @@ export function createTranslateLoader(http: HttpClient) {
     ShapeshiftSettingsPage,
     ShapeshiftPage,
     ShapeshiftShiftPage,
-    TermsOfUsePage,
     MercadoLibreCardDetailsPage,
     NotificationsPage,
     FeePolicyPage,
@@ -389,7 +387,6 @@ export function createTranslateLoader(http: HttpClient) {
     ShapeshiftSettingsPage,
     ShapeshiftPage,
     ShapeshiftShiftPage,
-    TermsOfUsePage,
     MercadoLibreCardDetailsPage,
     NotificationsPage,
     FeePolicyPage,
