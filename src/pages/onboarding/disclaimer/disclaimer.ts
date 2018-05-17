fbc828f3d068de1fe929ad83f1a8581a110facae
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2018-05-17 16:47:25 -0300

    Another checkbox to accept email

--- src/pages/onboarding/disclaimer/disclaimer.ts
+++ src/pages/onboarding/disclaimer/disclaimer.ts
@@ -5,6 +5,7 @@ import { Logger } from '../../../providers/logger/logger';
 import { TermsOfUsePage } from '../../settings/about/terms-of-use/terms-of-use';
 import { TabsPage } from '../../tabs/tabs';
 
+import { EmailNotificationsProvider } from '../../../providers/email-notifications/email-notifications';
 import { PersistenceProvider } from '../../../providers/persistence/persistence';
 
 @Component({
@@ -14,15 +15,19 @@ import { PersistenceProvider } from '../../../providers/persistence/persistence'
 export class DisclaimerPage {
   public accepted: any;
   public terms: any;
+  public hasEmail: boolean;
 
   constructor(
     public navCtrl: NavController,
     private logger: Logger,
+    private emailProvider: EmailNotificationsProvider,
     private persistenceProvider: PersistenceProvider
   ) {
+    this.hasEmail = this.emailProvider.getEmailIfEnabled() ? true : false;
     this.accepted = {
       first: false,
       second: false,
+      third: this.hasEmail ? false : true
     };
     this.terms = {
       accepted: false,
