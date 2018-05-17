81a5a25181bd3d5d23bccd744b126a1a411ccf2e
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2018-05-17 18:21:49 -0300

    Modify settings - notifications text

--- src/pages/settings/notifications/notifications.ts
+++ src/pages/settings/notifications/notifications.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController } from 'ionic-angular';
 import { Logger } from '../../../providers/logger/logger';
@@ -7,6 +8,7 @@ import { Logger } from '../../../providers/logger/logger';
 import { AppProvider } from '../../../providers/app/app';
 import { ConfigProvider } from '../../../providers/config/config';
 import { EmailNotificationsProvider } from '../../../providers/email-notifications/email-notifications';
+import { ExternalLinkProvider } from '../../../providers/external-link/external-link';
 import { PlatformProvider } from '../../../providers/platform/platform';
 import { PushNotificationsProvider } from '../../../providers/push-notifications/push-notifications';
 
@@ -37,7 +39,9 @@ export class NotificationsPage {
     private platformProvider: PlatformProvider,
     private pushProvider: PushNotificationsProvider,
     private emailProvider: EmailNotificationsProvider,
-    private logger: Logger
+    private externalLinkProvider: ExternalLinkProvider,
+    private logger: Logger,
+    private translate: TranslateService
   ) {
     this.emailForm = this.formBuilder.group({
       email: ['', Validators.compose([Validators.required, new EmailValidator(configProvider, emailProvider).isValid])]
@@ -101,6 +105,16 @@ export class NotificationsPage {
       email: this.emailForm.value.email
     });
     this.navCtrl.pop();
+  }
+
+  public openPrivacyPolicy() {
+    let url = 'https://bitpay.com/about/privacy';
+    let optIn = true;
+    let message = null;
+    let title = this.translate.instant('View Privacy Policy');
+    let okText = this.translate.instant('Open');
+    let cancelText = this.translate.instant('Go Back');
+    this.externalLinkProvider.open(url, optIn, title, message, okText, cancelText);
   }
 
 }
