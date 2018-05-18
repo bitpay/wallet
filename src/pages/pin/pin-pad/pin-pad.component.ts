9c6307c852355a2be09e03c4777d543853dc8141
Marty Alcala <marty@bitpay.com>
2018-05-18 19:15:45 -0400

    simplify pin pad

--- src/pages/pin/pin-pad/pin-pad.component.ts
+++ src/pages/pin/pin-pad/pin-pad.component.ts
@@ -11,8 +11,8 @@ export interface PinButton {
 @Component({
   selector: 'pin-pad',
   template: `
-    <ion-row *ngFor="let row of rows">
-      <ion-col *ngFor="let button of row" (click)="onKeystroke(button.value)">
+    <ion-row *ngFor="let row of buttonRows">
+      <ion-col *ngFor="let button of row" (click)="onKeystroke(button.value)" tappable>
         <div>
           <span *ngIf="button.value !== 'delete'">{{button.value}}</span>
           <img *ngIf="button.value === 'delete'" src="assets/img/tail-left.svg">
@@ -26,69 +26,64 @@ export class PinPad {
   keystrokeSubject: Subject<string> = new Subject<string>();
   @Output()
   keystroke: Observable<string> = this.keystrokeSubject.asObservable();
-  private buttons: PinButton[] = [
-    {
-      value: '1',
-      letters: ''
-    },
-    {
-      value: '2',
-      letters: 'ABC'
-    },
-    {
-      value: '3',
-      letters: 'DEF'
-    },
-    {
-      value: '4',
-      letters: 'GHI'
-    },
-    {
-      value: '5',
-      letters: 'JKL'
-    },
-    {
-      value: '6',
-      letters: 'MNO'
-    },
-    {
-      value: '7',
-      letters: 'PQRS'
-    },
-    {
-      value: '8',
-      letters: 'TUV'
-    },
-    {
-      value: '9',
-      letters: 'WXYZ'
-    },
-    {
-      value: '',
-      letters: ''
-    },
-    {
-      value: '0',
-      letters: ''
-    },
-    {
-      value: 'delete',
-      letters: ''
-    }
+  private buttonRows: PinButton[][] = [
+    [
+      {
+        value: '1',
+        letters: ''
+      },
+      {
+        value: '2',
+        letters: 'ABC'
+      },
+      {
+        value: '3',
+        letters: 'DEF'
+      }
+    ],
+    [
+      {
+        value: '4',
+        letters: 'GHI'
+      },
+      {
+        value: '5',
+        letters: 'JKL'
+      },
+      {
+        value: '6',
+        letters: 'MNO'
+      }
+    ],
+    [
+      {
+        value: '7',
+        letters: 'PQRS'
+      },
+      {
+        value: '8',
+        letters: 'TUV'
+      },
+      {
+        value: '9',
+        letters: 'WXYZ'
+      }
+    ],
+    [
+      {
+        value: '',
+        letters: ''
+      },
+      {
+        value: '0',
+        letters: ''
+      },
+      {
+        value: 'delete',
+        letters: ''
+      }
+    ]
   ];
-
-  rows: PinButton[][] = this.getRows();
-
-  private getRows(): PinButton[][] {
-    return this.buttons.reduce((rows, button, index) => {
-      const rowNum = Math.ceil((index + 1) / 3);
-      const rowIndex = rowNum - 1;
-      const currentRow = rows[rowIndex];
-      const updatedRow = !currentRow ? [button] : [...currentRow, button];
-      rows[rowIndex] = updatedRow;
-      return rows;
-    }, []);
-  }
 
   public onKeystroke(value: string): void {
     this.keystrokeSubject.next(value);
