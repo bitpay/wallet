faa1adf43e8c3ee16daac8d4ef7b9ee006a4c44d
Juan Pablo Abuin <juanpabloabuin@gmail.com>
2018-06-07 17:10:13 -0300

    refactor receive page style

--- src/pages/receive/receive.ts
+++ src/pages/receive/receive.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { Events, NavController } from 'ionic-angular';
+import { ActionSheetController, Events, NavController } from 'ionic-angular';
 import { Logger } from '../../providers/logger/logger';
 
 // Native
@@ -37,6 +37,7 @@ export class ReceivePage {
   public isOpenSelector: boolean;
 
   constructor(
+    private actionSheetCtrl: ActionSheetController,
     private navCtrl: NavController,
     private logger: Logger,
     private profileProvider: ProfileProvider,
@@ -173,5 +174,40 @@ export class ReceivePage {
       okText,
       cancelText
     );
+  }
+
+  public showMoreOptions(): void {
+    let buttons = [];
+
+    let specificAmountButton = {
+      text: this.translate.instant('Request Specific Amount'),
+      icon: 'calculator',
+      handler: () => {
+        this.requestSpecificAmount();
+      }
+    };
+    let shareButton = {
+      text: this.translate.instant('Share Address'),
+      icon: 'share',
+      handler: () => {
+        this.shareAddress();
+      }
+    };
+
+    buttons.push(specificAmountButton);
+
+    if (
+      this.showShareButton &&
+      this.wallet &&
+      this.wallet.isComplete() &&
+      !this.wallet.needsBackup
+    )
+      buttons.push(shareButton);
+
+    const actionSheet = this.actionSheetCtrl.create({
+      buttons
+    });
+
+    actionSheet.present();
   }
 }
