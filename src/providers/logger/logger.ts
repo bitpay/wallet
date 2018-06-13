61c6e023974798d3c088cf4912c9b6d19bb2bd9e
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2018-06-13 10:26:23 -0300

    Ignore console log in these lines

--- src/providers/logger/logger.ts
+++ src/providers/logger/logger.ts
@@ -1,4 +1,3 @@
-// tslint:disable-next-line:no-console
 import { Injectable, isDevMode } from '@angular/core';
 
 import * as _ from 'lodash';
@@ -28,6 +27,7 @@ export class Logger {
   public error(message?, ...optionalParams): void {
     let msg =
       '[error] ' + (_.isString(message) ? message : JSON.stringify(message));
+    // tslint:disable-next-line:no-console
     console.log(msg, ...optionalParams);
     let args = this.processingArgs(arguments);
     this.add('error', args);
@@ -36,6 +36,7 @@ export class Logger {
   public debug(message?, ...optionalParams): void {
     let msg =
       '[debug] ' + (_.isString(message) ? message : JSON.stringify(message));
+    // tslint:disable-next-line:no-console
     if (isDevMode()) console.log(msg, ...optionalParams);
     let args = this.processingArgs(arguments);
     this.add('debug', args);
@@ -44,6 +45,7 @@ export class Logger {
   public info(message?, ...optionalParams): void {
     let msg =
       '[info] ' + (_.isString(message) ? message : JSON.stringify(message));
+    // tslint:disable-next-line:no-console
     if (isDevMode()) console.log(msg, ...optionalParams);
     let args = this.processingArgs(arguments);
     this.add('info', args);
@@ -52,6 +54,7 @@ export class Logger {
   public warn(message?, ...optionalParams): void {
     let msg =
       '[warn] ' + (_.isString(message) ? message : JSON.stringify(message));
+    // tslint:disable-next-line:no-console
     if (isDevMode()) console.log(msg, ...optionalParams);
     let args = this.processingArgs(arguments);
     this.add('warn', args);
@@ -107,6 +110,7 @@ export class Logger {
           v = v.message ? v.message : JSON.stringify(v);
         }
       } catch (e) {
+        // tslint:disable-next-line:no-console
         console.log('Error at log decorator:', e);
         v = 'undefined';
       }
