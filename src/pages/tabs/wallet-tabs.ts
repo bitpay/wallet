5a17048608355d0b21b0021a12cf6dbb24d611c3
Marty Alcala <marty@bitpay.com>
2018-06-13 16:49:18 -0400

    remove unneeded variable

--- src/pages/tabs/wallet-tabs.ts
+++ src/pages/tabs/wallet-tabs.ts
@@ -2,15 +2,14 @@ import { Component, ViewChild } from '@angular/core';
 import { NavParams } from 'ionic-angular';
 import { ReceivePage } from '../receive/receive';
 import { AmountPage } from '../send/amount/amount';
-// import { SendPage } from '../send/send';
 import { WalletDetailsPage } from '../wallet-details/wallet-details';
 
 @Component({
   template: `
   <ion-tabs selectedIndex="1" #tabs>
-    <ion-tab [root]="receiveRoot" tabTitle="{{'Receive'|translate}}" tabIcon="tab-receive" [rootParams]="rootParams"></ion-tab>
-    <ion-tab [root]="activityRoot" tabTitle="{{'Activity'|translate}}" tabIcon="tab-activity" [rootParams]="rootParams"></ion-tab>
-    <ion-tab [root]="sendRoot" tabTitle="{{'Send'|translate}}" tabIcon="tab-send" [rootParams]="rootParams"></ion-tab>
+    <ion-tab [root]="receiveRoot" tabTitle="{{'Receive'|translate}}" tabIcon="tab-receive"></ion-tab>
+    <ion-tab [root]="activityRoot" tabTitle="{{'Activity'|translate}}" tabIcon="tab-activity"></ion-tab>
+    <ion-tab [root]="sendRoot" tabTitle="{{'Send'|translate}}" tabIcon="tab-send"></ion-tab>
   </ion-tabs>
   `
 })
@@ -20,13 +19,10 @@ export class WalletTabsPage {
   receiveRoot = ReceivePage;
   activityRoot = WalletDetailsPage;
   sendRoot = AmountPage;
-  // sendRoot = SendPage;
 
-  rootParams: NavParams;
   walletId: string;
 
   constructor(private navParams: NavParams) {
-    this.rootParams = this.navParams;
-    this.walletId = this.rootParams.get('walletId');
+    this.walletId = this.navParams.get('walletId');
   }
 }
