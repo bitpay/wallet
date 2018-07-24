0c869b79c124b9a82214c8fc567b82b671a9490e
Marty Alcala <marty@bitpay.com>
2018-07-24 19:10:08 -0400

    add label tip component

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,11 +1,13 @@
 import { ACTION_SHEET_COMPONENTS } from './action-sheets';
 import { EditableItemComponent } from './editable-item/editable-item';
 import { EXPANDABLE_HEADER_COMPONENTS } from './expandable-header/expandable-header';
+import { LabelTip } from './label-tip/label-tip';
 import { MINI_MODAL_COMPONENTS } from './mini-modal/mini-modal';
 
 export const COMPONENTS = [
   MINI_MODAL_COMPONENTS,
   EXPANDABLE_HEADER_COMPONENTS,
   ACTION_SHEET_COMPONENTS,
-  EditableItemComponent
+  EditableItemComponent,
+  LabelTip
 ];
