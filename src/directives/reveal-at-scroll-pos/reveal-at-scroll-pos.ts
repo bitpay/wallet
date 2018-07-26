37817c5018e41d86db47991c26bc04a86d91c872
Marty Alcala <marty@bitpay.com>
2018-07-26 15:43:28 -0400

    add translateX to the navbar title reveal animation

--- src/directives/reveal-at-scroll-pos/reveal-at-scroll-pos.ts
+++ src/directives/reveal-at-scroll-pos/reveal-at-scroll-pos.ts
@@ -8,6 +8,8 @@ export class RevealAtScrollPosition {
   @Input('reveal-at-scroll-pos') scrollThreshold: number;
   @Input('scrollArea') scrollArea: Content;
 
+  animationDistance: number = 28;
+
   constructor(private element: ElementRef, private renderer: Renderer) {}
 
   ngAfterViewInit() {
@@ -19,7 +21,9 @@ export class RevealAtScrollPosition {
 
   updateStyling(scrollTop: number) {
     const opacity = this.getOpacity(scrollTop);
+    const translateX = this.getTranslation(scrollTop);
     this.setOpacity(opacity);
+    this.setTransform(translateX);
   }
 
   setOpacity(opacity: number) {
@@ -30,10 +34,41 @@ export class RevealAtScrollPosition {
     );
   }
 
+  setTransform(translateX: number) {
+    this.renderer.setElementStyle(
+      this.element.nativeElement,
+      'transform',
+      `translateX(${translateX}px)`
+    );
+  }
+
   getOpacity(scrollTop: number) {
-    const startFadeAt = this.scrollThreshold - 28;
-    const m = 1 / (this.scrollThreshold - startFadeAt);
-    const opacity = m * (scrollTop - this.scrollThreshold) + 1;
+    const finalOpacity = 1;
+    const fadeStartPosition = this.scrollThreshold - this.animationDistance;
+    const m = finalOpacity / (this.scrollThreshold - fadeStartPosition);
+    const opacity = m * (scrollTop - this.scrollThreshold) + finalOpacity;
     return opacity;
+  }
+
+  getTranslation(scrollTop: number) {
+    /*
+    point-slope-form
+    y-y1 = m(x-x1)
+    y = m(x-x1) + y1
+      where m = (y2 - y1) / (x2 - x1)
+
+    initialTranslateX = -10
+    finalTranslateX = 0
+    p1 = (scrollThreshold, finalTranslateX)
+    p2 = (animationStartPos, initialTranslateX)
+    */
+    const initialTranslateX = -10;
+    const finalTranslateX = 0;
+    const animationStartPos = this.scrollThreshold - this.animationDistance;
+    const m =
+      (initialTranslateX - finalTranslateX) /
+      (animationStartPos - this.scrollThreshold);
+    const translateX = m * (scrollTop - this.scrollThreshold) + 0;
+    return translateX > 0 ? 0 : translateX;
   }
 }
