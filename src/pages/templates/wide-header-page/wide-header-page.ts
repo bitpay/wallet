9065328a6025fc2a8e1d9859dec49ae88f2fd9ae
Marty Alcala <marty@bitpay.com>
2018-07-27 18:59:25 -0400

    a little more padding on a real ios device

--- src/pages/templates/wide-header-page/wide-header-page.ts
+++ src/pages/templates/wide-header-page/wide-header-page.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { PlatformProvider } from '../../../providers/platform/platform';
 
 @Component({
   selector: 'wide-header-page',
@@ -7,4 +8,6 @@ import { Component, Input } from '@angular/core';
 export class WideHeaderPage {
   @Input() headerColor: string;
   @Input() title: string;
+
+  constructor(public platformProvider: PlatformProvider) {}
 }
