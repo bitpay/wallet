c7ade68079931ae1ef367a7cffc7df15a8fc110b
Juan Pablo Abuin <juanpabloabuin@gmail.com>
2018-08-09 16:33:58 -0300

    show message when an user use a legacy BCH address

--- src/providers/incoming-data/incoming-data.ts
+++ src/providers/incoming-data/incoming-data.ts
@@ -69,7 +69,7 @@ export class IncomingDataProvider {
     return !!this.bwcProvider.getBitcoreCash().URI.isValid(data);
   }
 
-  private isValidBitcoinCashUriWithLegacyAddress(data: string): boolean {
+  public isValidBitcoinCashUriWithLegacyAddress(data: string): boolean {
     data = this.sanitizeUri(data);
     return !!this.bwcProvider
       .getBitcore()
@@ -81,7 +81,7 @@ export class IncomingDataProvider {
     return !!/^https?:\/\//.test(data);
   }
 
-  private isValidBitcoinAddress(data: string): boolean {
+  public isValidBitcoinAddress(data: string): boolean {
     return !!(
       this.bwcProvider.getBitcore().Address.isValid(data, 'livenet') ||
       this.bwcProvider.getBitcore().Address.isValid(data, 'testnet')
