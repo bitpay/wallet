61c0844f2d4613b9a6915575c68d433b391b96b6
Gabriel Bazán <gabrielbazan7@hotmail.com>
2018-10-11 17:27:10 -0300

    set max width and min width and height

--- electron/main.js
+++ electron/main.js
@@ -20,7 +20,14 @@ const isDevMode = process.execPath.match(/[\\/]electron/);
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 400, height: 650 });
+  win = new BrowserWindow({
+    width: 400,
+    height: 650,
+    minWidth: 400,
+    maxWidth: 800,
+    minHeight: 650,
+    show: false
+  });
 
   // and load the index.html of the app.
   win.loadURL(
