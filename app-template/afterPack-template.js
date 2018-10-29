1afca1621dbf47f607730250d8730c037d0f76b7
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2018-10-29 15:07:11 -0300

    .

--- app-template/afterPack-template.js
+++ app-template/afterPack-template.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const fse = require('fs-extra');
 const path = require('path');
-module.exports = function (params) {
-    const unpackFile = path.join(params.appOutDir, '*NAMECASENOSPACE*.app/Contents/Resources/app.asar.unpacked');
-    console.log('EXISTS?', fs.existsSync(unpackFile));
-    console.log("REMOVE", unpackFile);
-    fse.removeSync(unpackFile);
+module.exports = function(params) {
+  const unpackFile = path.join(
+    params.appOutDir,
+    '*NAMECASENOSPACE*.app/Contents/Resources/app.asar.unpacked'
+  );
+  fse.removeSync(unpackFile);
 };
