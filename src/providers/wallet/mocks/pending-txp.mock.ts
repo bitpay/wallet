ae0924dbe08e374103db36b4b4ab996111f51532
Gabriel Masclef <gabrielmasclef@hotmail.com>
2018-12-31 12:30:05 -0300

    Unit tests for wallet provider

---
+++ src/providers/wallet/mocks/pending-txp.mock.ts
@@ -0,0 +1,176 @@
+export class PendingTxpMock {
+  version: number;
+  createdOn: number;
+  id: string;
+  walletId: string;
+  creatorId: string;
+  coin: string;
+  network: string;
+  outputs: [
+    {
+      amount: number;
+      toAddress: string;
+      message: string;
+      encryptedMessage;
+    }
+  ];
+  amount: number;
+  message: string;
+  payProUrl: string;
+  changeAddress: {
+    version: string;
+    createdOn: number;
+    address: string;
+    walletId: string;
+    isChange: boolean;
+    path: string;
+    publicKeys: string[];
+    coin: string;
+    network: string;
+    type: string;
+    hasActivity: boolean;
+    _id: string;
+  };
+  inputs: [
+    {
+      txid: string;
+      vout: number;
+      address: string;
+      scriptPubKey: string;
+      satoshis: number;
+      confirmations: number;
+      locked: boolean;
+      path: string;
+      publicKeys: string[];
+    }
+  ];
+  walletM: number;
+  walletN: number;
+  requiredSignatures: number;
+  requiredRejections: number;
+  status: string;
+  txid: string;
+  broadcastedOn;
+  inputPaths: string[];
+  actions?: [
+    {
+      version: string;
+      createdOn: number;
+      copayerId: string;
+      type: string;
+      signatures: string[];
+      xpub: string;
+      comment: string;
+      copayerName: string;
+    }
+  ];
+  outputOrder: number[];
+  fee: number;
+  feeLevel: string;
+  feePerKb: number;
+  excludeUnconfirmedUtxos: boolean;
+  addressType: string;
+  customData;
+  proposalSignature: string;
+  proposalSignaturePubKey: string;
+  proposalSignaturePubKeySig: string;
+  derivationStrategy: string;
+  creatorName: string;
+  deleteLockTime: number;
+  encryptedMessage;
+  hasUnconfirmedInputs: boolean;
+
+  constructor() {
+    this.version = 3;
+    this.createdOn = 1542039346;
+    this.id = 'txpId1';
+    this.walletId = 'walletId1';
+    this.creatorId = 'copayerId1';
+    this.coin = 'btc';
+    this.network = 'testnet';
+    this.outputs = [
+      {
+        amount: 100000,
+        toAddress: 'address1',
+        message: null,
+        encryptedMessage: null
+      }
+    ];
+    this.amount = 100000;
+    this.message = null;
+    this.payProUrl = null;
+    this.changeAddress = {
+      version: '1.0.0',
+      createdOn: 1542039346,
+      address: 'address1',
+      walletId: 'walletId1',
+      isChange: true,
+      path: 'm/1/455',
+      publicKeys: ['publicKeys1', 'publicKeys2'],
+      coin: 'btc',
+      network: 'testnet',
+      type: 'P2SH',
+      hasActivity: null,
+      _id: '_id1'
+    };
+    this.inputs = [
+      {
+        txid: 'txid1',
+        vout: 1,
+        address: 'address1',
+        scriptPubKey: 'scriptPubKey1',
+        satoshis: 100000,
+        confirmations: 28282,
+        locked: false,
+        path: 'm/0/174',
+        publicKeys: ['publicKeys3', 'publicKeys4']
+      },
+      {
+        txid: 'txid2',
+        vout: 0,
+        address: 'address2',
+        scriptPubKey: 'scriptPubKey2',
+        satoshis: 100000,
+        confirmations: 3011,
+        locked: false,
+        path: 'm/0/172',
+        publicKeys: ['publicKeys5', 'publicKeys6']
+      }
+    ];
+    this.walletM = 2;
+    this.walletN = 2;
+    this.requiredSignatures = 2;
+    this.requiredRejections = 1;
+    this.status = null; // values: pending, accepted
+    this.txid = null;
+    this.broadcastedOn = null;
+    this.inputPaths = ['m/0/174', 'm/0/172'];
+    this.actions = [
+      {
+        version: '1.0.0',
+        createdOn: 1542039348,
+        copayerId: 'copayerId1',
+        type: 'accept',
+        signatures: ['signature1', 'signature2'],
+        xpub: 'xpub1',
+        comment: 'action1',
+        copayerName: 'Satoshi Nakamoto'
+      }
+    ];
+    this.outputOrder = [0, 1];
+    this.fee = 616;
+    this.feeLevel = 'priority';
+    this.feePerKb = 1000;
+    this.excludeUnconfirmedUtxos = true;
+    this.addressType = 'P2SH';
+    this.customData = null;
+    this.proposalSignature = 'proposalSignature1';
+    this.proposalSignaturePubKey = 'proposalSignaturePubKey1';
+    this.proposalSignaturePubKeySig = 'proposalSignaturePubKeySig1';
+    this.derivationStrategy = 'BIP44';
+    this.creatorName = 'Satoshi Nakamoto';
+    this.deleteLockTime = 0;
+    this.encryptedMessage = null;
+    this.hasUnconfirmedInputs = false;
+  }
+}
---
+++ src/providers/wallet/mocks/wallet.mock.ts
@@ -0,0 +1,340 @@
+import { TransactionProposal } from '../wallet';
+import { PendingTxpMock } from './pending-txp.mock';
+
+export interface StatusMock {
+  balance: {
+    totalAmount: number;
+    lockedAmount: number;
+    totalConfirmedAmount: number;
+    lockedConfirmedAmount: number;
+    availableAmount: number;
+    availableConfirmedAmount: number;
+    byAddress: [
+      {
+        address: string;
+        path: string;
+        amount: number;
+      }
+    ];
+  };
+  isValid: boolean;
+  pendingTxps: PendingTxpMock[];
+  wallet: {
+    singleAddress: boolean;
+  };
+
+  statusUpdatedOn?: number;
+  email?: string;
+  balanceByAddress?;
+  totalBalanceSat?: number;
+  lockedBalanceSat?: number;
+  availableBalanceSat?: number;
+  totalBytesToSendMax?: number;
+  pendingAmount?: number;
+  spendableAmount?: number;
+  unitToSatoshi?: number;
+  satToUnit?: number;
+  totalBalanceStr?: string;
+  lockedBalanceStr?: string;
+  availableBalanceStr?: string;
+  spendableBalanceStr?: string;
+  pendingBalanceStr?: string;
+  alternativeName?: string;
+  alternativeIsoCode?: string;
+}
+
+export const sendMaxInfoMock = {
+  amount: 300000,
+  amountAboveMaxSize: 0,
+  amountBelowFee: 780000,
+  fee: 480000,
+  feePerKb: 100000,
+  inputs: [
+    {
+      txid: 'txid1',
+      address: 'address1',
+      satoshis: 100000
+    },
+    {
+      txid: 'txid2',
+      address: 'address2',
+      satoshis: 200000
+    }
+  ],
+  size: 4869,
+  utxosAboveMaxSize: 0,
+  utxosBelowFee: 73
+};
+
+export const txsFromLocal = [
+  {
+    amount: 10000,
+    fees: 99,
+    txid: 'txid1',
+    confirmations: 50,
+    recent: false,
+    time: Date.now()
+  },
+  {
+    amount: 10000,
+    fees: 99,
+    txid: 'txid2',
+    confirmations: 80,
+    recent: false,
+    time: Date.now()
+  }
+];
+
+export const statusMock: StatusMock = {
+  balance: {
+    totalAmount: 500000000,
+    lockedAmount: 100000000,
+    totalConfirmedAmount: 500000000,
+    lockedConfirmedAmount: 100000000,
+    availableAmount: 400000000,
+    availableConfirmedAmount: 400000000,
+    byAddress: [
+      {
+        address: 'address1',
+        path: 'm/1/219',
+        amount: 2000000
+      },
+      {
+        address: 'address2',
+        path: 'm/1/26',
+        amount: 7000000
+      }
+    ]
+  },
+  isValid: true,
+  pendingTxps: [],
+  wallet: {
+    singleAddress: false
+  }
+};
+
+export class WalletMock {
+  cachedStatus: StatusMock;
+  copayerId: string;
+  completeHistory?;
+  credentials: {
+    addressType: string;
+    scanStatus: string;
+    walletId: string;
+    walletName: string;
+    network?: string;
+    derivationStrategy?: string;
+    mnemonicHasPassphrase?: boolean;
+    publicKeyRing?: any[];
+    walletPrivKey?: string;
+    getBaseAddressDerivationPath?: () => {};
+  };
+  coin: string;
+  id: string;
+  m: number;
+  n: number;
+  needsBackup?: boolean;
+  network: string;
+  notAuthorized?: boolean;
+  pendingTxps: PendingTxpMock[];
+  scanning?: boolean;
+  status: StatusMock;
+  totalBalanceSat: string;
+
+  constructor() {
+    this.cachedStatus = statusMock;
+    this.credentials = {
+      addressType: 'P2PKH',
+      scanStatus: null,
+      walletId: 'walletid1',
+      walletName: 'Test wallet'
+    };
+    this.coin = 'btc';
+    this.id = 'walletid1';
+    this.needsBackup = false;
+    this.network = 'livenet';
+    this.pendingTxps = [];
+    this.scanning = null;
+    this.status = statusMock;
+    this.totalBalanceSat = '0.01 BTC';
+  }
+  isComplete() {
+    return true;
+  }
+  getStatus(_opts, cb) {
+    return cb(null, this.status);
+  }
+  getTxNote(_opts, cb) {
+    return cb(null, 'Note');
+  }
+  getTxNotes(_opts, cb) {
+    const notes = [
+      {
+        txid: 'txid1'
+      },
+      {
+        txid: 'txid2'
+      }
+    ];
+    return cb(null, notes);
+  }
+  editTxNote(_opts, cb) {
+    return cb(null, 'NoteEdited');
+  }
+  getTx(_txpid, cb) {
+    const tx = {
+      txid: 'txid'
+    };
+    return cb(null, tx);
+  }
+  getTxHistory(_opts, cb) {
+    const txsFromLocal = [
+      {
+        amount: 10000,
+        fees: 99,
+        txid: 'txid1',
+        confirmations: 50,
+        recent: false
+      },
+      {
+        amount: 10000,
+        fees: 99,
+        txid: 'txid2',
+        confirmations: 80,
+        recent: false
+      }
+    ];
+    return cb(null, txsFromLocal);
+  }
+  isPrivKeyEncrypted() {
+    return true;
+  }
+  createTxProposal(_txp, cb) {
+    const txp: TransactionProposal = {
+      amount: 1000,
+      toAddress: 'address1',
+      outputs: [
+        {
+          toAddress: 'address1',
+          amount: 1000,
+          message: 'msg1'
+        }
+      ],
+      inputs: null,
+      fee: 99,
+      message: 'message1',
+      payProUrl: null,
+      excludeUnconfirmedUtxos: false,
+      feePerKb: 100,
+      feeLevel: 'normal',
+      dryRun: false
+    };
+    return cb(null, txp);
+  }
+  publishTxProposal(_opts, cb) {
+    const txpPublished = _opts.txp;
+    return cb(null, txpPublished);
+  }
+  signTxProposal(_txp, _pass, cb) {
+    const signedTxp = _txp;
+    return cb(null, signedTxp);
+  }
+  broadcastTxProposal(_txp, cb) {
+    const broadcastedTxp = _txp;
+    const memo = 'memo1';
+    return cb(null, broadcastedTxp, memo);
+  }
+  rejectTxProposal(_txp, _opt, cb) {
+    const rejectedTxp = {
+      txid: 'txid1'
+    };
+    return cb(null, rejectedTxp);
+  }
+  removeTxProposal(_txp, cb) {
+    return cb(null);
+  }
+  savePreferences(_pref, cb) {
+    return cb(null);
+  }
+  recreateWallet(cb) {
+    return cb(null);
+  }
+  startScan(_opts, cb) {
+    return cb(null);
+  }
+  getMainAddresses(_opts, cb) {
+    const addresses = [
+      {
+        address: 'address1'
+      },
+      {
+        address: 'address2'
+      }
+    ];
+    return cb(null, addresses);
+  }
+  getBalance(_opts, cb) {
+    const balance = {
+      totalAmount: 1000
+    };
+    return cb(null, balance);
+  }
+  getUtxos(_opts, cb) {
+    const utxos = [
+      {
+        satoshis: 1000
+      },
+      {
+        satoshis: 2000
+      },
+      {
+        satoshis: 3000
+      },
+      {
+        satoshis: 9000000
+      }
+    ];
+    return cb(null, utxos);
+  }
+  encryptPrivateKey(_pass) {}
+  decryptPrivateKey(_pass) {}
+  checkPassword(_pass) {
+    return true;
+  }
+  canSign() {
+    return true;
+  }
+  getKeys(_pass) {
+    const keysWithMnemonics = {
+      mnemonic: 'mom mom mom mom mom mom mom mom mom mom mom mom',
+      xPrivKey: 'xPrivKey1'
+    };
+    const keysWithoutMnemonics = {
+      xPrivKey: 'xPrivKey1'
+    };
+
+    const keys =
+      _pass == 'password1' ? keysWithMnemonics : keysWithoutMnemonics;
+    return keys;
+  }
+  getSendMaxInfo(_opts, cb) {
+    return cb(null, sendMaxInfoMock);
+  }
+  _doJoinWallet(
+    _walletId,
+    _walletPrivKey,
+    _xPubKey,
+    _requestPubKey,
+    _copayerName,
+    _opts,
+    cb
+  ) {
+    return cb(null);
+  }
+  createAddress(_opts, cb) {
+    const addr = {
+      address: 'address2'
+    };
+    return cb(null, addr);
+  }
+}
