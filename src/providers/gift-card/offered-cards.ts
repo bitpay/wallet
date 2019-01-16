e5cff03fefbd79635ef811240a25ab1848b7af11
Marty Alcala <marty@bitpay.com>
2019-01-16 18:04:23 -0500

    group gift cards by featured/more, improve active gift card load performance, and add more brands

--- src/providers/gift-card/offered-cards.ts
+++ src/providers/gift-card/offered-cards.ts
@@ -1,24 +1,26 @@
-import { BaseCardConfig, CardBrand, CardName } from './gift-card.types';
+import {
+  BaseCardConfig,
+  CardBrand,
+  CardName,
+  ClaimCodeType
+} from './gift-card.types';
 
-export const offeredGiftCards: BaseCardConfig[] = [
+export const offeredGiftCards: BaseCardConfig[] = ([
   {
     brand: CardBrand.venue, // For Testnet
-    cardImage: 'https://app.giftango.com/GPCGraphics/CIR_000717_00.png',
+    cardImage: `${getCardImageDirectory(CardName.venue)}/card.png`,
     defaultClaimCodeType: 'code',
     emailRequired: false,
-    icon: 'https://app.giftango.com/GPCGraphics/CIR_000717_00.png',
-    logo: 'https://app.giftango.com/GPCGraphics/CIR_000717_00.png',
+    icon: `${getCardImageDirectory(CardName.venue)}/card.png`,
+    logo: `${getCardImageDirectory(CardName.venue)}/card.png`,
     logoBackgroundColor: '#913318',
     name: CardName.venue,
     website: 'venue.com'
   },
   {
     brand: CardBrand.amazon,
-    cardImage: 'assets/img/gift-cards/amazon/amazon-gift-card.png',
-    defaultClaimCodeType: 'code',
+    defaultClaimCodeType: ClaimCodeType.code,
     emailRequired: true,
-    icon: 'assets/img/gift-cards/amazon/amazon-icon.svg',
-    logo: 'assets/img/gift-cards/amazon/amazon-logo.png',
     logoBackgroundColor: '#363636',
     name: CardName.amazon,
     redeemUrl: 'https://www.amazon.com/gc/redeem?claimCode=',
@@ -26,49 +28,78 @@ export const offeredGiftCards: BaseCardConfig[] = [
   },
   {
     brand: CardBrand.amazon,
-    cardImage: 'assets/img/gift-cards/amazon/amazon-japan-gift-card.png',
-    defaultClaimCodeType: 'code',
+    defaultClaimCodeType: ClaimCodeType.code,
     emailRequired: true,
-    icon: 'assets/img/gift-cards/amazon/amazon-icon.svg',
-    logo: 'assets/img/gift-cards/amazon/amazon-logo.png',
     logoBackgroundColor: '#363636',
     name: CardName.amazonJapan,
     redeemUrl: 'https://www.amazon.co.jp/gc/redeem?claimCode=',
     website: 'amazon.co.jp'
   },
   {
-    brand: CardBrand.carnivalCruiseLine,
-    cardImage:
-      'assets/img/gift-cards/carnival-cruise-line/carnival-cruise-line-card.png',
-    defaultClaimCodeType: 'code',
+    brand: CardBrand.barnesNoble,
+    defaultClaimCodeType: ClaimCodeType.barcode,
     emailRequired: false,
-    icon:
-      'assets/img/gift-cards/carnival-cruise-line/carnival-cruise-line-icon.png',
-    logo:
-      'assets/img/gift-cards/carnival-cruise-line/carnival-cruise-line-logo.svg',
+    logoBackgroundColor: '#356251',
+    name: CardName.barnesNoble,
+    website: 'barnesandnoble.com'
+  },
+  {
+    brand: CardBrand.bassProShops,
+    defaultClaimCodeType: ClaimCodeType.barcode,
+    emailRequired: false,
+    icon: `${getCardImageDirectory(CardName.bassProShops)}icon.png`,
+    logo: `${getCardImageDirectory(CardName.bassProShops)}logo.png`,
+    logoBackgroundColor: '#ffffff',
+    name: CardName.bassProShops,
+    website: 'basspro.com'
+  },
+  {
+    brand: CardBrand.burgerKing,
+    defaultClaimCodeType: ClaimCodeType.barcode,
+    emailRequired: false,
+    logoBackgroundColor: '#ffffff',
+    name: CardName.burgerKing,
+    website: 'bk.com'
+  },
+  {
+    brand: CardBrand.carnivalCruiseLine,
+    defaultClaimCodeType: ClaimCodeType.code,
+    emailRequired: false,
+    icon: `${getCardImageDirectory(CardName.carnivalCruiseLine)}icon.png`,
     logoBackgroundColor: '#ffffff',
     name: CardName.carnivalCruiseLine,
     website: 'carnival.com'
   },
   {
     brand: CardBrand.delta,
-    cardImage: 'assets/img/gift-cards/delta/delta-gift-card-white.png',
-    defaultClaimCodeType: 'link',
+    defaultClaimCodeType: ClaimCodeType.link,
     emailRequired: false,
-    icon: 'assets/img/gift-cards/delta/delta-icon-white.svg',
-    logo: 'assets/img/gift-cards/delta/delta-logo-white.svg',
     logoBackgroundColor: '#ffffff',
     name: CardName.delta,
     website: 'delta.com'
   },
   {
+    brand: CardBrand.dsw,
+    defaultClaimCodeType: ClaimCodeType.barcode,
+    emailRequired: false,
+    logoBackgroundColor: '#000000',
+    name: CardName.dsw,
+    website: 'dsw.com'
+  },
+  {
+    brand: CardBrand.gamestop,
+    defaultClaimCodeType: ClaimCodeType.barcode,
+    emailRequired: false,
+    logoBackgroundColor: '#000000',
+    name: CardName.gamestop,
+    website: 'gamestop.com'
+  },
+  {
     brand: CardBrand.googlePlay,
-    cardImage: 'assets/img/gift-cards/google-play/google-play-gift-card.png',
-    defaultClaimCodeType: 'code',
+    defaultClaimCodeType: ClaimCodeType.code,
     emailRequired: false,
     hidePin: true,
-    icon: 'assets/img/gift-cards/google-play/google-play-icon.png',
-    logo: 'assets/img/gift-cards/google-play/google-play-logo.svg',
+    icon: `${getCardImageDirectory(CardName.googlePlay)}icon.png`,
     logoBackgroundColor: '#ffffff',
     name: CardName.googlePlay,
     redeemUrl: 'https://play.google.com/redeem?code=',
@@ -76,68 +107,88 @@ export const offeredGiftCards: BaseCardConfig[] = [
   },
   {
     brand: CardBrand.homeDepot,
-    cardImage: 'assets/img/gift-cards/home-depot/home-depot-card.png',
-    defaultClaimCodeType: 'barcode',
+    defaultClaimCodeType: ClaimCodeType.barcode,
     emailRequired: false,
-    icon: 'assets/img/gift-cards/home-depot/home-depot-icon.svg',
-    logo: 'assets/img/gift-cards/home-depot/home-depot-logo.svg',
     logoBackgroundColor: '#E17232',
     name: CardName.homeDepot,
     website: 'homedepot.com'
   },
   {
     brand: CardBrand.hotelsCom,
-    cardImage: 'assets/img/gift-cards/hotels.com/hotels.com-card.png',
-    defaultClaimCodeType: 'code',
+    defaultClaimCodeType: ClaimCodeType.code,
     emailRequired: false,
-    icon: 'assets/img/gift-cards/hotels.com/hotels.com-icon.svg',
-    logo: 'assets/img/gift-cards/hotels.com/hotels.com-logo.svg',
     logoBackgroundColor: '#DB4545',
     name: CardName.hotelsCom,
     website: 'hotels.com'
   },
   {
     brand: CardBrand.mercadoLibre,
-    cardImage: 'assets/img/gift-cards/mercado-libre/mercado-livre-card.png',
-    defaultClaimCodeType: 'code',
+    defaultClaimCodeType: ClaimCodeType.code,
     emailRequired: false,
-    icon: 'assets/img/gift-cards/mercado-libre/mercado-livre-icon.svg',
-    logo: 'assets/img/gift-cards/mercado-libre/mercado-livre-logo.png',
     logoBackgroundColor: '#ffffff',
     name: CardName.mercadoLibre,
     website: 'mercadolivre.com.br'
   },
   {
-    brand: CardBrand.royalCaribbean,
-    cardImage: 'assets/img/gift-cards/royal-caribbean/royal-caribbean-card.png',
-    defaultClaimCodeType: 'link',
+    brand: CardBrand.papaJohns,
+    defaultClaimCodeType: ClaimCodeType.barcode,
     emailRequired: false,
-    icon: 'assets/img/gift-cards/royal-caribbean/royal-caribbean-icon.svg',
-    logo: 'assets/img/gift-cards/royal-caribbean/royal-caribbean-logo.svg',
+    icon: `${getCardImageDirectory(CardName.papaJohns)}icon.png`,
+    logo: `${getCardImageDirectory(CardName.papaJohns)}logo.png`,
+    logoBackgroundColor: '#AC2A25',
+    name: CardName.papaJohns,
+    website: 'papajohns.com'
+  },
+  {
+    brand: CardBrand.potteryBarn,
+    defaultClaimCodeType: ClaimCodeType.barcode,
+    emailRequired: false,
+    logoBackgroundColor: '#1E355E',
+    name: CardName.potteryBarn,
+    website: 'potterybarn.com'
+  },
+  {
+    brand: CardBrand.royalCaribbean,
+    defaultClaimCodeType: ClaimCodeType.link,
+    emailRequired: false,
     logoBackgroundColor: '#0668A4',
     name: CardName.royalCaribbean,
     website: 'royalcaribbean.com'
   },
   {
-    brand: CardBrand.uber,
-    cardImage: 'assets/img/gift-cards/uber/uber-gift-card.png',
-    defaultClaimCodeType: 'code',
+    brand: CardBrand.spotify,
+    defaultClaimCodeType: ClaimCodeType.code,
     emailRequired: false,
-    icon: 'assets/img/gift-cards/uber/uber-icon.svg',
-    logo: 'assets/img/gift-cards/uber/uber-logo.png',
+    logoBackgroundColor: '#26CE7B',
+    name: CardName.spotify,
+    website: 'spotify.com'
+  },
+  {
+    brand: CardBrand.uber,
+    defaultClaimCodeType: ClaimCodeType.code,
+    emailRequired: false,
     logoBackgroundColor: '#000000',
     name: CardName.uber,
     website: 'uber.com'
   },
   {
     brand: CardBrand.uberEats,
-    cardImage: 'assets/img/gift-cards/uber-eats/uber-eats-card.png',
-    defaultClaimCodeType: 'code',
+    defaultClaimCodeType: ClaimCodeType.code,
     emailRequired: false,
-    icon: 'assets/img/gift-cards/uber-eats/uber-eats-icon.svg',
-    logo: 'assets/img/gift-cards/uber-eats/uber-eats-logo.svg',
     logoBackgroundColor: '#000000',
     name: CardName.uberEats,
     website: 'uber.com'
   }
-];
+] as BaseCardConfig[]).map(c => ({
+  ...c,
+  cardImage: c.cardImage || `${getCardImageDirectory(c.name)}card.png`,
+  icon: c.icon || `${getCardImageDirectory(c.name)}icon.svg`,
+  logo: c.logo || `${getCardImageDirectory(c.name)}logo.svg`
+}));
+
+function getCardImageDirectory(cardName: CardName) {
+  const cardImagePath = 'assets/img/gift-cards/';
+  return `${cardImagePath}${cardName
+    .toLowerCase()
+    .replace(/[^0-9a-z]/gi, '')}/`;
+}
