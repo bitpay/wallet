ce371316dcf0acdc75a7660c04a122b676c1a306
Gabriel Masclef <gabrielmasclef@hotmail.com>
2019-02-14 16:31:38 -0300

    Fix: Unit tests for lifecycles in home page

--- src/pages/home/home.spec.ts
+++ src/pages/home/home.spec.ts
@@ -29,71 +29,69 @@ describe('HomePage', () => {
   });
 
   describe('Lifecycle Hooks', () => {
-    describe('ionViewWillEnter', () => {
-      describe('ionViewDidEnter', () => {
-        it('should check clipboard', () => {
-          const spy = spyOn(instance, 'checkClipboard');
-          instance.ionViewDidEnter();
-          expect(spy).toHaveBeenCalled();
-        });
-        it('should subscribe to incoming data menu event', () => {
-          const spy = spyOn(instance, 'subscribeIncomingDataMenuEvent');
-          instance.ionViewDidEnter();
-          expect(spy).toHaveBeenCalled();
-        });
-        it('should subscribe to bws events', () => {
-          const spy = spyOn(instance, 'subscribeBwsEvents');
-          instance.ionViewDidEnter();
-          expect(spy).toHaveBeenCalled();
-        });
-      });
-
-      describe('ionViewDidLoad', () => {
-        it('should update wallets on platform resume', () => {
-          instance.plt.resume = new Subject();
-          instance.plt.pause = new Subject();
-          instance.ionViewDidLoad();
-          const setWalletsSpy = spyOn(instance, 'setWallets');
-          instance.plt.resume.next();
-          expect(setWalletsSpy).toHaveBeenCalled();
-        });
-      });
-
-      describe('ionViewWillLeave', () => {
-        it('should unsubscribe from bwsEvent event', () => {
-          const spy = spyOn(instance.events, 'unsubscribe');
-          instance.ionViewWillLeave();
-          expect(spy).toHaveBeenCalledWith('bwsEvent');
-        });
+    describe('ionViewDidEnter', () => {
+      it('should check clipboard', () => {
+        const spy = spyOn(instance, 'checkClipboard');
+        instance.ionViewDidEnter();
+        expect(spy).toHaveBeenCalled();
       });
     });
 
-    describe('Methods', () => {
-      describe('checkClipboard', () => {
-        let incomingDataProvider: IncomingDataProvider;
-        beforeEach(() => {
-          const clipboardProvider: ClipboardProvider = testBed.get(
-            ClipboardProvider
-          );
-          incomingDataProvider = testBed.get(IncomingDataProvider);
-          spyOn(clipboardProvider, 'getData').and.returnValue(
-            Promise.resolve()
-          );
+    describe('ionViewDidLoad', () => {
+      beforeEach(() => {
+        instance.plt.resume = new Subject();
+        instance.plt.pause = new Subject();
+      });
+      it('should subscribe to incoming data menu event', () => {
+        const spy = spyOn(instance, 'subscribeIncomingDataMenuEvent');
+        instance.ionViewDidLoad();
+        expect(spy).toHaveBeenCalled();
+      });
+      it('should subscribe to bws events', () => {
+        const spy = spyOn(instance, 'subscribeBwsEvents');
+        instance.ionViewDidLoad();
+        expect(spy).toHaveBeenCalled();
+      });
+      it('should update wallets on platform resume', () => {
+        instance.ionViewDidLoad();
+        const setWalletsSpy = spyOn(instance, 'setWallets');
+        instance.plt.resume.next();
+        expect(setWalletsSpy).toHaveBeenCalled();
+      });
+    });
+
+    describe('ionViewWillLeave', () => {
+      it('should call resetValuesForAnimationCard', () => {
+        const spy = spyOn(instance, 'resetValuesForAnimationCard');
+        instance.ionViewWillLeave();
+        expect(spy).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('Methods', () => {
+    describe('checkClipboard', () => {
+      let incomingDataProvider: IncomingDataProvider;
+      beforeEach(() => {
+        const clipboardProvider: ClipboardProvider = testBed.get(
+          ClipboardProvider
+        );
+        incomingDataProvider = testBed.get(IncomingDataProvider);
+        spyOn(clipboardProvider, 'getData').and.returnValue(Promise.resolve());
+      });
+      it('should ignore BitcoinAddress', async () => {
+        spyOn(incomingDataProvider, 'parseData').and.returnValue({
+          type: 'BitcoinAddress'
         });
-        it('should ignore BitcoinAddress', async () => {
-          spyOn(incomingDataProvider, 'parseData').and.returnValue({
-            type: 'BitcoinAddress'
-          });
-          await instance.checkClipboard();
-          expect(instance.validDataFromClipboard).toBeNull();
+        await instance.checkClipboard();
+        expect(instance.validDataFromClipboard).toBeNull();
+      });
+      it('should ignore BitcoinCashAddress', async () => {
+        spyOn(incomingDataProvider, 'parseData').and.returnValue({
+          type: 'BitcoinCashAddress'
         });
-        it('should ignore BitcoinCashAddress', async () => {
-          spyOn(incomingDataProvider, 'parseData').and.returnValue({
-            type: 'BitcoinCashAddress'
-          });
-          await instance.checkClipboard();
-          expect(instance.validDataFromClipboard).toBeNull();
-        });
+        await instance.checkClipboard();
+        expect(instance.validDataFromClipboard).toBeNull();
       });
     });
   });
