7efb387c9b52939036cf0051484e260457ac944f
Gabriel Bazán <gabrielbazan7@hotmail.com>
2019-03-12 17:27:22 -0400

    [FIX] tests

--- src/pages/add/create-wallet/create-wallet.spec.ts
+++ src/pages/add/create-wallet/create-wallet.spec.ts
@@ -20,13 +20,11 @@ describe('CreateWalletPage', () => {
   });
 
   describe('setTotalCopayers function', () => {
-    it('should call updateRCSelect and updateSeedSourceSelect functions', () => {
+    it('should call updateRCSelect functions', () => {
       const n = 3;
       const spy = spyOn(instance, 'updateRCSelect');
-      const secondSpy = spyOn(instance, 'updateSeedSourceSelect');
       instance.setTotalCopayers(n);
       expect(spy).toHaveBeenCalledWith(n);
-      expect(secondSpy).toHaveBeenCalled();
     });
   });
 
@@ -41,10 +39,7 @@ describe('CreateWalletPage', () => {
         networkName: 'livenet',
         bwsurl: 'https://bws.bitpay.com/bws/api',
         singleAddress: false,
-        coin: 'btc',
-        mnemonic: 'mom mom mom mom mom mom mom mom mom mom mom mom',
-        derivationStrategy: 'BIP44',
-        account: 0
+        coin: 'btc'
       };
 
       instance.createForm.value.walletName = 'test';
@@ -52,13 +47,9 @@ describe('CreateWalletPage', () => {
       instance.createForm.value.requiredCopayers = 2;
       instance.createForm.value.totalCopayers = 3;
       instance.createForm.value.testnetEnabled = false;
-      instance.createForm.value.derivationPath = "m/44'/0'/0'";
       instance.createForm.value.bwsURL = 'https://bws.bitpay.com/bws/api';
       instance.createForm.value.singleAddress = false;
       instance.createForm.value.coin = 'btc';
-      instance.createForm.value.selectedSeed = 'set';
-      instance.createForm.value.recoveryPhrase =
-        'mom mom mom mom mom mom mom mom mom mom mom mom';
 
       instance.setOptsAndCreate();
       expect(spy).toHaveBeenCalledWith(opts);
