dcb1fc67e95378433b265524c02af33e560a083e
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2019-04-15 17:03:07 -0300

    Fix chrome test for circleci

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,7 +27,13 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
