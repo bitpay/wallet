294074e2ff8b5832d0f152813bd872fbb802d815
Justin Kook <jkook@bitpay.com>
2019-04-25 16:28:00 -0400

    Added parsing invoiceUri in home.ts test

--- src/pages/home/home.spec.ts
+++ src/pages/home/home.spec.ts
@@ -105,6 +105,13 @@ describe('HomePage', () => {
         await instance.checkClipboard();
         expect(instance.validDataFromClipboard).toBeNull();
       });
+      it('should render clipboard html on InvoiceUri', async () => {
+        spyOn(incomingDataProvider, 'parseData').and.returnValue({
+          type: 'InvoiceUri'
+        });
+        await instance.checkClipboard();
+        expect(instance.validDataFromClipboard.type).toEqual('InvoiceUri');
+      });
     });
   });
 });
