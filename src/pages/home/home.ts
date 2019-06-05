fbf8c1cd0fdf2ac3bd03f98a6621d4c5a10dead8
Marty Alcala <marty@bitpay.com>
2019-06-05 14:10:32 -0400

    remove unused code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -805,27 +805,4 @@ export class HomePage {
   public settings(): void {
     this.navCtrl.push(SettingsPage);
   }
-
-  getBalance(wallet, currency) {
-    const lastKnownBalance = this.getLastKownBalance(wallet, currency);
-    const totalBalanceStr =
-      wallet.cachedStatus &&
-      wallet.cachedStatus.totalBalanceStr &&
-      wallet.cachedStatus.totalBalanceStr.replace(` ${currency}`, '');
-    return totalBalanceStr || lastKnownBalance;
-  }
-
-  getLastKownBalance(wallet, currency) {
-    return (
-      wallet.lastKnownBalance &&
-      wallet.lastKnownBalance.replace(` ${currency}`, '')
-    );
-  }
-
-  hasZeroBalance(wallet, currecy) {
-    return (
-      (wallet.cachedStatus && wallet.cachedStatus.totalBalanceSat === 0) ||
-      this.getLastKownBalance(wallet, currecy) === '0.00'
-    );
-  }
 }
