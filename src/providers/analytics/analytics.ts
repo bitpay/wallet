2efff3c74a5cf3b5a27277b6b9db43b48129db87
Marty Alcala <marty@bitpay.com>
2019-06-28 15:39:06 -0400

    standardize discount event params

--- src/providers/analytics/analytics.ts
+++ src/providers/analytics/analytics.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { CardConfig } from '../gift-card/gift-card.types';
 
 @Injectable()
 export class AnalyticsProvider {
   trackEvent(eventName: string, eventParams: { [key: string]: any }) {
     console.log(eventName, eventParams);
+  }
+
+  getDiscountEventParams(discountedCard: CardConfig, context?: string) {
+    const discount = discountedCard.discounts[0];
+    return {
+      brand: discountedCard.name,
+      code: discount.code,
+      context,
+      percentage: discount.amount
+    };
   }
 }
