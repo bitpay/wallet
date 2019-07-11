9fcfa21483898a4284ba8290d6096bd810679030
Justin Kook <jkook@bitpay.com>
2019-07-11 14:51:19 -0400

    lint and prettier home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -468,8 +468,8 @@ export class HomePage {
             this.payProDetailsData.amount = selectedTransactionCurrency
               ? paymentTotals[selectedTransactionCurrency]
               : Coin[currency]
-                ? price / 1e-8
-                : price;
+              ? price / 1e-8
+              : price;
             this.clearCountDownInterval();
             this.paymentTimeControl(expirationTime);
           } catch (err) {
@@ -590,9 +590,9 @@ export class HomePage {
 
     this.logger.debug(
       'fetching status for: ' +
-      opts.walletId +
-      ' alsohistory:' +
-      opts.alsoUpdateHistory
+        opts.walletId +
+        ' alsohistory:' +
+        opts.alsoUpdateHistory
     );
     const wallet = this.profileProvider.getWallet(opts.walletId);
     if (!wallet) return;
