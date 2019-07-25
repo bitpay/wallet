0f306b8a86f917a1ca6f1bfc45089776c282123b
Gabriel Bazán <gabrielbazan7@hotmail.com>
2019-07-25 10:36:49 -0300

    [FIX] remove keys from backup file

--- src/providers/backup/backup.ts
+++ src/providers/backup/backup.ts
@@ -65,8 +65,6 @@ export class BackupProvider {
       let b: any = {};
 
       b.credentials = JSON.parse(wallet.toString(opts));
-      if (wallet.canSign)
-        b.key = this.keyProvider.getKey(wallet.credentials.keyId);
       if (opts.addressBook) b.addressBook = opts.addressBook;
       b = JSON.stringify(b);
 
