d963ffec5b7d07ebce9350a5b91b3560b775d513
johnathan White <jwhite@bitpay.com>
2019-11-04 15:51:11 -0500

    added patch for iab status bar height on ios devices

---
+++ utils/iab-patch.js
@@ -0,0 +1,15 @@
+/*
+* Patches the default status bar height to 0 - needed for notched ios devices (iphonex etc)
+* */
+
+const fs = require('fs');
+const file = `${__dirname}/../platforms/ios/BitPay/Plugins/cordova-plugin-inappbrowser/CDVInAppBrowserNavigationController.m`;
+
+fs.readFile(file, 'utf8', (err,data) => {
+  if (err) throw err;
+  const result = data.replace(/STATUSBAR_HEIGHT/g, '0');
+  fs.writeFile(file, result, 'utf8', function (err) {
+    if (err) throw err;
+    console.log('successfully patched iab',);
+  });
+});
