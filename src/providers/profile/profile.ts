45bdcb107a960b9e990bcad1e6c7af91ef6c8782
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2019-11-13 01:27:58 -0300

    fix(new wallet): force saving wallet at creation of new key

--- src/providers/profile/profile.ts
+++ src/providers/profile/profile.ts
@@ -1436,7 +1436,10 @@ export class ProfileProvider {
                   });
                   Promise.all(bindWalletClients)
                     .then(walletClients => {
-                      return resolve(walletClients);
+                      this.storeProfileIfDirty().then(() => {
+                        this.events.publish('Local/WalletListChange');
+                        return resolve(walletClients);
+                      });
                     })
                     .catch(e => {
                       reject(e);
