0e44293c54c04d99f04f16d36066ab2dbd21be9f
Justin Kook <jkook@bitpay.com>
2019-11-25 16:48:48 -0500

    remove overrides from variables.scss

--- heroku.js
+++ heroku.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var cors = require('cors');
@@ -19,7 +20,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(express.static('www'));
+app.use(express.static(path.join(__dirname, 'www')));
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
