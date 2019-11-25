db1aad41c91c6d3472e80a8b2d8d6b0ade249148
Justin Kook <jkook@bitpay.com>
2019-11-25 15:20:53 -0500

    Added Procfile for heroku CLI build steps

--- heroku.js
+++ heroku.js
@@ -5,19 +5,22 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ 'extended': 'true' }));
+app.use(bodyParser.urlencoded({ extended: 'true' }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function () {
+app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
