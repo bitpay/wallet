f977d934329498669c126a38ec094e23ad0ecd87
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2019-11-27 17:00:49 -0300

    Store profile when joinWallet and importFile

--- src/providers/profile/profile.ts
+++ src/providers/profile/profile.ts
@@ -867,7 +867,8 @@ export class ProfileProvider {
         this.onGoingProcessProvider.resume();
         return this.keyProvider.addKey(data.key).then(() => {
           return this.addAndBindWalletClient(data.walletClient, {
-            bwsurl: opts.bwsurl
+            bwsurl: opts.bwsurl,
+            store: true
           }).then(walletClient => {
             return this.checkIfAlreadyExist([].concat(walletClient)).then(
               () => {
@@ -1610,7 +1611,8 @@ export class ProfileProvider {
           this.onGoingProcessProvider.resume();
           return this.keyProvider.addKey(data.key).then(() => {
             return this.addAndBindWalletClient(data.walletClient, {
-              bwsurl: opts.bwsurl
+              bwsurl: opts.bwsurl,
+              store: true
             }).then(walletClient => {
               return Promise.resolve(walletClient);
             });
