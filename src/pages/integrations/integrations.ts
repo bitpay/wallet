91e5a5fc289e9c7778048035a490628e29bf5356
Gabriel Bazán <gabrielbazan7@hotmail.com>
2020-01-13 16:44:10 -0300

    unused variable

--- src/pages/integrations/integrations.ts
+++ src/pages/integrations/integrations.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import * as _ from 'lodash';
 import { CoinbasePage } from '../integrations/coinbase/coinbase';
 import { ShapeshiftPage } from '../integrations/shapeshift/shapeshift';
 
