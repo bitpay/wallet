cbbb6ceec5961fb35cce4bdb0080a3655aa85fec
johnathan White <jwhite@bitpay.com>
2020-01-15 15:54:51 -0500

    updated in app browser config options

--- src/providers/in-app-browser/in-app-browser.ts
+++ src/providers/in-app-browser/in-app-browser.ts
@@ -6,7 +6,7 @@ import { ActionSheetProvider } from '../../providers/action-sheet/action-sheet';
 import { Logger } from '../../providers/logger/logger';
 
 const IAB_CONFIG =
-  'directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,hidden=yes,clearcache=yes,hidespinner=yes';
+  'directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,hidden=yes,clearcache=yes,hidespinner=yes,disallowoverscroll=yes,zoom=no';
 
 @Injectable()
 export class InAppBrowserProvider {
