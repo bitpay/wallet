cc1e30db823bc98d344026bd2e98e2b4c4153c31
Bernard Snowden <bsnowden@bitpay.com>
2020-01-23 13:42:12 -0500

    add catch block for timed out paypro response

--- src/providers/paypro/paypro.ts
+++ src/providers/paypro/paypro.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Logger } from '../../providers/logger/logger';
 
 // providers
+import { ActionSheetProvider } from '../action-sheet/action-sheet';
 import { BwcProvider } from '../bwc/bwc';
 import { CurrencyProvider } from '../currency/currency';
 import { OnGoingProcessProvider } from '../on-going-process/on-going-process';
@@ -12,9 +13,23 @@ export class PayproProvider {
     private logger: Logger,
     private bwcProvider: BwcProvider,
     private currencyProvider: CurrencyProvider,
-    private onGoingProcessProvider: OnGoingProcessProvider
+    private onGoingProcessProvider: OnGoingProcessProvider,
+    private actionSheetProvider: ActionSheetProvider
   ) {
     this.logger.debug('PayproProvider initialized');
+  }
+
+  private showErrorInfoSheet(
+    err: Error | string,
+    infoSheetTitle: string
+  ): void {
+    if (!err) return;
+    this.logger.error('Could not get payment information:', err);
+    const errorInfoSheet = this.actionSheetProvider.createInfoSheet(
+      'default-error',
+      { msg: err, title: infoSheetTitle }
+    );
+    errorInfoSheet.present();
   }
 
   public getPayProOptions(paymentUrl, disableLoader?: boolean): Promise<any> {
@@ -26,10 +41,17 @@ export class PayproProvider {
       this.onGoingProcessProvider.set('fetchingPayProOptions');
     }
 
-    return bwc.getPaymentOptions(options).then(payProOptions => {
-      if (!disableLoader) this.onGoingProcessProvider.clear();
-      return payProOptions;
-    });
+    return bwc
+      .getPaymentOptions(options)
+      .then(payProOptions => {
+        if (!disableLoader) this.onGoingProcessProvider.clear();
+        return payProOptions;
+      })
+      .catch(error => {
+        this.logger.debug(error);
+        this.onGoingProcessProvider.clear();
+        this.showErrorInfoSheet('Could not fetch payment options', 'Error');
+      });
   }
 
   public getPayProDetails(
@@ -48,9 +70,16 @@ export class PayproProvider {
       this.onGoingProcessProvider.set('fetchingPayPro');
     }
 
-    return bwc.selectPaymentOption(options).then(payProDetails => {
-      if (!disableLoader) this.onGoingProcessProvider.clear();
-      return payProDetails;
-    });
+    return bwc
+      .selectPaymentOption(options)
+      .then(payProDetails => {
+        if (!disableLoader) this.onGoingProcessProvider.clear();
+        return payProDetails;
+      })
+      .catch(error => {
+        this.logger.debug(error);
+        this.onGoingProcessProvider.clear();
+        this.showErrorInfoSheet('Could not fetch payment details', 'Error');
+      });
   }
 }
