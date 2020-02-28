a561eebffd2eb1b95e315e8a789f527c85a00d05
Gabriel Bazán <gabrielbazan7@hotmail.com>
2020-02-28 10:38:47 -0300

    [FIX] pin pad

--- src/pages/pin/pin-pad/pin-pad.component.ts
+++ src/pages/pin/pin-pad/pin-pad.component.ts
@@ -29,13 +29,6 @@ export interface PinButton {
           <span *ngSwitchCase="'.'">
             <span *ngIf="type === 'amount'">.</span>
           </span>
-          <span
-            *ngSwitchCase="'0'"
-            class="key-wrapper"
-            [ngClass]="{ 'swap-key': type === 'amount' }"
-          >
-            <span>0</span>
-          </span>
           <span *ngSwitchDefault>{{ button.value }}</span>
         </div>
         <div class="letters" *ngIf="type === 'pin'">{{ button.letters }}</div>
