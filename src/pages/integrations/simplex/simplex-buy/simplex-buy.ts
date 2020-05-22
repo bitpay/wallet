b0ef78c938f4d536d4c5ee2cf70107dc2c2dc4e5
Gabriel Masclef <gabrielmasclef@hotmail.com>
2020-05-22 17:23:25 -0300

    Feat: BUSD added to Simplex integration

--- src/pages/integrations/simplex/simplex-buy/simplex-buy.ts
+++ src/pages/integrations/simplex/simplex-buy/simplex-buy.ts
@@ -115,7 +115,7 @@ export class SimplexBuyPage {
     this.wallets = this.profileProvider.getWallets({
       network: 'livenet',
       onlyComplete: true,
-      coin: this.coin || ['btc', 'bch', 'eth', 'xrp', 'pax'],
+      coin: this.coin || ['btc', 'bch', 'eth', 'xrp', 'pax', 'busd'],
       backedUp: true
     });
     this.altCurrenciesToShow = ['USD', 'EUR'];
