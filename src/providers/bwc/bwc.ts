cc72d75fdad1f1fc1b23ac16df42248ac4490bc5
Bernard Snowden <bsnowden@bitpay.com>
2020-06-08 15:17:51 -0400

    local testing

--- src/providers/bwc/bwc.ts
+++ src/providers/bwc/bwc.ts
@@ -56,7 +56,8 @@ export class BwcProvider {
 
     // note opts use `bwsurl` all lowercase;
     let bwc = new BWC({
-      baseUrl: opts.bwsurl || 'https://bws.bitpay.com/bws/api',
+      baseUrl:
+        opts.bwsurl || 'https://bws.bitpay.com/bws/api' // 'http://localhost:3232/bws/api', uncmment for local testing
       verbose: opts.verbose,
       timeout: 100000,
       transports: ['polling'],
--- src/providers/config/config.ts
+++ src/providers/config/config.ts
@@ -168,7 +168,7 @@ export class ConfigProvider {
 
       // Bitcore wallet service URL
       bws: {
-        url: 'http://localhost:3232/bws/api'
+        url: 'https://bws.bitpay.com/bws/api' // Uncomment and replace w/ http://localhost:3232/bws/api for testing
       },
 
       adPubKey: {
