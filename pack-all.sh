1937cb46dc5693885a52bca06ec139e150c96bfd
Bernard Snowden <bsnowden@bitpay.com>
2020-06-08 15:27:35 -0400

    change pack-all version

--- pack-all.sh
+++ pack-all.sh
@@ -13,7 +13,7 @@ popd
 
  popd
 
-npm i ../../Desktop/clean-bitcore/packages/crypto-wallet-core/crypto-wallet-core-8.19.0.tgz
-npm i ../../Desktop/clean-bitcore/packages/bitcore-wallet-client/bitcore-wallet-client-8.19.0.tgz
+npm i ../../Desktop/clean-bitcore/packages/crypto-wallet-core/crypto-wallet-core-8.20.3.tgz
+npm i ../../Desktop/clean-bitcore/packages/bitcore-wallet-client/bitcore-wallet-client-8.20.3.tgz
 npm i
 npm start
