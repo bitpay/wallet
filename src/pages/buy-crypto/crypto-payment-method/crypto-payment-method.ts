f7028d5888c170a3a9bd55a020284254e2d80222
Juan Pablo Abuin <juanpabloabuin@gmail.com>
2020-06-25 16:49:37 -0300

    dark mode

--- src/pages/buy-crypto/crypto-payment-method/crypto-payment-method.ts
+++ src/pages/buy-crypto/crypto-payment-method/crypto-payment-method.ts
@@ -1,79 +1,65 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
+import * as _ from 'lodash';
 
 // Providers
 import { Logger } from '../../../providers/logger/logger';
+import { PlatformProvider } from '../../../providers/platform/platform';
 
 // Pages
 import { CryptoOrderSummaryPage } from '../../../pages/buy-crypto/crypto-order-summary/crypto-order-summary';
-
+export interface PaymentMethod {
+  label: any;
+  method: string;
+  imgSrc: string;
+  simplexSupport: boolean;
+  wyreSupport: boolean;
+  enabled: boolean;
+}
 @Component({
   selector: 'page-crypto-payment-method',
   templateUrl: 'crypto-payment-method.html'
 })
 export class CryptoPaymentMethodPage {
-  methods: {
-    applePay: {
-      label: any;
-      method: string;
-      imgSrc: string;
-      simplexSupport: boolean;
-    };
-    debitCard: {
-      label: any;
-      method: string;
-      imgSrc: string;
-      simplexSupport: boolean;
-    };
-    bankTransfer: {
-      label: any;
-      method: string;
-      imgSrc: string;
-      simplexSupport: boolean;
-    };
-    creditCard: {
-      label: any;
-      method: string;
-      imgSrc: string;
-      simplexSupport: boolean;
-    };
-  };
+  public methods: { [key: string]: PaymentMethod };
   public methodSelected: string;
   public paymentRequest;
   public useAsModal: boolean;
+  public isIOS: boolean;
 
   constructor(
     private logger: Logger,
     private navParams: NavParams,
     private translate: TranslateService,
     private navCtrl: NavController,
-    private viewCtrl: ViewController
+    private viewCtrl: ViewController,
+    private platformProvider: PlatformProvider
   ) {
     this.methods = {
+      applePay: {
+        label: this.translate.instant('Apple Pay'),
+        method: 'applePay',
+        imgSrc: 'assets/img/buy-crypto/apple-pay.svg',
+        simplexSupport: false,
+        wyreSupport: true,
+        enabled: this.platformProvider.isIOS
+      },
       creditCard: {
         label: this.translate.instant('Credit Card'),
         method: 'creditCard',
         imgSrc: 'assets/img/buy-crypto/debit-card.svg',
-        simplexSupport: true
+        simplexSupport: true,
+        wyreSupport: false,
+        enabled: true
       },
       debitCard: {
         label: this.translate.instant('Debit Card'),
         method: 'debitCard',
         imgSrc: 'assets/img/buy-crypto/debit-card.svg',
-        simplexSupport: true
-      },
-      applePay: {
-        label: this.translate.instant('Apple Pay'),
-        method: 'applePay',
-        imgSrc: 'assets/img/buy-crypto/apple-pay.svg',
-        simplexSupport: false
-      },
-      bankTransfer: {
-        label: this.translate.instant('Bank Transfer'),
-        method: 'bankTransfer',
-        imgSrc: 'assets/img/buy-crypto/apple-pay.svg',
-        simplexSupport: false
+        simplexSupport: true,
+        wyreSupport: true,
+        enabled: true
       }
     };
   }
@@ -83,6 +69,7 @@ export class CryptoPaymentMethodPage {
   }
 
   ionViewWillEnter() {
+    this.methods = _.pickBy(this.methods, m => m.enabled);
     this.methodSelected = this.navParams.data.paymentMethod || 'creditCard';
     this.useAsModal = this.navParams.data.useAsModal;
   }
