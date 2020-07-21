1005bae5e1fd5c92ba9a28219c6c91e660b744e5
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2020-07-21 13:40:02 -0300

    ref(logs): adds option to download/share logs if app crash

--- src/pages/settings/about/session-log/session-log.ts
+++ src/pages/settings/about/session-log/session-log.ts
@@ -2,15 +2,12 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ActionSheetController } from 'ionic-angular';
 
-// native
-import { SocialSharing } from '@ionic-native/social-sharing';
-
 // providers
 import { ActionSheetProvider } from '../../../../providers/action-sheet/action-sheet';
 import { AppProvider } from '../../../../providers/app/app';
 import { ConfigProvider } from '../../../../providers/config/config';
-import { DownloadProvider } from '../../../../providers/download/download';
 import { Logger } from '../../../../providers/logger/logger';
+import { LogsProvider } from '../../../../providers/logs/logs';
 import { PlatformProvider } from '../../../../providers/platform/platform';
 
 import * as _ from 'lodash';
@@ -26,20 +23,25 @@ export class SessionLogPage {
   public filteredLogs;
   public filterValue: number;
   public isCordova: boolean;
+  private platform: string;
 
   constructor(
     private configProvider: ConfigProvider,
     private logger: Logger,
-    private socialSharing: SocialSharing,
     private actionSheetCtrl: ActionSheetController,
     private platformProvider: PlatformProvider,
     private translate: TranslateService,
     private actionSheetProvider: ActionSheetProvider,
-    private downloadProvider: DownloadProvider,
-    private appProvider: AppProvider
+    private appProvider: AppProvider,
+    private logsProvider: LogsProvider
   ) {
     this.config = this.configProvider.get();
     this.isCordova = this.platformProvider.isCordova;
+    this.platform = this.isCordova
+      ? this.platformProvider.isAndroid
+        ? 'android'
+        : 'ios'
+      : 'desktop';
     const logLevels = this.logger.getLevels();
     this.logOptions = _.keyBy(logLevels, 'weight');
   }
@@ -71,95 +73,6 @@ export class SessionLogPage {
     this.configProvider.set(opts);
   }
 
-  private prepareSessionLogs() {
-    let log: string =
-      'Session Logs.\nBe careful, this could contain sensitive private data\n\n';
-    log += '\n\n';
-
-    const weight = 4; // share complete logs
-    const logs = _.sortBy(this.logger.get(weight), 'timestamp');
-
-    Object.keys(logs).forEach(key => {
-      log +=
-        '[' +
-        logs[key].timestamp +
-        '][' +
-        logs[key].level +
-        ']' +
-        logs[key].msg +
-        '\n';
-    });
-    return log;
-  }
-
-  private sendLogs(): void {
-    const logs = this.prepareSessionLogs();
-    const now = new Date().toISOString();
-    const subject: string = this.appProvider.info.nameCase + '-logs ' + now;
-    const message = this.translate.instant(
-      'Session Logs. Be careful, this could contain sensitive private data'
-    );
-
-    const blob = new Blob([logs], { type: 'text/txt' });
-
-    const reader = new FileReader();
-    reader.onload = event => {
-      const attachment = (event as any).target.result; // <-- data url
-
-      if (this.platformProvider.isAndroid) {
-        this.shareAndroid(message, subject, attachment);
-      } else {
-        this.shareIOS(message, subject, attachment);
-      }
-    };
-
-    reader.readAsDataURL(blob);
-  }
-
-  private shareAndroid(message, subject, attachment): void {
-    // share via email with attachment is not working correctly in some android versions
-    // so instead of shareViaEmail() -> share()
-    this.socialSharing.share(message, subject, attachment).catch(err => {
-      this.logger.error('socialSharing Error: ', err);
-    });
-  }
-
-  private shareIOS(message, subject, attachment): void {
-    // Check if sharing via email is supported
-    this.socialSharing
-      .canShareViaEmail()
-      .then(() => {
-        this.logger.info('sharing via email is possible');
-        this.socialSharing
-          .shareViaEmail(
-            message,
-            subject,
-            null, // TO: must be null or an array
-            null, // CC: must be null or an array
-            null, // BCC: must be null or an array
-            attachment // FILES: can be null, a string, or an array
-          )
-          .then(data => {
-            this.logger.info('Email created successfully: ', data);
-          })
-          .catch(err => {
-            this.logger.error('socialSharing Error: ', err);
-          });
-      })
-      .catch(() => {
-        this.logger.warn('sharing via email is not possible');
-        this.socialSharing
-          .share(
-            message,
-            subject,
-            attachment // FILES: can be null, a string, or an array
-          )
-          .catch(err => {
-            this.logger.error('socialSharing Error: ', err);
-          });
-      });
-  }
-
   public showOptionsMenu(): void {
     const downloadText = this.translate.instant('Download logs');
     const shareText = this.translate.instant('Share logs');
@@ -185,14 +98,8 @@ export class SessionLogPage {
     );
     infoSheet.present();
     infoSheet.onDidDismiss(option => {
-      if (option) this.isCordova ? this.sendLogs() : this.download();
+      if (option)
+        this.logsProvider.get(this.appProvider.info.nameCase, this.platform);
     });
-  }
-
-  public download(): void {
-    const logs = this.prepareSessionLogs();
-    const now = new Date().toISOString();
-    const filename = this.appProvider.info.nameCase + '-logs ' + now + '.txt';
-    this.downloadProvider.download(logs, filename);
   }
 }
