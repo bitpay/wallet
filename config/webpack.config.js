d9cb07248620a813e6ba20b57e626e8c09d14e1b
Bernard Snowden <bsnowden@bitpay.com>
2020-10-13 10:27:49 -0400

    add webpack-obfuscator

---
+++ config/webpack.config.js
@@ -0,0 +1,212 @@
+/*
+ * The webpack config exports an object that has a valid webpack configuration
+ * For each environment name. By default, there are two Ionic environments:
+ * "dev" and "prod". As such, the webpack.config.js exports a dictionary object
+ * with "keys" for "dev" and "prod", where the value is a valid webpack configuration
+ * For details on configuring webpack, see their documentation here
+ * https://webpack.js.org/configuration/
+ */
+
+var path = require('path');
+var webpack = require('webpack');
+const JavaScriptObfuscator = require('webpack-obfuscator');
+var ionicWebpackFactory = require(process.env.IONIC_WEBPACK_FACTORY);
+const Dotenv = require('dotenv-webpack');
+
+var ModuleConcatPlugin = require('webpack/lib/optimize/ModuleConcatenationPlugin');
+var PurifyPlugin = require('@angular-devkit/build-optimizer').PurifyPlugin;
+
+var optimizedProdLoaders = [{
+    test: /\.json$/,
+    loader: 'json-loader'
+  },
+  {
+    test: /\.js$/,
+    loader: [{
+        loader: process.env.IONIC_CACHE_LOADER
+      },
+
+      {
+        loader: '@angular-devkit/build-optimizer/webpack-loader',
+        options: {
+          sourceMap: true
+        }
+      }
+    ]
+  },
+  {
+    test: /\.ts$/,
+    loader: [{
+        loader: process.env.IONIC_CACHE_LOADER
+      },
+
+      {
+        loader: '@angular-devkit/build-optimizer/webpack-loader',
+        options: {
+          sourceMap: true
+        }
+      },
+
+      {
+        loader: process.env.IONIC_WEBPACK_LOADER
+      }
+    ]
+  }
+];
+
+function getProdLoaders() {
+  if (process.env.IONIC_OPTIMIZE_JS === 'true') {
+    return optimizedProdLoaders;
+  }
+  return devConfig.module.loaders;
+}
+
+var devConfig = {
+  entry: process.env.IONIC_APP_ENTRY_POINT,
+  output: {
+    path: '{{BUILD}}',
+    publicPath: 'build/',
+    filename: '[name].js',
+    devtoolModuleFilenameTemplate: ionicWebpackFactory.getSourceMapperFunction()
+  },
+  devtool: process.env.IONIC_SOURCE_MAP_TYPE,
+
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    modules: [path.resolve('node_modules')]
+  },
+
+  module: {
+    loaders: [{
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.ts$/,
+        loader: process.env.IONIC_WEBPACK_LOADER
+      }
+    ]
+  },
+
+  plugins: [
+    new Dotenv({
+      path: '.env.dev', // load this now instead of the ones in '.env'
+      systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+      silent: true // hide any errors
+    }),
+    new JavaScriptObfuscator({
+        compact: true,
+        controlFlowFlattening: false,
+        deadCodeInjection: false,
+        debugProtection: false,
+        debugProtectionInterval: false,
+        disableConsoleOutput: false,
+        identifierNamesGenerator: 'hexadecimal',
+        log: false,
+        numbersToExpressions: false,
+        renameGlobals: false,
+        rotateStringArray: true,
+        selfDefending: false,
+        shuffleStringArray: true,
+        simplify: true,
+        splitStrings: false,
+        stringArray: true,
+        stringArrayEncoding: true,
+        stringArrayWrappersCount: 1,
+        stringArrayWrappersChainedCalls: true,
+        stringArrayWrappersType: 'variable',
+        stringArrayThreshold: 0.75,
+        unicodeEscapeSequence: false,
+        transformObjectKeys: true
+      },
+      [
+        'vendor.js',
+        'polyfills.js'
+      ]
+    ),
+    ionicWebpackFactory.getIonicEnvironmentPlugin(),
+    ionicWebpackFactory.getCommonChunksPlugin()
+  ],
+
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell Webpack to provide empty mocks for them so importing them works.
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  }
+};
+
+var prodConfig = {
+  entry: process.env.IONIC_APP_ENTRY_POINT,
+  output: {
+    path: '{{BUILD}}',
+    publicPath: 'build/',
+    filename: '[name].js',
+    devtoolModuleFilenameTemplate: ionicWebpackFactory.getSourceMapperFunction()
+  },
+  devtool: process.env.IONIC_SOURCE_MAP_TYPE,
+
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    modules: [path.resolve('node_modules')]
+  },
+
+  module: {
+    loaders: getProdLoaders()
+  },
+
+  plugins: [
+    new Dotenv({
+      path: '.env.prod', // load this now instead of the ones in '.env'
+      systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+      silent: true // hide any errors
+    }),
+    new JavaScriptObfuscator({
+        compact: true,
+        controlFlowFlattening: false,
+        deadCodeInjection: false,
+        debugProtection: false,
+        debugProtectionInterval: false,
+        disableConsoleOutput: false,
+        identifierNamesGenerator: 'hexadecimal',
+        log: false,
+        numbersToExpressions: false,
+        renameGlobals: true,
+        rotateStringArray: true,
+        selfDefending: false,
+        shuffleStringArray: true,
+        simplify: true,
+        splitStrings: false,
+        stringArray: true,
+        stringArrayEncoding: true,
+        stringArrayWrappersCount: 1,
+        stringArrayWrappersChainedCalls: true,
+        stringArrayWrappersType: 'variable',
+        stringArrayThreshold: 0.75,
+        unicodeEscapeSequence: false,
+        transformObjectKeys: true
+      },
+      [
+        'vendor.js',
+        'polyfills.js'
+      ]
+    ),
+    ionicWebpackFactory.getIonicEnvironmentPlugin(),
+    ionicWebpackFactory.getCommonChunksPlugin(),
+    new ModuleConcatPlugin()
+  ],
+
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell Webpack to provide empty mocks for them so importing them works.
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  }
+};
+
+module.exports = {
+  dev: devConfig,
+  prod: prodConfig
+};
