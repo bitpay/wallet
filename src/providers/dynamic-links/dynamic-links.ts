14f2329630785c270f0a9b95dbdf71acd999588f
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2020-11-24 14:25:45 -0300

    Adds getDynamicLink function

--- src/providers/dynamic-links/dynamic-links.ts
+++ src/providers/dynamic-links/dynamic-links.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { FCMNG } from 'fcm-ng';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class DynamicLinksProvider {
   constructor(private FCMPlugin: FCMNG) {}
   onDynamicLink(): Promise<any> {
     return this.FCMPlugin.onDynamicLink();
+  }
+
+  getDynamicLink(): Observable<any> {
+    return this.FCMPlugin.getDynamicLink();
   }
 
   createDynamicLink(params: any): Promise<any> {
