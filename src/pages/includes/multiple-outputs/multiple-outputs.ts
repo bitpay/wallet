0b1ad2cc5c7bba77bda55ba77150552510298cc8
Gonzalo Leon <gonzalomleon@gmail.com>
2020-12-08 18:53:51 -0300

    Improve TxDetails to handle misunderstood outputs

--- src/pages/includes/multiple-outputs/multiple-outputs.ts
+++ src/pages/includes/multiple-outputs/multiple-outputs.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import * as _ from 'lodash';
 
 // Providers
@@ -15,6 +15,7 @@ import { WalletProvider } from '../../../providers/wallet/wallet';
 export class MultipleOutputsPage {
   public coin: Coin;
   private _tx;
+  private _misunderstoodOutputsMsg;
 
   public contactName: string;
   public showMultiplesOutputs: boolean;
@@ -28,9 +29,11 @@ export class MultipleOutputsPage {
     this.showMultiplesOutputs = false;
   }
 
+  @Output() openBlockChainEvent = new EventEmitter<string>();
   @Input()
   set tx(tx) {
     this._tx = tx;
+    this._misunderstoodOutputsMsg = tx.misunderstoodOutputs ? "There are some misunderstood outputs, please view on blockchain." : undefined;
     this.tx.outputs.forEach(output => {
       const outputAddr = output.toAddress ? output.toAddress : output.address;
       this.coin = this._tx.coin
@@ -54,6 +57,14 @@ export class MultipleOutputsPage {
     return this._tx;
   }
 
+  get misunderstoodOutputsMsg(){
+    return this._misunderstoodOutputsMsg;
+  }
+
+  viewOnBlockchain(): void {
+    this.openBlockChainEvent.next();
+  }
+  
   private contact(): void {
     const addr = this._tx.toAddress;
     this.addressBookProvider
