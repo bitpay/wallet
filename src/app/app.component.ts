eb636cfc8e4abe0e1f43504b850d66788da03397
Gabriel Masclef <gabrielmasclef@hotmail.com>
2021-01-19 10:11:19 -0300

    Ref: Remove Exchange crypto feature from MacOS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -516,7 +516,10 @@ export class CopayApp {
     }
 
     // Exchange Crypto
-    if (this.appProvider.info._enabledExtensions.exchangecrypto) {
+    if (
+      this.appProvider.info._enabledExtensions.exchangecrypto &&
+      this.platformProvider.getOS().OSName != 'MacOS'
+    ) {
       this.exchangeCryptoProvider.register();
     }
 
