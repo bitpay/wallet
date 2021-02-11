03de846905ab3e8de3ec071066d50be38ce14d39
johnathan White <jwhite@bitpay.com>
2021-02-11 07:57:54 -0500

    added animation to try it button

--- src/pages/new-feature/new-feature.ts
+++ src/pages/new-feature/new-feature.ts
@@ -1,10 +1,31 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, ViewChild } from '@angular/core';
 import { NavParams, Slides, ViewController } from 'ionic-angular';
 import { TryItType } from '../../providers/new-feature-data/new-feature-data';
 import { ThemeProvider } from '../../providers/theme/theme';
 @Component({
   selector: 'page-new-feature',
-  templateUrl: 'new-feature.html'
+  templateUrl: 'new-feature.html',
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({
+          transform: 'translateX(3px)',
+          opacity: 0
+        }),
+        animate('200ms')
+      ]),
+      transition(':leave', [
+        animate(
+          '200ms',
+          style({
+            transform: 'translateX(3px)',
+            opacity: 0
+          })
+        )
+      ])
+    ])
+  ]
 })
 export class NewFeaturePage {
   @ViewChild('newFeatureSlides') slider: Slides;
