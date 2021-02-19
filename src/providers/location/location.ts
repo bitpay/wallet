6c16e1e30ae3048de75422ea0e7d16c5e685f5d3
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2021-02-19 17:34:15 -0300

    Ref locationProvider

--- src/providers/location/location.ts
+++ src/providers/location/location.ts
@@ -1,21 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Logger } from '../../providers/logger/logger';
+
 @Injectable()
 export class LocationProvider {
   public countryPromise: Promise<string>;
-  constructor(public http: HttpClient) {
-    this.getCountry();
+  constructor(public http: HttpClient, private logger: Logger) {
+    this.logger.debug('LocationProvider initialized');
   }
 
   getCountry(): Promise<string> {
-    this.countryPromise = this.countryPromise
-      ? this.countryPromise
-      : this.http
-          .get('https://bitpay.com/wallet-card/location')
-          .map((res: { country: string }) => res.country)
-          .toPromise()
-          .catch(_ => 'US');
+    if (this.countryPromise) {
+      this.logger.debug('Got cached country');
+      return this.countryPromise;
+    }
+    this.countryPromise = this.http
+      .get('https://bitpay.com/wallet-card/location')
+      .map((res: { country: string }) => res.country)
+      .toPromise()
+      .catch(_ => 'US');
     return this.countryPromise;
   }
 }
