7c13d8219ba2ce11da1ffd2bd9d99f071d9578b3
johnathan White <jwhite@bitpay.com>
2021-05-06 12:19:41 -0400

    hiding switcher if cardTop

--- src/pages/send/amount/amount.ts
+++ src/pages/send/amount/amount.ts
@@ -95,6 +95,7 @@ export class AmountPage {
   private alternativeCurrency;
   public fromBuyCrypto: boolean;
   public fromExchangeCrypto: boolean;
+  public isCardTopUp: boolean;
   public quoteForm: FormGroup;
   public supportedFiatAltCurrencies: string[];
   public altCurrenciesToShow: string[];
@@ -149,7 +150,7 @@ export class AmountPage {
     this.alternativeCurrency = this.navParams.data.alternativeCurrency;
     this.fromBuyCrypto = this.navParams.data.fromBuyCrypto;
     this.fromExchangeCrypto = this.navParams.data.fromExchangeCrypto;
-
+    this.isCardTopUp = !!this.navParams.data.card;
     this.showSendMax = false;
     this.useSendMax = false;
     this.allowSend = false;
