4045945de29d1cdbdb8b27867221fb6aef42efa2
johnathan White <jwhite@bitpay.com>
2021-08-09 22:15:44 -0400

    fixed patch script

--- utils/walletconnect-patch.sh
+++ utils/walletconnect-patch.sh
@@ -3,6 +3,6 @@ readonly/g;s/[[:space:]]get/\/\/@ts-ignore\
 get/g;s/[[:space:]]set/\/\/@ts-ignore\
 set/g' ./node_modules/@walletconnect/core/dist/cjs/index.d.ts;
 sed -i -e 's/import("detect-browser").OperatingSystem | NodeJS.Platform | undefined*/any/g' ./node_modules/@walletconnect/browser-utils/dist/cjs/browser.d.ts;
-sed -i -e 's/^export interface JsonRpcRequest.*/\/\/@ts-ignore\n&/g' ./node_modules/@walletconnect/jsonrpc-types/dist/cjs/jsonrpc.d.ts;
+sed -i -e 's/extends Required<RequestArguments<T>>//g' ./node_modules/@walletconnect/jsonrpc-types/dist/cjs/jsonrpc.d.ts;
 sed -i -e 's/readonly/\/\/@ts-ignore\
 readonly/g' ./node_modules/detect-browser/index.d.ts
