5702b93ec4fca273c8e9a59d5cfc6a4982f6e022
johnathan White <jwhite@bitpay.com>
2021-08-20 12:10:28 -0400

    moved onGoingProcess clear

--- src/providers/dynamic-links/dynamic-links.ts
+++ src/providers/dynamic-links/dynamic-links.ts
@@ -49,10 +49,10 @@ export class DynamicLinksProvider {
           if (ready && !this.locked) {
             this.locked = true;
             setTimeout(() => {
+              this.onGoingProcessProvider.clear();
               this.incomingDataProvider.redir(decodeURIComponent(dynLink), {
                 force: true
               });
-              this.onGoingProcessProvider.clear();
             }, timeout);
             setTimeout(() => (this.locked = false), LOCK_TIMEOUT);
             subscription && subscription.unsubscribe();
