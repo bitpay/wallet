751d9daa23e4e887736233f33341d1d9d82812c5
khanh vinh nguyen <khanhvinh.nguyen.0628@gmail.com>
2021-08-30 12:59:38 +0700

    issue-78: exchange rates

--- src/components/exchange-rates/exchange-rates.ts
+++ src/components/exchange-rates/exchange-rates.ts
@@ -106,6 +106,11 @@ export class ExchangeRates {
     this.fiatIsoCode = this.isFiatIsoCodeSupported ? alternativeIsoCode : 'USD';
   }
 
+  public decimaltoFox(coin:string) {
+    var decimal = Math.round(1/Number(coin)).toString().length+1;
+      return Number(coin).toFixed(decimal);
+  }
+
   public getDigitsInfo(coin: string) {
     switch (coin) {
       case 'xrp':
