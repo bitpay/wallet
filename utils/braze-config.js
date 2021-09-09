28d8ae5d63d97b66b50c2f8e10637e6e57e23a1d
johnathan White <jwhite@bitpay.com>
2021-09-09 17:39:15 -0400

    added android config

--- utils/braze-config.js
+++ utils/braze-config.js
@@ -8,22 +8,36 @@ const file = `${__dirname}/../app-template/config-template.xml`;
 let content = fs.readFileSync(file, 'utf8');
 
 
-readline.question('Enter Braze key: ', key => {
-  readline.question('Enter Braze endpoint: ', endpoint => {
+readline.question('Which platform? (ios or android): ', entry => {
 
-    content = content.replace(
-      /BRAZE_API_KEY_REPLACE_ME/g,
-      key
-    );
-
-    content = content.replace(
-      /BRAZE_API_ENDPOINT_REPLACE_ME/g,
-      endpoint
-    );
-
-    fs.writeFileSync(file, content);
-    console.log('Braze config successfully updated');
-
+  if (!['ios', 'android'].includes(entry)) {
+    console.error('invalid platform');
     readline.close();
+    return;
+  }
+
+  readline.question('Enter Braze key: ', key => {
+    readline.question('Enter Braze endpoint: ', endpoint => {
+
+      const platform = entry.toUpperCase();
+
+      content = content.replace(
+        `${platform}_BRAZE_API_KEY_REPLACE_ME`,
+        key
+      );
+
+      content = content.replace(
+        `${platform}_BRAZE_API_ENDPOINT_REPLACE_ME`,
+        endpoint
+      );
+
+      fs.writeFileSync(file, content);
+      console.log(`Braze config successfully updated for ${platform}`);
+
+
+      readline.close();
+    });
   });
+
 });
+
