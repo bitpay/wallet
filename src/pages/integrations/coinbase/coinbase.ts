7d558a1ea443ba30f85bc0a8131b41f0d9fa51fb
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2021-09-23 10:39:23 -0300

    Revert security code condition

--- src/pages/integrations/coinbase/coinbase.ts
+++ src/pages/integrations/coinbase/coinbase.ts
@@ -102,7 +102,7 @@ export class CoinbasePage {
   private submitOauthCode(code: string): void {
     // Security check
     if (
-      this.isElectron &&
+      !this.isElectron &&
       !this.navParams.data.state &&
       this.navParams.data.state != this.coinbaseProvider.getCurrentState()
     ) {
