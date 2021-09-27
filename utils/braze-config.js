73de60b4dd575a1444ace9f4d76ae7fae08e9486
johnathan White <jwhite@bitpay.com>
2021-09-27 10:29:08 -0400

    updated braze config script

--- utils/braze-config.js
+++ utils/braze-config.js
@@ -7,9 +7,7 @@ const readline = require('readline').createInterface({
 const file = `${__dirname}/../app-template/config-template.xml`;
 let content = fs.readFileSync(file, 'utf8');
 
-
 readline.question('Which platform? (ios or android): ', entry => {
-
   if (!['ios', 'android'].includes(entry)) {
     console.error('invalid platform');
     readline.close();
@@ -19,25 +17,31 @@ readline.question('Which platform? (ios or android): ', entry => {
   readline.question('Enter Braze key: ', key => {
     readline.question('Enter Braze endpoint: ', endpoint => {
 
+      const complete = () => {
+        fs.writeFileSync(file, content);
+        console.log(`Braze config successfully updated for ${platform}`);
+        readline.close();
+      };
       const platform = entry.toUpperCase();
 
-      content = content.replace(
-        `${platform}_BRAZE_API_KEY_REPLACE_ME`,
-        key
-      );
+      content = content.replace(`${platform}_BRAZE_API_KEY_REPLACE_ME`, key);
 
       content = content.replace(
         `${platform}_BRAZE_API_ENDPOINT_REPLACE_ME`,
         endpoint
       );
 
-      fs.writeFileSync(file, content);
-      console.log(`Braze config successfully updated for ${platform}`);
-
-
-      readline.close();
+      if (platform === 'ANDROID') {
+        readline.question('Enter fcm senderId: ', senderId => {
+          content = content.replace(
+            `ANDROID_BRAZE_FCM_SENDER_ID_REPLACE_ME`,
+            `str_${senderId}`
+          );
+          complete();
+        });
+      } else {
+        complete();
+      }
     });
   });
-
 });
-
