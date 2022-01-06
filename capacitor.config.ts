c67ceb8c57fad33cfe4b25f62d80f606f15b0d4c
Van Tran <vant@jiohealth.com>
2022-01-06 17:07:54 +0700

    update script package json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.abcpay.wallet.dev',
+  appId: 'cash.abcpay.wallet.dev',
   appName: 'abcpay-new',
   webDir: 'www',
   bundledWebRuntime: false,
