58aaacc266613e3e2717368c6e970dde026597e8
takYoon <tan8651913@gmail.com>
2022-04-23 10:10:46 +0700

    udpate switch currency in flow send

--- src/app/components/recipient/recipient.component.ts
+++ src/app/components/recipient/recipient.component.ts
@@ -166,8 +166,8 @@ export class RecipientComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.setAvailableUnits();
-    this.updateUnitUI();
+    this.setAvailableUnits(!!(this.token && this.token.tokenId));
+    this.updateUnitUI(!!(this.token && this.token.tokenId));
     if (this.recipient && this.recipient.toAddress && !this.isDonation) {
       this.searchValue = this.recipient.toAddress;
       this.processInput();
@@ -184,6 +184,25 @@ export class RecipientComponent implements OnInit {
     }
   }
 
+  public changeUnit(): void {
+    if (this.fixedUnit) return;
+
+    this.unitIndex++;
+    if (this.unitIndex >= this.availableUnits.length) this.unitIndex = 0;
+
+    if (this.availableUnits[this.unitIndex].isFiat) {
+      // Always return to BTC... TODO?
+      this.altUnitIndex = 0;
+    } else {
+      this.altUnitIndex = _.findIndex(this.availableUnits, {
+        isFiat: true
+      });
+    }
+
+    this.expression = '';
+    this.processAmount();
+    this.updateUnitUI();
+  }
 
   private updateAddressHandler: any = data => {
     if (data.recipientId === this.recipient.id) {
@@ -191,18 +210,20 @@ export class RecipientComponent implements OnInit {
     }
   };
 
-  private updateUnitUI(): void {
+  private updateUnitUI(isToken?: boolean): void {
     this.unit = this.availableUnits[this.unitIndex].shortName;
     this.alternativeUnit = this.availableUnits[this.altUnitIndex].shortName;
-    const { unitToSatoshi, unitDecimals } = this.availableUnits[this.unitIndex]
-      .isFiat
-      ? this.currencyProvider.getPrecision(
-        this.availableUnits[this.altUnitIndex].id
-      )
-      : this.currencyProvider.getPrecision(this.unit.toLowerCase() as Coin);
-    this.unitToSatoshi = unitToSatoshi;
-    this.satToUnit = 1 / this.unitToSatoshi;
-    this.unitDecimals = unitDecimals;
+    if (!isToken) {
+      const { unitToSatoshi, unitDecimals } = this.availableUnits[this.unitIndex]
+        .isFiat
+        ? this.currencyProvider.getPrecision(
+          this.availableUnits[this.altUnitIndex].id
+        )
+        : this.currencyProvider.getPrecision(this.unit.toLowerCase() as Coin);
+      this.unitToSatoshi = unitToSatoshi;
+      this.satToUnit = 1 / this.unitToSatoshi;
+      this.unitDecimals = unitDecimals;
+    }
     this.processAmount();
     this.logger.debug(
       'Update unit coin @amount unit:' +
@@ -212,23 +233,33 @@ export class RecipientComponent implements OnInit {
     );
   }
 
-  private setAvailableUnits(): void {
+  private setAvailableUnits(isToken?: boolean): void {
     this.availableUnits = [];
 
-    const parentWalletCoin = this.navParamsData.wallet
+    const parentWalletCoin = !isToken ? (this.navParamsData.wallet
       ? this.navParamsData.wallet.coin
-      : this.wallet && this.wallet.coin;
+      : this.wallet && this.wallet.coin)
+      : this.token.tokenInfo.coin;
 
-    for (const coin of this.currencyProvider.getAvailableCoins()) {
-      if (parentWalletCoin === coin || !parentWalletCoin) {
-        const { unitName, unitCode } = this.currencyProvider.getPrecision(coin);
-        this.availableUnits.push({
-          name: this.currencyProvider.getCoinName(coin),
-          id: unitCode,
-          shortName: unitName
-        });
+    if (isToken) {
+      this.availableUnits.push({
+        name: this.token.tokenInfo.name,
+        id: this.token.tokenId,
+        shortName: this.token.tokenInfo.symbol
+      });
+    } else {
+      for (const coin of this.currencyProvider.getAvailableCoins()) {
+        if (parentWalletCoin === coin || !parentWalletCoin) {
+          const { unitName, unitCode } = this.currencyProvider.getPrecision(coin);
+          this.availableUnits.push({
+            name: this.currencyProvider.getCoinName(coin),
+            id: unitCode,
+            shortName: unitName
+          });
+        }
       }
     }
+
 
     this.unitIndex = 0;
 
@@ -279,6 +310,11 @@ export class RecipientComponent implements OnInit {
 
     if (this.navParamsData.fixedUnit) {
       this.fixedUnit = true;
+    }
+    if (this.token) {
+      if (!this.rateProvider.getRate(this.fiatCode, this.token.tokenInfo.symbol)) {
+        this.fixedUnit = true;
+      }
     }
   }
 
@@ -333,6 +369,14 @@ export class RecipientComponent implements OnInit {
     );
   }
 
+  private fromFiatToken(val: number, tokenSymbol): number {
+    return parseFloat(
+      (
+        this.rateProvider.fromFiatToken(val, this.fiatCode, tokenSymbol)
+      ).toFixed(2)
+    );
+  }
+
   changeSelectedAmount(event) {
     if (this.isSelectedTotalAmout) {
       this.expression = this.amountFromSelectedInput;
@@ -359,7 +403,7 @@ export class RecipientComponent implements OnInit {
     return parseFloat(rateProvider.toString());
   }
 
-  public processAmount(): void {
+  public processAmount(isSendMax?:boolean): void {
     let formatedValue = this.format(this.expression);
     let result = this.evaluate(formatedValue);
     this.allowSend = this.onlyIntegers
@@ -401,16 +445,24 @@ export class RecipientComponent implements OnInit {
     let amount = result;
     if (this.token) {
       const decimals = _.get(this.token, 'tokenInfo.decimals', undefined);
-      const unit = Math.pow(10, decimals)
-      if (decimals && result > 0) amount = (amount * unit).toFixed(0);
-      this.recipient.amount = parseInt(amount, 10);
-      const alternativeAmount = this.rateProvider.toFiatToken(
-        result,
-        this.wallet.cachedStatus.alternativeIsoCode,
-        this.token.tokenInfo.symbol
-      );
-      this.alternativeAmount = this.filterProvider.formatFiatAmount(alternativeAmount);
-      this.recipient.altAmountStr = this.alternativeAmount;
+      const unitDecimal = Math.pow(10, decimals);
+      if (!unit.isFiat) {
+        if (decimals && result > 0) amount = (amount * unitDecimal).toFixed(0);
+        this.recipient.amount = parseInt(amount, 10);
+        const alternativeAmount = this.rateProvider.toFiatToken(
+          result,
+          this.wallet.cachedStatus.alternativeIsoCode,
+          this.token.tokenInfo.symbol
+        );
+        this.alternativeAmount = this.filterProvider.formatFiatAmount(alternativeAmount);
+        this.recipient.altAmountStr = this.alternativeAmount;
+      }
+      else {
+        amount = this.fromFiatToken(amount, this.token.tokenInfo.symbol);
+        this.recipient.amount = parseInt((amount * unitDecimal).toFixed(0), 10);
+        this.alternativeAmount = this.filterProvider.formatFiatAmount(amount).toString();
+        this.recipient.altAmountStr = this.alternativeAmount;
+      }
     } else {
       amount = unit.isFiat
         ? (this.fromFiat(amount) * this.unitToSatoshi).toFixed(0)
@@ -422,6 +474,9 @@ export class RecipientComponent implements OnInit {
     }
     this.validAmount = result > 0;
     this.checkRecipientValid();
+    if(isSendMax){
+      this.sendMaxEvent.emit(true);
+    }
   }
 
   public async processInput() {
@@ -614,9 +669,10 @@ export class RecipientComponent implements OnInit {
   public sendMax(): void {
     if (this.token) {
       this.expression = this.token.amountToken;
-      this.processAmount()
+      this.processAmount(true);
+
     } else {
-      this.sendMaxEvent.emit(true);
+      this.sendMaxEvent.emit(false);
     }
   }
 
